be307f9ebb252e91d58258806072b7ef1b9e0315
Ceki Gulcu <ceki@apache.org>
2000-12-14 13:11:14 +0000

    very simple usage example
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@308882 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/test/Hello.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.APL file.  */
+
+package org.log4j.test; 
+
+import org.log4j.Category;
+import org.log4j.BasicConfigurator;
+
+/**
+   Very simple log4j usage example.
+
+   @author  Ceki G&uuml;lc&uuml;   
+ */
+public class Hello {
+
+  static Category cat = Category.getInstance(Hello.class);
+
+  public 
+  static 
+  void main(String argv[]) {
+    BasicConfigurator.configure();
+    cat.debug("Hello world.");
+    cat.info("What a beatiful day.");
+  }
+}
