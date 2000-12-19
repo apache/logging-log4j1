0023be27462973ac41f161a837245cd9fd4aa6cf
Ceki Gulcu <ceki@apache.org>
2000-12-19 13:23:43 +0000

    DisableOverrideTest.java now uses the DOMConfigurator.configure method. do?.xml files modified to refer to log4j.dtd instead of the wrong ../log4j.dtd.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@308890 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/xml/test/DisableOverrideTest.java
+++ src/java/org/apache/log4j/xml/test/DisableOverrideTest.java
@@ -6,9 +6,9 @@ import org.apache.log4j.xml.DOMConfigurator;
 import org.apache.log4j.SimpleLayout;
 import org.apache.log4j.FileAppender;
 
-import org.xml.sax.InputSource;
-import org.apache.log4j.xml.examples.ReportParserError;
-import org.apache.xerces.parsers.DOMParser;
+//import org.xml.sax.InputSource;
+//import org.apache.log4j.xml.examples.ReportParserError;
+//import org.apache.xerces.parsers.DOMParser;
 
 public class DisableOverrideTest {
 
@@ -22,20 +22,21 @@ public class DisableOverrideTest {
     else 
       Usage("Wrong number of arguments.");
      
-    try {
-      DOMParser domParser = new DOMParser();
-      domParser.setFeature("http://xml.org/sax/features/validation", true);
-      domParser.setFeature("http://apache.org/xml/features/validation/dynamic",
-			   true);      
-      domParser.setErrorHandler(new ReportParserError());      
-      domParser.parse(new InputSource(configFile));
-      DOMConfigurator.configure(domParser.getDocument().getDocumentElement());
-    }
-    catch(Exception e) {
-      System.err.println("Could not initialize test program.");
-      e.printStackTrace();
-      System.exit(1);           
-    }
+    //try {
+      //DOMParser domParser = new DOMParser();
+      //domParser.setFeature("http://xml.org/sax/features/validation", true);
+      //domParser.setFeature("http://apache.org/xml/features/validation/dynamic",
+      //		   true);      
+      //domParser.setErrorHandler(new ReportParserError());      
+      //domParser.parse(new InputSource(configFile));
+      //DOMConfigurator.configure(domParser.getDocument().getDocumentElement());
+    //}
+    //catch(Exception e) {
+    //System.err.println("Could not initialize test program.");
+    //e.printStackTrace();
+    // System.exit(1);           
+    //}
+    DOMConfigurator.configure(configFile);
     DOMConfigurator.disableInfo();       
     CAT.debug("Hello world");
   }
