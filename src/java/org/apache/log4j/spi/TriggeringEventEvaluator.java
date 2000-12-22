ee656c245af09d7266475c7423102d80fa1c530b
Ceki Gulcu <ceki@apache.org>
2000-12-22 13:58:30 +0000

    Restructured the SMTPAppedner to use a more modular design where the e-mail triggering event is defined by an object implementing the spi/TriggeringEventEvaluator.java interface.
    No big deal but a sligtly better design.
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@308901 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/spi/TriggeringEventEvaluator.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.APL file.  */
+
+package org.apache.log4j.spi;
+
+/**
+   
+   Implementions of this interface allow certain appenders to decide
+   when to perform an appender specific action.
+
+  <p>See {@link org.apache.log4j.nete.SMPTPAppender} for an example of
+  an appender that depends on a
+  <code>TriggeringEventEvaluators</code>.
+
+  @author Ceki G&uuml;lc&uuml;
+  @since version 1.0
+   
+ */
+public interface TriggeringEventEvaluator {
+  
+  /**
+     Is this the triggering event?
+   */
+  public boolean isTriggeringEvent(LoggingEvent event);
+}
