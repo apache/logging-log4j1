b567dc3b99e4edf43205161ceecdc02a148081b3
Ceki Gulcu <ceki@apache.org>
2001-02-21 07:38:40 +0000

    Added one new test.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@309017 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/test/UnitTestDRFA.java
+++ src/java/org/apache/log4j/test/UnitTestDRFA.java
@@ -9,6 +9,8 @@
 
 package org.apache.log4j;
 
+import java.util.Calendar;
+import java.util.Date;
 
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -23,20 +25,105 @@ public class UnitTestDRFA extends TestCase {
   }
 
   public
-  void test1() {
+  void testComputeCheckPeriod() {
     DailyRollingFileAppender drfa = new DailyRollingFileAppender();
+    drfa.setName("testComputeCheckPeriod");
     drfa.setOption(DailyRollingFileAppender.DATE_PATTERN_OPTION, 
-		   "YY-MM-dd");
-
+		   "yyyy-MM-dd.'log'");
     assertEquals(drfa.computeCheckPeriod(), 
 		 DailyRollingFileAppender.TOP_OF_DAY);
+
+    drfa.setOption(DailyRollingFileAppender.DATE_PATTERN_OPTION, 
+		   "yyyy-MM-dd mm.'log'");
+    assertEquals(drfa.computeCheckPeriod(), 
+		 DailyRollingFileAppender.TOP_OF_MINUTE);
+
+    drfa.setOption(DailyRollingFileAppender.DATE_PATTERN_OPTION, 
+		   "yyyy-MM-dd a.'log'");
+    assertEquals(drfa.computeCheckPeriod(), 
+		 DailyRollingFileAppender.HALF_DAY);
+
+    drfa.setOption(DailyRollingFileAppender.DATE_PATTERN_OPTION, 
+		   "yyyy-MM-dd HH.'log'");
+    assertEquals(drfa.computeCheckPeriod(), 
+		 DailyRollingFileAppender.TOP_OF_HOUR);
+
+    drfa.setOption(DailyRollingFileAppender.DATE_PATTERN_OPTION, 
+		   "yyyy-MM.'log'");
+    assertEquals(drfa.computeCheckPeriod(), 
+		 DailyRollingFileAppender.TOP_OF_MONTH);
+
+    drfa.setOption(DailyRollingFileAppender.DATE_PATTERN_OPTION, 
+		   "HH'log'");
+    assertEquals(drfa.computeCheckPeriod(), 
+		 DailyRollingFileAppender.TOP_OF_HOUR);
+
+
   }
 
+
+  public
+  void testRC1() {  
+    RollingCalendar rc = new RollingCalendar();
+    rc.setType(DailyRollingFileAppender.TOP_OF_DAY);
+
+
+    Calendar c = Calendar.getInstance();
+    c.clear();
+    c.set(Calendar.YEAR, 20);
+    c.set(Calendar.MONTH, 2);
+    c.set(Calendar.DAY_OF_MONTH, 10); 
+    c.set(Calendar.HOUR_OF_DAY, 1);
+    c.set(Calendar.MINUTE, 10); 
+    c.set(Calendar.SECOND, 10);
+    c.set(Calendar.MILLISECOND, 88);
+    
+    c.setTime(rc.getNextCheckDate(c.getTime()));
+    assertEquals(c.get(Calendar.DAY_OF_MONTH), 11);
+    assertEquals(c.get(Calendar.HOUR_OF_DAY), 0);
+    assertEquals(c.get(Calendar.MINUTE), 0);
+    assertEquals(c.get(Calendar.SECOND), 0);
+    assertEquals(c.get(Calendar.MILLISECOND), 0);
+  }
+
+  public
+  void testRC2() {  
+    RollingCalendar rc = new RollingCalendar();
+    rc.setType(DailyRollingFileAppender.TOP_OF_DAY);
+
+
+    Calendar c = Calendar.getInstance();
+    c.clear();
+    c.set(Calendar.YEAR, 20);
+    c.set(Calendar.MONTH, Calendar.JANUARY); 
+    c.set(Calendar.DATE, 31); 
+    c.set(Calendar.HOUR_OF_DAY, 1);
+    c.set(Calendar.MINUTE, 10); 
+    c.set(Calendar.SECOND, 10);
+    c.set(Calendar.MILLISECOND, 88);
+
+    System.out.println(c);
+    System.out.println("\n");
+
+    c.setTime(rc.getNextCheckDate(c.getTime()));
+    assertEquals(c.get(Calendar.MONTH), Calendar.FEBRUARY);
+    assertEquals(c.get(Calendar.DATE), 1);
+    assertEquals(c.get(Calendar.HOUR_OF_DAY), 0);
+    assertEquals(c.get(Calendar.MINUTE), 0);
+    assertEquals(c.get(Calendar.SECOND), 0);
+    assertEquals(c.get(Calendar.MILLISECOND), 0);
+
+    System.out.println(c);
+  }
+
+  
   public 
   static
   Test suite() {
     TestSuite suite = new TestSuite();
-    suite.addTest(new UnitTestDRFA("test1"));
+    suite.addTest(new UnitTestDRFA("testComputeCheckPeriod"));
+    suite.addTest(new UnitTestDRFA("testRC1"));
+    suite.addTest(new UnitTestDRFA("testRC2"));
     return suite;
   }
   
