b88ee05d0bf6c11b3ef8d66a1a2e7669c6a3c470
Ceki Gulcu <ceki@apache.org>
2001-03-15 17:40:01 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@309046 13f79535-47bb-0310-9956-ffa450edef68

---
+++ build/build.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#--------------------------------------------
+# No need to edit anything past here
+#--------------------------------------------
+if test -z "${JAVA_HOME}" ; then
+    echo "ERROR: JAVA_HOME not found in your environment."
+    echo "Please, set the JAVA_HOME variable in your environment to match the"
+    echo "location of the Java Virtual Machine you want to use."
+    exit
+fi
+
+if test -f ${JAVA_HOME}/lib/tools.jar ; then
+    CLASSPATH=${CLASSPATH}:${JAVA_HOME}/lib/tools.jar
+fi
+
+# convert the existing path to unix
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+fi
+
+ANT_JAR='lib/ant.jar'
+JAXP_JAR='lib/jaxp.jar'
+PARSER_JAR='lib/parser.jar'
+
+CLASSPATH=${CLASSPATH}:${ANT_JAR}
+CLASSPATH=${CLASSPATH}:${JAXP_JAR}
+CLASSPATH=${CLASSPATH}:${PARSER_JAR}
+
+# convert the unix path to windows
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+fi
+
+BUILDFILE=build.xml
+
+${JAVA_HOME}/bin/java -classpath ${CLASSPATH} org.apache.tools.ant.Main \
+                      -buildfile ${BUILDFILE} "$@"
