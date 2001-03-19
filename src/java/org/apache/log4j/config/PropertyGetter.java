fac773b99729bfd6a966aaa4a6d109363569b7d9
Anders Kristensen <anders@apache.org>
2001-03-19 12:31:42 +0000

    Utility classes for handling new JavaBeans style configuration.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@309047 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/config/PropertyGetter.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.APL file.
+ */
+
+package org.apache.log4j.config;
+
+import java.beans.*;
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Priority;
+import org.apache.log4j.helpers.LogLog;
+
+/**
+   Used for inferring configuration information for a log4j's component.
+   
+   @author  Anders Kristensen
+ */
+public class PropertyGetter {
+  protected static final Object[] NULL_ARG = new Object[] {};
+  protected Object obj;
+  protected PropertyDescriptor[] props;
+  
+  public interface PropertyCallback {
+    void foundProperty(Object obj, String prefix, String name, Object value);
+  }
+  
+  /**
+    Create a new PropertySetter for the specified Object. This is done
+    in prepartion for invoking {@link #setProperty} one or more times.
+    
+    @param obj  the object for which to set properties
+   */
+  public
+  PropertyGetter(Object obj) throws IntrospectionException {
+    BeanInfo bi = Introspector.getBeanInfo(obj.getClass());
+    props = bi.getPropertyDescriptors();
+    this.obj = obj;
+  }
+  
+  public
+  static
+  void getProperties(Object obj, PropertyCallback callback, String prefix) {
+    try {
+      new PropertyGetter(obj).getProperties(callback, prefix);
+    } catch (IntrospectionException ex) {
+      LogLog.error("Failed to introspect object " + obj, ex);
+    }
+  }
+  
+  public
+  void getProperties(PropertyCallback callback, String prefix) {
+    for (int i = 0; i < props.length; i++) {
+        Method getter = props[i].getReadMethod();
+        if (getter == null) continue;
+        if (!isHandledType(getter.getReturnType())) {
+          //System.err.println("Ignoring " + props[i].getName() +" " + getter.getReturnType());
+          continue;
+        }
+        String name = props[i].getName();
+        try {
+          Object result = getter.invoke(obj, NULL_ARG);
+          //System.err.println("PROP " + name +": " + result);
+          if (result != null) {
+            callback.foundProperty(obj, prefix, name, result);
+          }
+        } catch (Exception ex) {
+          LogLog.warn("Failed to get value of property " + name);
+        }
+    }
+  }
+  
+  protected
+  boolean isHandledType(Class type) {
+    return String.class.isAssignableFrom(type) ||
+           Integer.TYPE.isAssignableFrom(type) ||
+           Long.TYPE.isAssignableFrom(type)    ||
+           Boolean.TYPE.isAssignableFrom(type) ||
+           Priority.class.isAssignableFrom(type);
+  }
+}
---
+++ src/java/org/apache/log4j/config/PropertySetterException.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software License
+ * version 1.1, a copy of which has been included  with this distribution in
+ * the LICENSE.APL file.
+ */
+
+package org.apache.log4j.config;
+
+/**
+ * Thrown when an error is encountered whilst attempting to set a property
+ * using the {@link PropertySetter} utility class.
+ * 
+ * @author Anders Kristensen
+ * @since 1.1
+ */
+public class PropertySetterException extends Exception {
+  protected Throwable rootCause;
+  
+  public
+  PropertySetterException(String msg) {
+    super(msg);
+  }
+  
+  public
+  PropertySetterException(Throwable rootCause)
+  {
+    super();
+    this.rootCause = rootCause;
+  }
+  
+  /**
+     Returns descriptive text on the cause of this exception.
+   */
+  public
+  String getMessage() {
+    String msg = super.getMessage();
+    if (msg == null && rootCause != null) {
+      msg = rootCause.getMessage();
+    }
+    return msg;
+  }
+}
