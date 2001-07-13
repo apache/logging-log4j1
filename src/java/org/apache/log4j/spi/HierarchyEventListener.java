1a02da6a5cae8ebabbc4f404a331670b6a45fa1b
Ceki Gulcu <ceki@apache.org>
2001-07-13 22:19:28 +0000

    Added missing HierarchyEventListener.java
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@309229 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/spi/HierarchyEventListener.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.  */
+
+package org.apache.log4j.spi;
+
+import org.apache.log4j.Category;
+
+/**
+   
+  Implement this interface to create new instances of Category or
+  a sub-class of Category.
+
+  <p>See {@link org.apache.log4j.examples.MyCategory} for an example.
+
+  @author Ceki G&uuml;lc&uuml;
+  @since version 0.8.5
+   
+ */
+public interface HierarchyEventListener {
+
+ 
+  public
+  void categoryCreationEvent(Category cat);
+
+}
