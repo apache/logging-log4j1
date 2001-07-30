955e26b4966417c94779a0501a2064ca2a58d63a
Ceki Gulcu <ceki@apache.org>
2001-07-30 20:32:49 +0000

    Added missing XTest file. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@309248 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/xml/test/XTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.APL file.  */
+
+package org.apache.log4j.xml.test;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.log4j.Category;
+import org.apache.log4j.Priority;
+
+import org.apache.log4j.xml.examples.XPriority;
+import org.apache.log4j.xml.examples.XCategory;
+
+
+/**
+   
+   
+   @author Ceki G&uuml;lc&uuml;
+*/
+public class XTest {
+  static XCategory cat = (XCategory) XCategory.getInstance(XTest.class);
+
+
+  public 
+  static 
+  void main(String argv[]) {
+
+    if(argv.length == 1) 
+      init(argv[0]);
+    else 
+      Usage("Wrong number of arguments.");
+
+    test();
+  }
+
+  static
+  void Usage(String msg) {
+    System.err.println(msg);
+    System.err.println( "Usage: java " + XTest.class.getName() +
+			" configFile");
+    System.exit(1);
+  }
+  
+  static
+  void init(String configFile) {
+    DOMConfigurator.configure(configFile);
+  }
+
+  static
+  void test() {
+    int i = -1;
+    Category root = Category.getRoot();
+
+    cat.trace("Message " + ++i);
+    root.debug("Message " + i);        
+    
+    cat.debug("Message " + ++i);
+    root.debug("Message " + i);        
+
+    cat.info ("Message " + ++i);
+    root.info("Message " + i);        
+
+    cat.warn ("Message " + ++i);
+    root.warn("Message " + i);        
+
+    cat.error("Message " + ++i);
+    root.error("Message " + i);
+    
+    cat.log(Priority.FATAL, "Message " + ++i);
+    root.log(Priority.FATAL, "Message " + i);    
+    
+    Exception e = new Exception("Just testing");
+    cat.debug("Message " + ++i, e);
+    root.debug("Message " + i, e);
+    
+    cat.error("Message " + ++i, e);
+    root.error("Message " + i, e);    
+
+    Category.shutdown();
+  }
+}
