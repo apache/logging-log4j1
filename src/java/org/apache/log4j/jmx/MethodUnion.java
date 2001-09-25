5cf2853ae174d2b56480ff118f81cabc419180d5
Ceki Gulcu <ceki@apache.org>
2001-09-25 08:48:45 +0000

    Added missing files in ..../jmx/ Added support for the logger prefix in PropertyConfigurator.java.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@309325 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/jmx/MethodUnion.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.  */
+
+package org.apache.log4j.jmx;
+
+import java.lang.reflect.Method;
+
+class MethodUnion {
+
+  Method readMethod;
+  Method writeMethod;
+
+  MethodUnion( Method readMethod, Method writeMethod) {
+    this.readMethod = readMethod;
+    this.writeMethod = writeMethod;
+  }
+
+}
