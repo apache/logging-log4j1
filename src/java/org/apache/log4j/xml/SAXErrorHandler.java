6386da7dc2bb38512e7c6aa0c6aa4e4be0034dd4
Ceki Gulcu <ceki@apache.org>
2001-10-04 21:07:04 +0000

    - Improved error reporting in xml/DOMConfigurator.java
    - fixed compatibility issue in log4j.dtd
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@309342 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/xml/SAXErrorHandler.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.  */
+
+package org.apache.log4j.xml;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXParseException;
+import org.apache.log4j.helpers.LogLog;
+
+public class SAXErrorHandler implements ErrorHandler {
+
+  public
+  void error(SAXParseException ex) {
+    LogLog.error("Parsing error on line "+ex.getLineNumber()+" and column "
+		 +ex.getColumnNumber());
+    LogLog.error(ex.getMessage(), ex.getException());
+  }
+  
+  public
+  void fatalError(SAXParseException ex) {
+    error(ex);
+  }
+   
+  public
+  void warning(SAXParseException ex) {
+    LogLog.warn("Parsing error on line "+ex.getLineNumber()+" and column "
+		+ex.getColumnNumber());
+    LogLog.warn(ex.getMessage(), ex.getException());
+  }
+
+
+}
