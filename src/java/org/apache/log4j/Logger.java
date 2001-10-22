751d63ee9d83b33f434557270c678569e814b04a
Ceki Gulcu <ceki@apache.org>
2001-10-22 23:08:59 +0000

    Added support for the FINE level as an alias for DEBUG.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@309352 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/Logger.java
+++ src/java/org/apache/log4j/Logger.java
@@ -38,9 +38,61 @@ import java.net.MalformedURLException;
   @author Ceki G&uuml;lc&uuml; */
 public class Logger extends Category {
 
+  /**
+     The fully qualified name of the Level class. See also the 
+     getFQCN method. */
+  private static final String FQCN = Level.class.getName();
+
+
   protected 
   Logger(String name) {
     super(name);
+  }
+
+  /** 
+    Log a message object with the {@link Level#FINE FINE} level which
+    is just an alias for the {@link Level#DEBUG DEBUG} level.
+
+    <p>This method first checks if this category is <code>DEBUG</code>
+    enabled by comparing the level of this category with the {@link
+    Level#DEBUG DEBUG} level. If this category is
+    <code>DEBUG</code> enabled, then it converts the message object
+    (passed as parameter) to a string by invoking the appropriate
+    {@link ObjectRenderer}. It then proceeds to call all the
+    registered appenders in this category and also higher in the
+    hierarchy depending on the value of the additivity flag.
+
+    <p><b>WARNING</b> Note that passing a {@link Throwable} to this
+    method will print the name of the <code>Throwable</code> but no
+    stack trace. To print a stack trace use the {@link #debug(Object,
+    Throwable)} form instead.
+    
+    @param message the message object to log. */
+  public
+  void fine(Object message) {
+    if(repository.isDisabled(Level.DEBUG_INT)) 
+      return;    
+    if(Level.DEBUG.isGreaterOrEqual(this.getChainedLevel())) {
+      forcedLog(FQCN, Level.DEBUG, message, null);
+    }
+  }
+  
+
+  /**  
+   Log a message object with the <code>FINE</code> level including
+   the stack trace of the {@link Throwable} <code>t</code> passed as
+   parameter.
+   
+   <p>See {@link #fine(Object)} form for more detailed information.
+   
+   @param message the message object to log.
+   @param t the exception to log, including its stack trace.  */  
+  public
+  void fine(Object message, Throwable t) {
+    if(repository.isDisabled(Level.DEBUG_INT)) 
+      return;
+    if(Level.DEBUG.isGreaterOrEqual(this.getChainedLevel()))
+      forcedLog(FQCN, Level.FINE, message, t);    
   }
 
   /**
