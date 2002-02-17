5639634f5d71373f0bec2814dd62ee4507d76607
Ceki Gulcu <ceki@apache.org>
2002-02-17 21:50:58 +0000

    - Incorporated the performance enhancements to ISO8601DateFormat and    AbsoluteTimeDateFormat classes submitted by Andrew Vajoczki.
    PR:
    Obtained from:
    Submitted by: Andrew Vajoczki
    Reviewed by: Ceki
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@309405 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/helpers/ISO8601DateFormat.java
+++ src/java/org/apache/log4j/helpers/ISO8601DateFormat.java
@@ -28,7 +28,8 @@ import java.text.DateFormatSymbols;
    information on this format.
    
    @author Ceki G&uuml;lc&uuml;
-   
+   @author Andrew Vajoczki 
+
    @since 0.7.5
 */
 public class ISO8601DateFormat extends AbsoluteTimeDateFormat {
@@ -41,6 +42,9 @@ public class ISO8601DateFormat extends AbsoluteTimeDateFormat {
   ISO8601DateFormat(TimeZone timeZone) {
     super(timeZone);
   }
+
+  static private long   lastTime;
+  static private char[] lastTimeString = new char[20];
   
   /**
      Appends a date in the format "YYYY-mm-dd HH:mm:ss,SSS"
@@ -52,37 +56,85 @@ public class ISO8601DateFormat extends AbsoluteTimeDateFormat {
   StringBuffer format(Date date, StringBuffer sbuf,
 		      FieldPosition fieldPosition) {
 
-    calendar.setTime(date);      
+    long now = date.getTime();
+    int millis = (int)(now % 1000);
 
-    int year =  calendar.get(Calendar.YEAR);
-    sbuf.append(year);
+    if ((now - millis) != lastTime) {
+      // We reach this point at most once per second
+      // across all threads instead of each time format()
+      // is called. This saves considerable CPU time.
+      
+      calendar.setTime(date);      
+      
+      int start = sbuf.length();
+      
+      int year =  calendar.get(Calendar.YEAR);
+      sbuf.append(year);
+      
+      String month;
+      switch(calendar.get(Calendar.MONTH)) {
+      case Calendar.JANUARY: month = "-01-"; break;      
+      case Calendar.FEBRUARY: month = "-02-";  break;     
+      case Calendar.MARCH: month = "-03-"; break;      
+      case Calendar.APRIL: month = "-04-";  break;     
+      case Calendar.MAY: month = "-05-"; break;      
+      case Calendar.JUNE: month = "-06-";  break;     
+      case Calendar.JULY: month = "-07-"; break;      
+      case Calendar.AUGUST: month = "-08-";  break;     
+      case Calendar.SEPTEMBER: month = "-09-"; break;      
+      case Calendar.OCTOBER: month = "-10-"; break;      
+      case Calendar.NOVEMBER: month = "-11-";  break;           
+      case Calendar.DECEMBER: month = "-12-";  break;
+      default: month = "-NA-"; break;
+      }
+      sbuf.append(month);
+      
+      int day = calendar.get(Calendar.DAY_OF_MONTH);
+      if(day < 10) 
+	sbuf.append('0');
+      sbuf.append(day);
+      
+      sbuf.append(' ');    
+      
+      int hour = calendar.get(Calendar.HOUR_OF_DAY);
+      if(hour < 10) {
+	sbuf.append('0');
+      }
+      sbuf.append(hour);
+      sbuf.append(':');
+      
+      int mins = calendar.get(Calendar.MINUTE);
+      if(mins < 10) {
+	sbuf.append('0');
+      }
+      sbuf.append(mins);
+      sbuf.append(':');
+      
+      int secs = calendar.get(Calendar.SECOND);
+      if(secs < 10) {
+	sbuf.append('0');
+      }
+      sbuf.append(secs);
 
-    String month;
-    switch(calendar.get(Calendar.MONTH)) {
-    case Calendar.JANUARY: month = "-01-"; break;      
-    case Calendar.FEBRUARY: month = "-02-";  break;     
-    case Calendar.MARCH: month = "-03-"; break;      
-    case Calendar.APRIL: month = "-04-";  break;     
-    case Calendar.MAY: month = "-05-"; break;      
-    case Calendar.JUNE: month = "-06-";  break;     
-    case Calendar.JULY: month = "-07-"; break;      
-    case Calendar.AUGUST: month = "-08-";  break;     
-    case Calendar.SEPTEMBER: month = "-09-"; break;      
-    case Calendar.OCTOBER: month = "-10-"; break;      
-    case Calendar.NOVEMBER: month = "-11-";  break;           
-    case Calendar.DECEMBER: month = "-12-";  break;
-    default: month = "-NA-"; break;
-    }
-    sbuf.append(month);
-
-    int day = calendar.get(Calendar.DAY_OF_MONTH);
-    if(day < 10) 
-      sbuf.append('0');
-    sbuf.append(day);
-
-    sbuf.append(' ');    
-    return super.format(date, sbuf, fieldPosition);
-  }
+      sbuf.append(',');      
+      
+      // store the time string for next time to avoid recomputation
+      sbuf.getChars(start, sbuf.length(), lastTimeString, 0);
+      lastTime = now - millis;
+    } 
+    else {
+      sbuf.append(lastTimeString);
+    } 
+    
+    
+    if (millis < 100)
+      sbuf.append('0'); 
+    if (millis < 10)
+      sbuf.append('0'); 
+    
+    sbuf.append(millis);
+    return sbuf;
+  } 
 
   /**
     This method does not do anything but return <code>null</code>.
@@ -92,3 +144,4 @@ public class ISO8601DateFormat extends AbsoluteTimeDateFormat {
     return null;
   }  
 }
+
