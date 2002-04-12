fbc83ec0defc11d654ddc5d4bfb39d0c42483adb
Ceki Gulcu <ceki@apache.org>
2002-04-12 12:59:38 +0000

    Removing superflous make/ directory
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@309465 13f79535-47bb-0310-9956-ffa450edef68

--- make/exercise/echo/Echo.java
+++
@@ -1,9 +0,0 @@
-
-package exercise.echo;
-
-import java.rmi.Remote; 
-import java.rmi.RemoteException; 
-
-public interface Echo extends Remote {
-  String reverse(String s) throws RemoteException;
-}
