87293e40b7b708b7e020f1d15b7ee01f3a247851
Ceki Gulcu <ceki@apache.org>
2002-04-25 14:15:29 +0000

    MyPatternLayout.java and MyPatternParser.java were copied from examples/ to tests/src/java/org/apache/log4j/ in order to remove the dependency on examples/.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@309491 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/src/java/org/apache/log4j/MyPatternLayout.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.  */
+
+package org.apache.log4j;
+import org.apache.log4j.helpers.PatternParser;
+
+/**
+
+  Example showing how to extend PatternLayout to recognize additional
+  conversion characters.  
+  
+  <p>In this case MyPatternLayout recognizes %# conversion pattern. It
+  outputs the value of an internal counter which is also incremented
+  at each call.
+
+  <p>See <a href=doc-files/MyPatternLayout.java><b>source</b></a> code
+  for more details.
+
+  @see MyPatternParser
+  @see org.apache.log4j.PatternLayout
+  @author Anders Kristensen 
+*/
+public class MyPatternLayout extends PatternLayout {
+  public
+  MyPatternLayout() {
+    this(DEFAULT_CONVERSION_PATTERN);
+  }
+
+  public
+  MyPatternLayout(String pattern) {
+    super(pattern);
+  }
+    
+  public
+  PatternParser createPatternParser(String pattern) {
+    return new MyPatternParser(
+      pattern == null ? DEFAULT_CONVERSION_PATTERN : pattern);
+  }
+  
+  public
+  static void main(String[] args) {
+    Layout layout = new MyPatternLayout("[counter=%.10#] - %m%n");
+    Category cat = Category.getInstance("some.cat");
+    cat.addAppender(new ConsoleAppender(layout, ConsoleAppender.SYSTEM_OUT));
+    cat.debug("Hello, log");
+    cat.info("Hello again...");    
+  }
+}
---
+++ tests/src/java/org/apache/log4j/MyPatternParser.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.  */
+
+package org.apache.log4j;
+
+import org.apache.log4j.helpers.FormattingInfo;
+import org.apache.log4j.helpers.PatternConverter;
+import org.apache.log4j.helpers.PatternParser;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+  Example showing how to extend PatternParser to recognize additional
+  conversion characters.  The examples shows that minimum and maximum
+  width and alignment settings apply for "extension" conversion
+  characters just as they do for PatternLayout recognized characters.
+  
+  <p>In this case MyPatternParser recognizes %# and outputs the value
+  of an internal counter which is also incremented at each call.
+
+  See <a href=doc-files/MyPatternParser.java><b>source</b></a> code
+   for more details.
+  
+  @see org.apache.log4j.examples.MyPatternLayout
+  @see org.apache.log4j.helpers.PatternParser
+  @see org.apache.log4j.PatternLayout
+
+  @author Anders Kristensen 
+*/
+public class MyPatternParser extends PatternParser {
+
+  int counter = 0;
+
+  public
+  MyPatternParser(String pattern) {
+    super(pattern);
+  }
+    
+  public
+  void finalizeConverter(char c) {
+    if (c == '#') {
+      addConverter(new UserDirPatternConverter(formattingInfo));
+      currentLiteral.setLength(0);
+    } else {
+      super.finalizeConverter(c);
+    }
+  }
+  
+  private class UserDirPatternConverter extends PatternConverter {
+    UserDirPatternConverter(FormattingInfo formattingInfo) {
+      super(formattingInfo);
+    }
+
+    public
+    String convert(LoggingEvent event) {
+      return String.valueOf(++counter);
+    }
+  }  
+}
