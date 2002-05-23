045c3cee6ce6dfe5b027436c4ec0f240f1b8ae50
Ceki Gulcu <ceki@apache.org>
2002-05-23 08:05:39 +0000

    A version of NullAppender that actually conpiles.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/branches/v1_2-branch@309545 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/varia/NullAppender.java
+++ src/java/org/apache/log4j/varia/NullAppender.java
@@ -5,9 +5,14 @@
  * License version 1.1, a copy of which has been included with this
  * distribution in the LICENSE.txt file.  */
 
-package org.apache.log4j.performance;
+package org.apache.log4j.varia;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.OptionHandler;
 import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ErrorHandler;
+import org.apache.log4j.spi.Filter;
 import org.apache.log4j.AppenderSkeleton;
 
 /**
@@ -15,13 +20,23 @@ import org.apache.log4j.AppenderSkeleton;
   * device.  
   * @author Ceki G&uuml;lc&uml;
   */
-public class NullAppender implements OptionHandler {
+public class NullAppender extends AppenderSkeleton {
 
   private static NullAppender instance = new NullAppender();
 
   public NullAppender() {
   }
 
+  /** 
+   * There are no options to acticate.
+   * */
+  public void activateOptions() {
+  }
+
+  /**
+   * Whenever you can, use this method to retreive an instance instead
+   * of instantiating a new one with <code>new</code>.
+   * */
   public NullAppender getInstance() {
     return instance;
   }
@@ -29,13 +44,21 @@ public class NullAppender implements OptionHandler {
   public void close() {
   }
 
+  /**
+   * Does not do anything. 
+   * */
   public void doAppend(LoggingEvent event) {
   }
 
   /**
+   * Does not do anything. 
+   * */
+  protected void append(LoggingEvent event) {
+  }
+
+  /**
     * NullAppenders do not need a layout.  
-    *
-    */
+    * */
   public boolean requiresLayout() {
     return false;
   }
