43b5c22ac873099015df4490e9ae6d01fc0986c7
Mark Dwayne Womack <mwomack@apache.org>
2002-11-22 04:49:13 +0000

    Deprecated methods related to HierarchyEventListener. Added methods to support LoggerRepositoryEventListener and LoggerEventListener. Made public methods to fire events related to LoggerRepositoryEventListenter and LoggerEventListener.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@309673 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/spi/LoggerRepository.java
+++ src/java/org/apache/log4j/spi/LoggerRepository.java
@@ -26,9 +26,34 @@ public interface LoggerRepository {
 
   /**
      Add a {@link HierarchyEventListener} event to the repository.
+     @deprecated As of v1.3, use {@link #addLoggerRepositoryEventListener}
+     and {@link addLoggerEventListener} methods instead.
   */
-  public
-  void addHierarchyEventListener(HierarchyEventListener listener);
+  public void addHierarchyEventListener(HierarchyEventListener listener);
+
+  /**
+    Add a {@link LoggerRepositoryEventListener} to the repository. The 
+    listener will be called when repository events occur. 
+    @since 1.3*/
+  public void addLoggerRepositoryEventListener(
+    LoggerRepositoryEventListener listener);
+    
+  /**
+    Remove a {@link LoggerRepositoryEventListener} from the repository.
+    @since 1.3*/
+  public void removeLoggerRepositoryEventListener(
+    LoggerRepositoryEventListener listener);
+
+  /**
+    Add a {@link LoggerEventListener} to the repository. The  listener 
+    will be called when repository events occur.
+    @since 1.3*/
+  public void addLoggerEventListener(LoggerEventListener listener);
+    
+  /**
+    Remove a {@link LoggerEventListener} from the repository.
+    @since 1.3*/
+  public void removeLoggerEventListener(LoggerEventListener listener);
 
   /**
      Is the repository disabled for a given level? The answer depends
@@ -40,56 +65,80 @@ public interface LoggerRepository {
      Set the repository-wide threshold. All logging requests below the
      threshold are immediately dropped. By default, the threshold is
      set to <code>Level.ALL</code> which has the lowest possible rank.  */
-  public
-  void setThreshold(Level level);
+  public void setThreshold(Level level);
 
   /**
       Another form of {@link #setThreshold(Level)} accepting a string
       parameter instead of a <code>Level</code>. */
-  public
-  void setThreshold(String val);
+  public void setThreshold(String val);
 
-  public
-  void emitNoAppenderWarning(Category cat);
+  public void emitNoAppenderWarning(Category cat);
 
   /**
      Get the repository-wide threshold. See {@link
      #setThreshold(Level)} for an explanation. */
-  public
-  Level getThreshold();
+  public Level getThreshold();
 
-  public
-  Logger getLogger(String name);
+  public Logger getLogger(String name);
 
-  public
-  Logger getLogger(String name, LoggerFactory factory);
+  public Logger getLogger(String name, LoggerFactory factory);
 
-  public
-  Logger getRootLogger();
+  public Logger getRootLogger();
 
-  public
-  abstract
-  Logger exists(String name);
+  public abstract Logger exists(String name);
 
-  public
-  abstract
-  void shutdown();
+  public abstract void shutdown();
 
-  public
-  Enumeration getCurrentLoggers();
+  public Enumeration getCurrentLoggers();
 
   /**
      @deprecated Please use {@link #getCurrentLoggers} instead.  */
-  public
-  Enumeration getCurrentCategories();
+  public Enumeration getCurrentCategories();
 
+  public abstract void resetConfiguration();
 
-  public
-  abstract
-  void fireAddAppenderEvent(Category logger, Appender appender);
+  /**
+    @deprecated As of 1.3, please use fireAddAppenderEvent(Logger,Appender).*/
+  public abstract void fireAddAppenderEvent(Category logger,
+      Appender appender);
 
-  public
-  abstract
-  void resetConfiguration();
+  /**
+    Requests that a appender added event be sent to any registered
+    {@link LoggerEventListener}.
+    @param logger The logger to which the appender was added. 
+    @param appender The appender added to the logger.
+    @since 1.3*/
+  public abstract void fireAddAppenderEvent(Logger logger,
+    Appender appender);
 
+  /**
+    Requests that a appender removed event be sent to any registered
+    {@link LoggerEventListener}.
+    @param logger The logger from which the appender was removed. 
+    @param appender The appender removed from the logger.
+    @since 1.3*/
+  public abstract void fireRemoveAppenderEvent(Logger logger,
+    Appender appender);
+
+  /**
+    Requests that an all appenders removed event be sent to any registered
+    {@link LoggerEventListener}. 
+    @param logger The logger from which all appenders were removed.
+    @since 1.3*/
+  public abstract void fireRemoveAllAppendersEvent(Logger logger);
+
+  /**
+    Requests that a level changed event be sent to any registered
+    {@link LoggerEventListener}. 
+    @param logger The logger which changed levels.
+    @since 1.3*/
+  public abstract void fireLevelChangedEvent(Logger logger);
+
+  /**
+    Requests that a configuration changed event be sent to any registered
+    {@link LoggerRepositoryEventListener}. 
+    @param logger The logger which changed levels.
+    @since 1.3*/
+  public abstract void fireConfigurationChangedEvent();
+   
 }
