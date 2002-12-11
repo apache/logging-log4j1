4ce45de439067d55de04415d6920cd24ed701fcf
Mark Dwayne Womack <mwomack@apache.org>
2002-12-11 07:25:14 +0000

    Moved from o.a.log4j to o.a.log4j.plugins package. Fixed bug in PluginRegistry where stopped plugin was not removed from plugin map.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@309690 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/plugins/Plugin.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.  */
+
+package org.apache.log4j.plugins;
+
+import org.apache.log4j.spi.LoggerRepository;
+import org.apache.log4j.spi.OptionHandler;
+
+/**
+  Defines the required interface for all Plugin objects.
+  
+  <p>A plugin implements some specific functionality to extend
+  the log4j framework.  Each plugin is associated with a specific
+  LoggerRepository, which it then uses/acts upon. If no
+  repository is specified, the default repository from
+  LogManager.getLoggerRepository() is used.
+  
+  <p>Examples of plugins are Receiver and Watchdog. Receiver plugins
+  allow for remote logging events to be received and processed by
+  a repository as if the event was sent locally. Watchdog plugins
+  allow for a repository to be reconfigured when some "watched"
+  configuration data changes.
+    
+  @author Mark Womack
+  @author Nicko Cadell
+  @since 1.3
+*/
+public interface Plugin extends OptionHandler {
+    
+  /**
+    Gets the name of the plugin. */
+  public String getName();
+  
+  /**
+    Sets the name of the plugin. */
+  public void setName(String _name);
+      
+  /**
+    Gets the logger repository for this plugin. If not
+    explicity set, returns the value of 
+    LogManager.getLoggerRepository(). */
+  public LoggerRepository getLoggerRepository();
+
+  /**
+    Sets the logger repository used by this plugin. This
+    repository will be used by the plugin functionality. */
+  public void setLoggerRepository(LoggerRepository _repository);
+      
+  /**
+    True if the plugin is active and running. */
+  public boolean isActive();
+    
+  /**
+    Call when the plugin should be stopped. */
+  public void shutdown();
+}
---
+++ src/java/org/apache/log4j/plugins/PluginSkeleton.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.  */
+
+package org.apache.log4j.plugins;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.spi.LoggerRepository;
+
+/**
+  A convienent abstract class for receiver classes that implements
+  the basic methods of the Plugin interface. Subclasses are required
+  to implement the isActive(), activateOptions(), and shutdown()
+  methods.
+  
+  <p>Developers are not required to subclass PluginSkeleton to
+  develop their own plugins (they are only required to implement the
+  Plugin interface), but it provides a convienent base class to start
+  from.
+  
+  Contributors: Nicko Cadell
+  
+  @author Mark Womack
+  @since 1.3
+*/
+public abstract class PluginSkeleton implements Plugin {
+  protected String name = "";
+  protected LoggerRepository repository;
+  
+  /**
+    Gets the name of the plugin. */
+  public String getName() {
+    return name;
+  }
+  
+  /**
+    Sets the name of the plugin. */
+  public void setName(String _name) {
+    name = _name;
+  }
+      
+  /**
+    Gets the logger repository for this plugin. If not
+    explicity set, returns the value of 
+    LogManager.getLoggerRepository(). */
+  public LoggerRepository getLoggerRepository() {
+    if (repository != null) {
+      return repository;
+    } else {
+      return LogManager.getLoggerRepository();
+    }
+  }
+
+  /**
+    Sets the logger repository used by this plugin. This
+    repository will be used by the plugin functionality. */
+  public void setLoggerRepository(LoggerRepository _repository) {
+    repository = _repository;
+  }
+}
+
