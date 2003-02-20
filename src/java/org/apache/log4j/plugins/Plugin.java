9aee017c9f90b704541947c5eae0e11885d8935a
Mark Dwayne Womack <mwomack@apache.org>
2003-02-20 07:18:32 +0000

    Jalopy-ized and checkstyle-d versions of the plugin files.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@309751 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/plugins/Plugin.java
+++ src/java/org/apache/log4j/plugins/Plugin.java
@@ -4,64 +4,63 @@
  * This software is published under the terms of the Apache Software
  * License version 1.1, a copy of which has been included with this
  * distribution in the LICENSE.txt file.  */
-
 package org.apache.log4j.plugins;
 
 import org.apache.log4j.spi.LoggerRepository;
 import org.apache.log4j.spi.OptionHandler;
 
+
 /**
   Defines the required interface for all Plugin objects.
-  
+
   <p>A plugin implements some specific functionality to extend
   the log4j framework.  Each plugin is associated with a specific
   LoggerRepository, which it then uses/acts upon.  The functionality
   of the plugin is up to the developer.
-  
+
   <p>Examples of plugins are Receiver and Watchdog. Receiver plugins
   allow for remote logging events to be received and processed by
   a repository as if the event was sent locally. Watchdog plugins
   allow for a repository to be reconfigured when some "watched"
   configuration data changes.
-    
+
   @author Mark Womack
   @author Nicko Cadell
   @since 1.3
 */
 public interface Plugin extends OptionHandler {
-    
   /**
-    Gets the name of the plugin. 
-    
+    Gets the name of the plugin.
+
     @return String the name of the plugin. */
   public String getName();
-  
+
   /**
-    Sets the name of the plugin. 
-    
+    Sets the name of the plugin.
+
     @param name the name of the plugin. */
   public void setName(String name);
-      
+
   /**
-    Gets the logger repository for this plugin. 
-    
+    Gets the logger repository for this plugin.
+
     @return LoggerRepository the logger repository this plugin is
       attached to. */
   public LoggerRepository getLoggerRepository();
 
   /**
     Sets the logger repository used by this plugin. This
-    repository will be used by the plugin functionality. 
-    
+    repository will be used by the plugin functionality.
+
     @param repository the logger repository to attach this plugin to. */
   public void setLoggerRepository(LoggerRepository repository);
-      
+
   /**
-    True if the plugin is active and running. 
-    
+    True if the plugin is active and running.
+
     @return boolean true if the plugin is currently active. */
   public boolean isActive();
-    
+
   /**
     Call when the plugin should be stopped. */
   public void shutdown();
--- src/java/org/apache/log4j/plugins/Receiver.java
+++ src/java/org/apache/log4j/plugins/Receiver.java
@@ -4,41 +4,41 @@
  * This software is published under the terms of the Apache Software
  * License version 1.1, a copy of which has been included with this
  * distribution in the LICENSE.txt file.  */
-
 package org.apache.log4j.plugins;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
 
+
 /**
   Defines the base class for Receiver plugins.
-  
+
   <p>Just as Appenders send logging events outside of the log4j
   environment (to files, to smtp, to sockets, etc), Receivers bring
   logging events inside the log4j environment.
-  
+
   <p>Receivers are meant to support the receiving of
-  remote logging events from another process. For example, 
-  SocketAppender "appends" a logging event to a socket, configured 
-  for a specific host and port number.  On the receiving side of 
-  the socket can be a SocketReceiver object.  The SocketReceiver 
+  remote logging events from another process. For example,
+  SocketAppender "appends" a logging event to a socket, configured
+  for a specific host and port number.  On the receiving side of
+  the socket can be a SocketReceiver object.  The SocketReceiver
   object receives the logging event, and then "posts" it to the
-  log4j environment (LoggerRepository) on the receiving machine, to 
-  be handled by the configured appenders, etc.  The various 
-  settings in this environment (Logger levels, Appender filters & 
+  log4j environment (LoggerRepository) on the receiving machine, to
+  be handled by the configured appenders, etc.  The various
+  settings in this environment (Logger levels, Appender filters &
   thresholds) are applied to the received logging event.
-  
+
   <p>Receivers can also be used to "import" log messages from other
   logging packages into the log4j environment.
-  
+
   <p>Receivers can be configured to post events to a given
   LoggerRepository.
-  
+
   <p>Subclasses of Receiver must implement the isActive(),
   activateOptions(), and shutdown() methods. The doPost() method
   is provided to standardize the "import" of remote events into
   the repository.
-  
+
   @author Mark Womack
   @author Ceki G&uuml;lc&uuml;
   @since 1.3
@@ -46,19 +46,20 @@ import org.apache.log4j.spi.LoggingEvent;
 public abstract class Receiver extends PluginSkeleton {
   /**
     Posts the logging event to a logger in the configured logger
-    repository. */    
+    repository. 
+    
+    @param event the log event to post to the local log4j environment. */
   public void doPost(LoggingEvent event) {
     // get the "local" logger for this event from the
     // configured repository.
-  	Logger localLogger = 
+    Logger localLogger =
       getLoggerRepository().getLogger(event.getLoggerName());
-  	
-  	// if the logger level is greater or equal to the level
-  	// of the event, use the logger to append the event.
-  	if(event.getLevel().isGreaterOrEqual(localLogger.getEffectiveLevel())) {
+
+    // if the logger level is greater or equal to the level
+    // of the event, use the logger to append the event.
+    if (event.getLevel().isGreaterOrEqual(localLogger.getEffectiveLevel())) {
       // call the loggers appenders to process the event
-  	  localLogger.callAppenders(event);
-  	}
+      localLogger.callAppenders(event);
+    }
   }
 }
-
