9c911d52f3c3db0049d80c8a75e88061766b5818
Ceki Gulcu <ceki@apache.org>
2003-03-03 19:02:05 +0000

    Added missing file.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@309760 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/xml/Log4jEntityResolver.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) The Apache Software Foundation. All rights reserved.
+ *
+ * This software is published under the terms of the Apache Software
+ * License version 1.1, a copy of which has been included with this
+ * distribution in the LICENSE.txt file.  */
+
+package org.apache.log4j.xml;
+
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+
+import org.apache.log4j.helpers.LogLog;
+
+/**
+ * An {@link EntityResolver} specifically designed to return
+ * <code>log4j.dtd</code> which is embedded within the log4j jar
+ * file. 
+ *
+ * @author Paul Austin
+ * */
+public class Log4jEntityResolver implements EntityResolver {
+
+  public InputSource resolveEntity (String publicId, String systemId) {
+    System.err.println("piblicID: ["+publicId+"]");
+    System.err.println("systemId: ["+systemId+"]");
+    if (systemId.endsWith("log4j.dtd")) {
+      Class clazz = getClass();
+      InputStream in = clazz.getResourceAsStream("/org/apache/log4j/xml/log4j.dtd");
+      if (in == null) {
+	LogLog.error("Could not find [log4j.dtd]. Used [" + clazz.getClassLoader() 
+		     + "] class loader in the search.");
+	return null;
+      } else {
+	return new InputSource(in);
+      }
+    } else {
+      return null;
+    }
+  }
+}
