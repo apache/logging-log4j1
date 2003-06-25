636e6ccaa269aa7c4b159d57b0be593665671043
Ceki Gulcu <ceki@apache.org>
2003-06-25 15:13:20 +0000

    Corrected the package declaration.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@309868 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/java/org/apache/log4j/xml/XMLDecoderTest.java
+++ tests/src/java/org/apache/log4j/xml/XMLDecoderTest.java
@@ -4,7 +4,7 @@
  * To change the template for this generated file go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-package org.apache.log4j.net;
+package org.apache.log4j.xml;
 
 import java.io.File;
 import java.util.List;
