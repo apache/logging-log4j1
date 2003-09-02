9008122dc85d4a5562b02339bf74db846b65ae1b
Paul Jeffrey Smith <psmith@apache.org>
2003-09-02 06:26:03 +0000

    A new interface to represent those parties that a interested in receiving batches of events by the ChainsawAppenderHandler
    Implementations can be interested in batches from all sources
    or those only with a specific identifier (remote location, say).
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@309947 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/chainsaw/EventBatchListener.java
@@ -0,0 +1,80 @@
+/*
+ * ============================================================================
+ *                   The Apache Software License, Version 1.1
+ * ============================================================================
+ *
+ *    Copyright (C) 1999 The Apache Software Foundation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of  source code must  retain the above copyright  notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include  the following  acknowledgment:  "This product includes  software
+ *    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+ *    Alternately, this  acknowledgment may  appear in the software itself,  if
+ *    and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "log4j" and  "Apache Software Foundation"  must not be used to
+ *    endorse  or promote  products derived  from this  software without  prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ *
+ * 5. Products  derived from this software may not  be called "Apache", nor may
+ *    "Apache" appear  in their name,  without prior written permission  of the
+ *    Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ * APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ * ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ * (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software  consists of voluntary contributions made  by many individuals
+ * on  behalf of the Apache Software  Foundation.  For more  information on the
+ * Apache Software Foundation, please see <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.log4j.chainsaw;
+
+import java.util.EventListener;
+import java.util.List;
+
+
+/**
+ * Implementations of this interface like to be notified
+ * of arriving batches of LoggingEvents, but may only
+ * be interested in those coming from a particular source
+ * @author Paul Smith <psmith@apache.org>
+ *
+ */
+public interface EventBatchListener extends EventListener {
+  /**
+   * Returns the string representing the identifier
+   * that this instance is only interested in, or
+   * null if this instance is interested in ALL events from
+   * any identifier
+   * @return String identifier of the interested source
+   */
+  public String getInterestedIdentifier();
+
+  /**
+   * Implementations receive a list of ChainsawEventBatchEntry instances only if they are interested,
+   * that is, if the source of the eventBatch matches this instances interested identifier
+   * @param eventBatchEntries List of ChainsawEventBatchEntry instances
+   * @param identifier the identifier this list of eventBatchEntrys is associated with
+   */
+  public void receiveEventBatch(String identifier, List eventBatchEntrys);
+}
