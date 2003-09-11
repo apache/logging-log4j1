45d7b4dcda45ec040f9c8545dd85e9158785b979
Ceki Gulcu <ceki@apache.org>
2003-09-11 17:25:24 +0000

    Bug fixes
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310023 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/joran/action/ActionConst.java
+++ src/java/org/apache/joran/action/ActionConst.java
@@ -15,12 +15,14 @@ public abstract class ActionConst {
 	public static final String NAME_ATTRIBUTE = "name";
 	public static final String VALUE_ATTRIBUTE = "value";
 	public static final String CLASS_ATTRIBUTE = "class";
-	
+  public static final String ADDITIVITY_ATTRIBUTE = "additivity";
+
 
 	static final String INHERITED = "INHERITED";
 	static final String NULL = "NULL";
 	static final String EMPTY_STR = "";
-	
+	static final Class[] ONE_STRING_PARAM = new Class[] { String.class };
+
 	public static final String APPENDER_BAG = "APPENDER_BAG";
 	
 
