10b7a5ee7315b0718c2b7a3f71fbdab566b37964
Ceki Gulcu <ceki@apache.org>
2003-09-17 18:03:16 +0000

    All tests are now green.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310061 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/java/org/apache/log4j/rolling/TimeBasedRollingTest.java
+++ tests/src/java/org/apache/log4j/rolling/TimeBasedRollingTest.java
@@ -57,7 +57,6 @@ import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
-import org.apache.log4j.rolling.helpers.Compress;
 import org.apache.log4j.util.Compare;
 
 import java.text.SimpleDateFormat;
