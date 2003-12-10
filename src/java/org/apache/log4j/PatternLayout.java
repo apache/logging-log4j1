9361668efa0ec0cd1c1edbf69f4a00bbf98cf340
Paul Jeffrey Smith <psmith@apache.org>
2003-12-10 02:45:12 +0000

    Added the javadoc describing the Properties pattern in a similar veign to the MDC entry.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310189 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/PatternLayout.java
+++ src/java/org/apache/log4j/PatternLayout.java
@@ -309,6 +309,22 @@ import org.apache.log4j.spi.LoggingEvent;
      </td>
    </tr>
 
+      <tr>
+     <td align=center><b>Y</b></td>
+
+     <td>
+
+     <p>Used to output the Properties associated
+     with the logging event. The <b>Y</b>
+     conversion character can be followed by the key for the
+     map placed between braces, as in <b>%Y{log4japp}</b> where
+     <code>log4japp</code> is the key. The value in the Properties bundle
+     corresponding to the key will be output. If no additional sub-option
+     is specified, then the entire contents of the Properties key value pair set
+     is output using a format {{key1,val1},{key2,val2}}</p>
+     </td>
+   </tr>
+
    <tr>
 
      <td align=center><b>%</b></td>
