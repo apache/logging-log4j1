51b317666d70ab587e598e2c742f78f86a0c1d7c
Paul Jeffrey Smith <psmith@apache.org>
2003-12-19 03:20:31 +0000

    Added a helper package, and added a helper class to assist with editing tables.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310259 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/chainsaw/helper/TableCellEditorFactory.java
@@ -0,0 +1,46 @@
+/*
+ */
+package org.apache.log4j.chainsaw.helper;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
+import javax.swing.table.TableCellEditor;
+
+import org.apache.log4j.Level;
+
+/**
+ * @author Paul Smith <psmith@apache.org>
+ *
+ */
+public class TableCellEditorFactory
+{
+
+  
+  /**
+   * Creates a TableCellEditor to be used for editing boolean values
+   * @return TableCellEditor
+   */ 
+  public static final TableCellEditor createBooleanTableCellEditor() {
+  
+    JComboBox comboBox = new JComboBox(new Boolean[] {Boolean.TRUE, Boolean.FALSE});
+    return new DefaultCellEditor(comboBox);
+    
+  }
+  
+  /**
+   * 
+   */
+  private TableCellEditorFactory()
+  {
+  }
+
+  /**
+   * @return
+   */
+  public static Object createLevelTableCellEditor()
+  {
+    JComboBox comboBox = new JComboBox(new Level[] {Level.DEBUG, Level.INFO, Level.WARN, Level.ERROR, Level.FATAL, Level.OFF, Level.ALL});
+    return new DefaultCellEditor(comboBox); 
+  }
+
+}
