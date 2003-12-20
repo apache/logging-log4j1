71f19eeffa5797d172c3897d0251ac1a97587a7a
Paul Jeffrey Smith <psmith@apache.org>
2003-12-20 04:55:15 +0000

    added the license text and a bit more documentation.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310280 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/chainsaw/messages/MessageCenterAppender.java
+++ src/java/org/apache/log4j/chainsaw/messages/MessageCenterAppender.java
@@ -1,25 +1,81 @@
-package org.apache.log4j.chainsaw.messages;
+/*
+ * ============================================================================
+ *                   The Apache Software License, Version 1.1
+ * ============================================================================
+ *
+ *    Copyright (C) 1999 The Apache Software Foundation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of  source code must  retain the above copyright  notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include  the following  acknowledgment:  "This product includes  software
+ *    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+ *    Alternately, this  acknowledgment may  appear in the software itself,  if
+ *    and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "log4j" and  "Apache Software Foundation"  must not be used to
+ *    endorse  or promote  products derived  from this  software without  prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ *
+ * 5. Products  derived from this software may not  be called "Apache", nor may
+ *    "Apache" appear  in their name,  without prior written permission  of the
+ *    Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ * APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ * ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ * (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software  consists of voluntary contributions made  by many individuals
+ * on  behalf of the Apache Software  Foundation.  For more  information on the
+ * Apache Software Foundation, please see <http://www.apache.org/>.
+ *
+ */
 
-import javax.swing.DefaultListModel;
-import javax.swing.ListModel;
+package org.apache.log4j.chainsaw.messages;
 
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
 
+import javax.swing.DefaultListModel;
+import javax.swing.ListModel;
+
+
 /**
- * 
+ * A very basic appender that takes the events and stores them in to a
+ * ListModel for late retrieval.
+ *
  * @author Paul Smith <psmith@apache.org>
  *
  */
 public final class MessageCenterAppender extends AppenderSkeleton {
-
-
   private final DefaultListModel model = new DefaultListModel();
-  
+
+  /**
+   * Returns a reference to the ListModel that contains all the LoggingEvents
+   * that have been appended to this class.
+   * 
+   * @return
+   */
   public final ListModel getModel() {
     return model;
   }
-  
+
   /* (non-Javadoc)
    * @see org.apache.log4j.AppenderSkeleton#append(org.apache.log4j.spi.LoggingEvent)
    */
@@ -38,5 +94,12 @@ public final class MessageCenterAppender extends AppenderSkeleton {
    */
   public boolean requiresLayout() {
     return false;
+  }
+
+  /**
+   * Removes all the Events from the model
+   */
+  public void clearModel() {
+    model.clear();
   }
 }
