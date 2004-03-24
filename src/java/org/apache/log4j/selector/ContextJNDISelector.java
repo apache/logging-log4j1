2679db75828de8bc79b6c24ca9c873684d886c9a
Ceki Gulcu <ceki@apache.org>
2004-03-24 19:32:54 +0000

    Fixed Jalopy formatting error related to the Apache license.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310385 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/selector/ContextJNDISelector.java
+++ src/java/org/apache/log4j/selector/ContextJNDISelector.java
@@ -1,53 +1,4 @@
 /*
- * ============================================================================
- *                   The Apache Software License, Version 1.1
- * ============================================================================
- *
- *    Copyright (C) 1999 The Apache Software Foundation. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modifica-
- * tion, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of  source code must  retain the above copyright  notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. The end-user documentation included with the redistribution, if any, must
- *    include  the following  acknowledgment:  "This product includes  software
- *    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
- *    Alternately, this  acknowledgment may  appear in the software itself,  if
- *    and wherever such third-party acknowledgments normally appear.
- *
- * 4. The names "log4j" and  "Apache Software Foundation"  must not be used to
- *    endorse  or promote  products derived  from this  software without  prior
- *    written permission. For written permission, please contact
- *    apache@apache.org.
- *
- * 5. Products  derived from this software may not  be called "Apache", nor may
- *    "Apache" appear  in their name,  without prior written permission  of the
- *    Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
- * APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
- * DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
- * ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
- * (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * This software  consists of voluntary contributions made  by many individuals
- * on  behalf of the Apache Software  Foundation.  For more  information on the
- * Apache Software Foundation, please see <http://www.apache.org/>.
- *
- */
-
-/*
  * Copyright 1999,2004 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -62,22 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.log4j.selector;
 
 import org.apache.log4j.Hierarchy;
 import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
 import org.apache.log4j.helpers.Constants;
 import org.apache.log4j.helpers.IntializationUtil;
 import org.apache.log4j.helpers.Loader;
-import org.apache.log4j.helpers.LogLog;
-import org.apache.log4j.helpers.OptionConverter;
+
 import org.apache.log4j.spi.LoggerRepository;
 import org.apache.log4j.spi.RepositorySelector;
 import org.apache.log4j.spi.RootCategory;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -237,7 +185,7 @@ public class ContextJNDISelector implements RepositorySelector {
 
         // Use automatic configration to configure the default hierarchy
         IntializationUtil.log4jInternalConfiguration(hierarchy);
-        
+
         String configResourceStr = lookup(ctx, JNDI_CONFIGURATION_RESOURCE);
         String configuratorClassName = lookup(ctx, JNDI_CONFIGURATOR_CLASS);
 
@@ -254,6 +202,7 @@ public class ContextJNDISelector implements RepositorySelector {
         IntializationUtil.initialConfiguration(
           hierarchy, configResourceStr, configuratorClassName);
       }
+
       return hierarchy;
     }
   }
