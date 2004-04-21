3f9c9d000bf163d239d70b7a7e8f3005ab95ee5a
Ceki Gulcu <ceki@apache.org>
2004-04-21 09:45:10 +0000

    Added a new JUnit filter in XLoggerTestCase.common method in order to solve execution problems on a particular platform running Gump.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310460 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/java/org/apache/log4j/customLogger/XLoggerTestCase.java
+++ tests/src/java/org/apache/log4j/customLogger/XLoggerTestCase.java
@@ -1,31 +1,40 @@
 /*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
+ * Copyright 1999,2004 The Apache Software Foundation.
  *
- * This software is published under the terms of the Apache Software
- * License version 1.1, a copy of which has been included with this
- * distribution in the LICENSE.txt file.  */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.apache.log4j.customLogger;
 
-import org.apache.log4j.xml.DOMConfigurator;
-import org.apache.log4j.Logger;
-import org.apache.log4j.util.*;
-
+import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import junit.framework.Test;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.util.*;
+import org.apache.log4j.xml.DOMConfigurator;
+
 
 /**
    Tests handling of custom loggers.
-   
+
    @author Ceki G&uuml;lc&uuml;
 */
 public class XLoggerTestCase extends TestCase {
-
   static String FILTERED = "output/filtered";
   static XLogger logger = (XLogger) XLogger.getLogger(XLoggerTestCase.class);
 
-  public XLoggerTestCase(String name){
+  public XLoggerTestCase(String name) {
     super(name);
   }
 
@@ -33,28 +42,36 @@ public class XLoggerTestCase extends TestCase {
     logger.getLoggerRepository().resetConfiguration();
   }
 
-  public void test1()  throws Exception  { common(1); }
-  public void test2()  throws Exception  { common(2); }
+  public void test1() throws Exception {
+    common(1);
+  }
+
+  public void test2() throws Exception {
+    common(2);
+  }
 
   void common(int number) throws Exception {
-    DOMConfigurator.configure("input/xml/customLogger"+number+".xml");
+    DOMConfigurator.configure("input/xml/customLogger" + number + ".xml");
 
     int i = -1;
     Logger root = Logger.getRootLogger();
 
     logger.trace("Message " + ++i);
     logger.debug("Message " + ++i);
-    logger.warn ("Message " + ++i);
+    logger.warn("Message " + ++i);
     logger.error("Message " + ++i);
     logger.fatal("Message " + ++i);
+
     Exception e = new Exception("Just testing");
     logger.debug("Message " + ++i, e);
 
-    Transformer.transform("output/temp", FILTERED, new Filter[] {
-                                                 new LineNumberFilter(),
-                                                 new SunReflectFilter()});
-    assertTrue(Compare.compare(FILTERED, "witness/customLogger."+number));
-
+    Transformer.transform(
+      "output/temp", FILTERED,
+      new Filter[] {
+        new LineNumberFilter(), new SunReflectFilter(),
+        new JunitTestRunnerFilter()
+      });
+    assertTrue(Compare.compare(FILTERED, "witness/customLogger." + number));
   }
 
   public static Test suite() {
