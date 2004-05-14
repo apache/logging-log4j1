578d6e61f9fce2058539aa7fe1a6e9e7cc0c1bd5
Paul Jeffrey Smith <psmith@apache.org>
2004-05-14 03:19:57 +0000

    This class really just tracks a version string, which is hard coded, but might eventually be Ant'fied to have an incremental build version in it or something.
    For now, we should remember to update this number, and describe the changes between
    releases in the release-notes.html file.
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310531 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/chainsaw/version/VersionManager.java
@@ -0,0 +1,30 @@
+package org.apache.log4j.chainsaw.version;
+
+
+/**
+ * @author psmith
+ *
+ */
+public class VersionManager
+{
+
+    private static final VersionManager instance = new VersionManager();
+    
+    private static final String VERSION_INFO = "1.99.99";
+    
+    public static final VersionManager getInstance() {
+        return instance;
+    }
+    
+    public String getVersionNumber() {
+        return VERSION_INFO;
+    }
+    
+    /**
+     * 
+     */
+    private VersionManager()
+    {
+    }
+
+}
