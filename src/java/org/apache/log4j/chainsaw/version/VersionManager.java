2cb862f3ddb86981de4ebe3bfdf767dca2ab4014
Paul Jeffrey Smith <psmith@apache.org>
2004-05-17 22:26:09 +0000

    made sure an appropriate set of JavaDoc is bundled in with the chainsaw jar, and updated the release notes and version info.
    New signed version has been uploaded to the webstart area.
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310550 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/chainsaw/version/VersionManager.java
+++ src/java/org/apache/log4j/chainsaw/version/VersionManager.java
@@ -10,7 +10,7 @@ public class VersionManager
 
     private static final VersionManager instance = new VersionManager();
     
-    private static final String VERSION_INFO = "1.99.99";
+    private static final String VERSION_INFO = "1.99.99 (18th May 2004)";
     
     public static final VersionManager getInstance() {
         return instance;
