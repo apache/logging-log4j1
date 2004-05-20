ae5a19796d81ba878d522fa09787980f9924636e
Paul Jeffrey Smith <psmith@apache.org>
2004-05-20 23:13:50 +0000

    child directories are now located 'lazily', that is, only when you explicitly select the folder in the tree.  This defers the cost of finding out the children until you need it, making the gui much snappier.
    Changed so that important user messages go to the MessageCenter for
    display.
    
    Currently I am not sure how to determine when child directories become 'stale'
    and I have taken the approach that they 'never' do, and I will add
    a 'refresh' action eventually.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310578 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/chainsaw/vfs/VFSPlugin.java
+++ src/java/org/apache/log4j/chainsaw/vfs/VFSPlugin.java
@@ -1,18 +1,24 @@
 package org.apache.log4j.chainsaw.vfs;
 
 import java.awt.BorderLayout;
+import java.awt.Cursor;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import javax.swing.JSplitPane;
+import javax.swing.JTree;
+import javax.swing.SwingUtilities;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
 import org.apache.commons.vfs.FileObject;
@@ -20,6 +26,8 @@ import org.apache.commons.vfs.FileSystemException;
 import org.apache.commons.vfs.SystemInfo;
 import org.apache.commons.vfs.VFS;
 import org.apache.commons.vfs.impl.StandardFileSystemManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.chainsaw.messages.MessageCenter;
 import org.apache.log4j.chainsaw.plugins.GUIPluginSkeleton;
 import org.apache.log4j.helpers.LogLog;
 
@@ -43,7 +51,8 @@ public class VFSPlugin extends GUIPluginSkeleton {
 	private StandardFileSystemManager fileSystemManager;
     
     private final AOFileTablePopulater aoTablePopulator = new AOFileTablePopulater(fileObjectTable.getTableModel());
-	
+	private final AOChildDirectorScanner aoDirectoryScanner = new AOChildDirectorScanner();
+    
 	private Set supportedSchemes = new HashSet();
 	
 	public VFSPlugin() {
@@ -91,7 +100,9 @@ public class VFSPlugin extends GUIPluginSkeleton {
 			
 			DefaultMutableTreeNode node = this.fileSystemTree.addFileObject("local", fileObject);
             
-            VFSUtils.lookForChildren(this.fileSystemTree.getTree(), node);
+            // TODO replace this with an explicit call to pre-select the firs node in the tree, which
+            // would then trigger the look for children
+            //VFSUtils.lookForChildren(this.fileSystemTree.getTree(), node);
             
 		} catch (Exception e) {
 			LogLog.error("error creating local VFS",e);
@@ -121,9 +132,67 @@ public class VFSPlugin extends GUIPluginSkeleton {
 		add(splitPane, BorderLayout.CENTER);
         
         fileSystemTree.getTree().addTreeSelectionListener(this.aoTablePopulator);
+        fileSystemTree.getTree().addTreeSelectionListener(this.aoDirectoryScanner);
 	}
 	
 	
+    /**
+     * This class will scan a VFSNode for it's children and automatically 
+     * add them as nodes to the tree
+     * 
+     * @author psmith
+     *
+     */
+    private static class AOChildDirectorScanner implements TreeSelectionListener{
+        private Logger USER_MESSAGE_LOGGER = MessageCenter.getInstance().getLogger();
+        
+		/* (non-Javadoc)
+		 * @see javax.swing.event.TreeSelectionListener#valueChanged(javax.swing.event.TreeSelectionEvent)
+		 */
+		public void valueChanged(TreeSelectionEvent e) {
+			final JTree sourceTree = (JTree) e.getSource();
+            final DefaultTreeModel treeModel = (DefaultTreeModel) sourceTree.getModel();
+            
+            TreePath path = e.getPath();
+            
+            if(path == null) {
+             return;   
+            }
+            final DefaultMutableTreeNode lastPathComponent = (DefaultMutableTreeNode) path.getLastPathComponent();
+            
+            // TODO this method will NEVER remove the children and repopulates if the node already has
+            // children, be nice to find out
+            // whether VFS can help determine whether it should 'relook' for changes or something.
+            
+            
+            if(lastPathComponent == null) {
+             return ;   
+            } else if(lastPathComponent.getChildCount()>0) {
+             // already determined Children
+                return;
+            }
+  
+            Object userObject = lastPathComponent.getUserObject();
+            if(userObject == null || !(userObject instanceof VFSNode)) {
+             return;   
+            }
+            final VFSNode vfsNode = (VFSNode) userObject;
+            Thread thread = new Thread(new Runnable() {
+
+				public void run() {
+                    USER_MESSAGE_LOGGER.info("Scanning directory...");
+                    sourceTree.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+                    lastPathComponent.removeAllChildren();
+					new BackgroundChildFileObjectPopulator(lastPathComponent,vfsNode).run();
+					treeModel.reload(lastPathComponent);
+                    sourceTree.setCursor(Cursor.getDefaultCursor());
+				}});
+			thread.setPriority(Thread.MIN_PRIORITY);
+            thread.start();
+		}
+        
+    }
+    
     /**
      * Triggered when the user selects a node in the tree, it automatically populates all the child node 
      * information in the table
@@ -134,6 +203,7 @@ public class VFSPlugin extends GUIPluginSkeleton {
     private static class AOFileTablePopulater implements TreeSelectionListener{
 
         private DirectoryListTableModel tableModel;
+        private Logger USER_MESSAGE_LOGGER = MessageCenter.getInstance().getLogger();
         
         /**
          * @param tableModel
@@ -157,7 +227,6 @@ public class VFSPlugin extends GUIPluginSkeleton {
             
             Object userObject = treeNode.getUserObject();
             if(!(userObject instanceof VFSNode)) {
-             LogLog.error("User Object of selected item is not a VFSNode, ignoring");   
              return;
             }
             final VFSNode vfsNode = (VFSNode) userObject;
@@ -177,7 +246,7 @@ public class VFSPlugin extends GUIPluginSkeleton {
 						}
                         tableModel.setFiles(objects);
                     } catch (FileSystemException ex) {
-                        LogLog.error("Failed to retrieve children for " + vfsNode,ex);
+                        USER_MESSAGE_LOGGER.error("Failed to retrieve children for " + vfsNode,ex);
                         return;
                     }
 				}};
@@ -187,6 +256,80 @@ public class VFSPlugin extends GUIPluginSkeleton {
             thread.start();
             
 		}
-        
+    }
+    
+    /**
+     * Given a tree node, first determine if the user object is a VFSNode, and
+     * if not, ignores the request and returns immediately. Otherwise a new,
+     * low-priority thread is started to go look for any potential children of
+     * the fileObject. As each child is located, an new child TreeNode is added
+     * to the passed in node, and done within the Swing's EventDispatchThread.
+     * 
+     * Child TreeNodes' that are added to this node will have a VFSNode as it's
+     * UserObject.
+     * 
+     * @param node
+     */
+    private static final class BackgroundChildFileObjectPopulator
+            implements
+                Runnable {
+        private final VFSNode vfsNode;
+        private final DefaultMutableTreeNode node;
+        private Logger USER_MESSAGE_LOGGER = MessageCenter.getInstance().getLogger();
+        /**
+         * @param node
+         * @param vfsNode
+         */
+        public BackgroundChildFileObjectPopulator(
+                DefaultMutableTreeNode node, VFSNode vfsNode) {
+            this.node = node;
+            this.vfsNode = vfsNode;
+        }
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.lang.Runnable#run()
+         */
+        public void run() {
+            USER_MESSAGE_LOGGER.debug("Looking for children of node " + vfsNode.getName());
+            // first, lets add a tempopary node that says "Pending..." in it
+            // while we work out what's going on.
+            final DefaultMutableTreeNode pendingNode = new DefaultMutableTreeNode(
+                    "Pending...");
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    node.add(pendingNode);
+                }
+            });
+            try {
+                List children = new ArrayList(Arrays.asList(this.vfsNode
+                        .getFileObject().getChildren()));
+                Collections.sort(children, VFSUtils.FILE_OBJECT_COMPARATOR);
+                USER_MESSAGE_LOGGER.debug("Found " + children.size() + " children");
+                for (Iterator iter = children.iterator(); iter.hasNext();) {
+                    FileObject child = (FileObject) iter.next();
+                    // we only add non-leaf nodes, as the leaf nodes get
+                    // displayed in the table
+                    if (child.getType().hasChildren()) {
+                        final DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(
+                                new VFSNode(child.getName().getBaseName(),
+                                        child));
+                        SwingUtilities.invokeLater(new Runnable() {
+                            public void run() {
+                                node.add(childNode);
+                            }
+                        });
+                    }
+                }
+            } catch (Exception e) {
+                USER_MESSAGE_LOGGER.error("Failed to populate Children", e);
+            } finally {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        node.remove(pendingNode);
+                    }
+                });
+            }
+        }
     }
 }
