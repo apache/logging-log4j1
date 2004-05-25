ac7b53fbb0fa243e73f48bc5d2fc8399c23608d5
Paul Jeffrey Smith <psmith@apache.org>
2004-05-25 01:01:01 +0000

    fixed a bug where the SocketNode was catching a remote socket problem but was not notifying it's listeners correctly.
    The Receiver therefor did not know that it should restart up the Connector
    thread.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310614 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/chainsaw/version/VersionManager.java
+++ src/java/org/apache/log4j/chainsaw/version/VersionManager.java
@@ -10,7 +10,7 @@ public class VersionManager
 
     private static final VersionManager instance = new VersionManager();
     
-    private static final String VERSION_INFO = "1.99.99 (25th May 2004 08:00 GMT+10)";
+    private static final String VERSION_INFO = "1.99.99 (25th May 2004 11:00 GMT+10)";
     
     public static final VersionManager getInstance() {
         return instance;
