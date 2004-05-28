b486772b53ac95249bc68b2cea8a243456fa29c3
Ceki Gulcu <ceki@apache.org>
2004-05-28 16:39:52 +0000

    A bit more descriptive name.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310636 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/pattern/PropertiesPatternConverter.java
+++ src/java/org/apache/log4j/pattern/PropertiesPatternConverter.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,49 +33,49 @@ import java.util.*;
  *@since 1.3
  */
 public class PropertiesPatternConverter extends PatternConverter {
+  
+  String name;
+  
+  public StringBuffer convert(LoggingEvent event) {
 
-    public StringBuffer convert(LoggingEvent event) {
+    StringBuffer buf = new StringBuffer(32);
 
-        StringBuffer buf = new StringBuffer(32);
+    // if there is no additional options, we output every single
+    // Key/Value pair for the MDC in a similar format to Hashtable.toString()
+    if (option == null) {
+      buf.append("{");
 
-        /**
-         * if there is no additional options, we output every single
-         * Key/Value pair for the MDC in a similar format to Hashtable.toString()
-         */
-        if (option == null) {
-            buf.append("{");
+      Set keySet = event.getPropertyKeySet();
 
-            Set keySet = event.getPropertyKeySet();
+      for (Iterator i = keySet.iterator(); i.hasNext();) {
+        Object item = i.next();
+        Object val = event.getProperty(item.toString());
+        buf.append("{").append(item).append(",").append(val).append("}");
+      }
 
-            for (Iterator i = keySet.iterator(); i.hasNext();) {
+      buf.append("}");
 
-                Object item = i.next();
-                Object val = event.getProperty(item.toString());
-                buf.append("{").append(item).append(",").append(val).append(
-                    "}");
-            }
-
-            buf.append("}");
-
-            return buf;
-        }
-
-        /**
-         * otherwise they just want a single key output
-         */
-        Object val = event.getProperty(option);
-
-        if (val != null) {
-
-            return buf.append(val);
-        }
-
-        return buf;
+      return buf;
     }
-    
-    public String getName()
-    {
-        return "Property";
+
+    // otherwise they just want a single key output
+    Object val = event.getProperty(option);
+
+    if (val != null) {
+      return buf.append(val);
     }
-    
+
+    return buf;
+  }
+
+  public String getName() {
+    if(name == null) {
+      if(option != null) {
+        name += "Property{"+option+"}";
+      } else {
+        name = "Properties";
+      }
+    }
+    return name;
+  }
 }
