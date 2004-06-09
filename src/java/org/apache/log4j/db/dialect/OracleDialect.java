bd89e31af25d7f9f4d220c2fcd2d8854521bf6fb
Ceki Gulcu <ceki@apache.org>
2004-06-09 15:22:42 +0000

    Doc improvements
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310675 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/db/dialect/OracleDialect.java
+++ src/java/org/apache/log4j/db/dialect/OracleDialect.java
@@ -17,9 +17,10 @@
 package org.apache.log4j.db.dialect;
 
 /**
- * The Oracle dialect is untested.
+ * The Oracle dialect. Tested successfully on Oracle9i Release 9.2.0.3.0 by 
+ * James Stauffer.
  * 
- * @author ceki
+ * @author Ceki G&uuml;lc&uuml;
  */
 public class OracleDialect implements SQLDialect {
   public static final String SELECT_CURRVAL = "SELECT logging_event_id_seq.currval from dual";
