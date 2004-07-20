b58ff4a89a90a479d9679cf8734e4ee09b03ce43
Paul Jeffrey Smith <psmith@apache.org>
2004-07-20 22:19:15 +0000

    Modified the Odd_Row colour to be slightly more grey, as it sometimes appears white on some screens, and therefore taking away from the niceness of row striping.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310703 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/chainsaw/ChainsawConstants.java
+++ src/java/org/apache/log4j/chainsaw/ChainsawConstants.java
@@ -30,7 +30,7 @@ import java.net.URL;
 public class ChainsawConstants {
   private ChainsawConstants(){}
   
-  public static final Color COLOR_ODD_ROW = new Color(230, 230, 230);
+  public static final Color COLOR_ODD_ROW = new Color(200, 200, 200);
   public static final Color COLOR_EVEN_ROW = new Color(255,255,255);
   
   public static final URL WELCOME_URL = ChainsawConstants.class.getClassLoader().getResource(
