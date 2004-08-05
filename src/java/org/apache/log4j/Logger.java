dabf3bf05b312340b66d281b0148d9b15d8ac7a2
Yoav Shapira <yoavs@apache.org>
2004-08-05 17:45:45 +0000

    Added TRACE level: modified Level class to have TRACE as a native level, between ALL and DEBUG.  Modified Category class to add trace(..) methods like the existing debug(...) methods.  Modified LoggerTestCase and StressCategory tests to do a bit of TRACE-specific testing.
    Inspected but did not modify UGLI classes.  They don't include the FATAL
    level, so I didn't want to inculde TRACE level without further discussion
    which I will start on log4j-dev shortly.
    
    Ensured everything compiles.  Ran unit tests: the relevant unit tests
    pass.  Both the "regression" and "runAll" targets fail on a size-based
    rolling policy test, which was also failing before I made my modifications
    and anyways doesn't touch Level-related stuff, so I don't think it's
    related.
    
    There will probably be an additional coding pass in this area (if not
    multiple) to further integrate TRACE, modify JavaDocs, modify tests, etc.
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310725 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/Logger.java
+++ src/java/org/apache/log4j/Logger.java
@@ -25,58 +25,17 @@ import org.apache.log4j.spi.LoggerFactory;
 
   @since log4j 1.2
 
-  @author Ceki G&uuml;lc&uuml; */
+  @author Ceki G&uuml;lc&uuml; 
+*/
 public class Logger extends Category {
-  protected Logger(String name) {
-    super(name);
-  }
-
-  /**
-    Log a message object with the {@link Level#FINE FINE} level which
-    is just an alias for the {@link Level#DEBUG DEBUG} level.
-
-    <p>This method first checks if this category is <code>DEBUG</code>
-    enabled by comparing the level of this category with the {@link
-    Level#DEBUG DEBUG} level. If this category is
-    <code>DEBUG</code> enabled, then it converts the message object
-    (passed as parameter) to a string by invoking the appropriate
-    {@link org.apache.log4j.or.ObjectRenderer}. It then proceeds to call all the
-    registered appenders in this category and also higher in the
-    hierarchy depending on the value of the additivity flag.
-
-    <p><b>WARNING</b> Note that passing a {@link Throwable} to this
-    method will print the name of the <code>Throwable</code> but no
-    stack trace. To print a stack trace use the {@link #debug(Object,
-    Throwable)} form instead.
-
-    @param message the message object to log. */
-
-  //public
-  //void fine(Object message) {
-  //  if(repository.isDisabled(Level.DEBUG_INT))
-  //    return;
-  //  if(Level.DEBUG.isGreaterOrEqual(this.getChainedLevel())) {
-  //    forcedLog(FQCN, Level.DEBUG, message, null);
-  //  }
-  //}
-
-  /**
-   Log a message object with the <code>FINE</code> level including
-   the stack trace of the {@link Throwable} <code>t</code> passed as
-   parameter.
-
-   <p>See {@link #fine(Object)} form for more detailed information.
-
-   @param message the message object to log.
-   @param t the exception to log, including its stack trace.  */
-
-  //public
-  //void fine(Object message, Throwable t) {
-  //  if(repository.isDisabled(Level.DEBUG_INT))
-  //    return;
-  //  if(Level.DEBUG.isGreaterOrEqual(this.getChainedLevel()))
-  //    forcedLog(FQCN, Level.FINE, message, t);
-  //}
+    /**
+     * Constructor.
+     *
+     * @param name The logger instance name
+     */
+    protected Logger(String name) {
+	super(name);
+    }
 
   /**
      Retrieve a logger by name.
@@ -117,3 +76,5 @@ public class Logger extends Category {
     return LogManager.getLogger(name, factory);
   }
 }
+
+// End of class: Logger.java
