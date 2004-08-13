deea30d8b00740fe8e0aac81b46a669aa8024e70
Ceki Gulcu <ceki@apache.org>
2004-08-13 19:44:04 +0000

    s/VARCHAR2/VARCHAR/
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310734 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/db/dialect/mssql.sql
+++ src/java/org/apache/log4j/db/dialect/mssql.sql
@@ -11,10 +11,10 @@ CREATE TABLE logging_event
   ( 
     sequence_number   DECIMAL(20) NOT NULL, 
     timestamp         DECIMAL(20) NOT NULL, 
-    rendered_message  VARCHAR2(4000) NOT NULL, 
+    rendered_message  VARCHAR(4000) NOT NULL, 
     logger_name       VARCHAR(254) NOT NULL, 
     level_string      VARCHAR(254) NOT NULL, 
-    ndc               VARCHAR2(4000), 
+    ndc               VARCHAR(4000), 
     thread_name       VARCHAR(254), 
     reference_flag    SMALLINT, 
     caller_filename   VARCHAR(254) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE logging_event_property
   ( 
     event_id          INT NOT NULL, 
     mapped_key        VARCHAR(254) NOT NULL, 
-    mapped_value      VARCHAR2(1024), 
+    mapped_value      VARCHAR(1024), 
     PRIMARY KEY(event_id, mapped_key), 
     FOREIGN KEY (event_id) REFERENCES logging_event(event_id) 
   ) 
