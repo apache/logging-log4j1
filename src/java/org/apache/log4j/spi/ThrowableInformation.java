2711a7ac08afbfc4e079e7da0a087767bfaf6926
Ceki Gulcu <ceki@apache.org>
2004-09-10 14:57:24 +0000

    - Log4j will now automatically include stack traces for nested exceptions. Log4j extends the automatic nested exception printing available in JDK 1.4 to nested exceptions accessible through methods named "getRootCause", "getNextException" and "getException", not just "getCause" as in JDK 1.4. [*]
    - Added o.a.l.helpers.PlatformInfo utiliity class which gives information about the current
     platform properties. Some classes were modified to take advantage of the PlatformInfo class.
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310780 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/spi/ThrowableInformation.java
+++ src/java/org/apache/log4j/spi/ThrowableInformation.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,49 +19,87 @@ package org.apache.log4j.spi;
 import java.io.PrintWriter;
 import java.io.Writer;
 
+import java.lang.reflect.Method;
+
 import java.util.Vector;
+
+import org.apache.log4j.helpers.PlatformInfo;
 
 
 /**
-  * ThrowableInformation is log4j's internal representation of
-  * throwables. It essentially consists of a string array, called
-  * 'rep', where the first element, that is rep[0], represents the
-  * string representation of the throwable (i.e. the value you get
-  * when you do throwable.toString()) and subsequent elements
-  * correspond the stack trace with the top most entry of the stack
-  * corresponding to the second entry of the 'rep' array that is
-  * rep[1].
-  * 
-  * Note that ThrowableInformation does not store the throwable it represents. 
-  * 
-  * @author Ceki G&uuml;lc&uuml;
-  *
-  * */
+ * ThrowableInformation is log4j's internal representation of throwables. It
+ * essentially consists of a string array, called 'rep', where the first
+ * element, that is rep[0], represents the string representation of the
+ * throwable (i.e. the value you get when you do throwable.toString()) and
+ * subsequent elements correspond the stack trace with the top most entry of the
+ * stack corresponding to the second entry of the 'rep' array that is rep[1].
+ *
+ * Note that ThrowableInformation does not store the throwable it represents.
+ *
+ * @author Ceki G&uuml;lc&uuml;
+ *
+ */
 public class ThrowableInformation implements java.io.Serializable {
   static final long serialVersionUID = -4748765566864322735L;
+
   //private transient Throwable throwable;
-  
   private String[] rep;
 
   public ThrowableInformation(Throwable throwable) {
-    rep = extractStringRep(throwable);
+    VectorWriter vw = new VectorWriter();
+    extractStringRep(throwable, vw);
+    rep = vw.toStringArray();
   }
 
   public ThrowableInformation(String[] rep) {
     this.rep = rep;
   }
 
- // public Throwable getThrowable() {
-   // return throwable;
+  // public Throwable getThrowable() {
+  // return throwable;
   //}
 
-  public String[] extractStringRep(Throwable throwable) {
-    VectorWriter vw = new VectorWriter();
-    throwable.printStackTrace(vw);
-    String[] extractedRep = vw.toStringArray();
-    return extractedRep;   
+  public void extractStringRep(Throwable t, VectorWriter vw) {
+    t.printStackTrace(vw);
+
+    // Check if the Throwable t has a nested Throwable. If so, invoke
+    // extractStringRep recursively.
+    // Note that the Throwable.getCause was added in JDK 1.4. The printStackTrace
+    // method was modified in JDK 1.4 to handle the nested throwable returned
+    // by Throwable.getCause.
+    try {
+      Class tC = t.getClass();
+      Method[] mA = tC.getMethods();
+      Method nextThrowableMethod = null;
+      for (int i = 0; i < mA.length; i++) {
+        if (("getCause".equals(mA[i].getName()) && !PlatformInfo.isJDK14OrLater())
+            || "getRootCause".equals(mA[i].getName())
+            || "getNextException".equals(mA[i].getName())
+            || "getException".equals(mA[i].getName())) {
+          // check param types
+          Class[] params = mA[i].getParameterTypes();
+          if ((params == null) || (params.length == 0)) {
+            // just found the getter for the nested throwable
+            nextThrowableMethod = mA[i];
+            break; // no need to search further
+          }
+        }
+      }
+
+      if (nextThrowableMethod != null) {
+        // get the nested throwable and log it
+        Throwable nextT =
+          (Throwable) nextThrowableMethod.invoke(t, new Object[0]);
+        if (nextT != null) {
+          vw.print("Root cause follows.");
+          extractStringRep(nextT, vw);
+        }
+      }
+    } catch (Exception e) {
+      // do nothing
+    }
   }
-  
+
   /**
    * Retun a clone of the string representation of the exceptopn (throwable)
    * that this object represents.
@@ -69,52 +107,52 @@ public class ThrowableInformation implements java.io.Serializable {
   public String[] getThrowableStrRep() {
     return (String[]) rep.clone();
   }
-  
+
   public boolean equals(Object o) {
-    if(this == o) {
+    if (this == o) {
       return true;
     }
-    
-    if(!(o instanceof ThrowableInformation)) {
+
+    if (!(o instanceof ThrowableInformation)) {
       return false;
     }
-    
+
     ThrowableInformation r = (ThrowableInformation) o;
-    
-    if(rep == null) {
-      if(r.rep != null) {
+
+    if (rep == null) {
+      if (r.rep != null) {
         return false;
       } else {
         return true;
       }
     }
-    
+
     // at this point we know that both rep and r.rep are non-null.
-    if(rep.length != r.rep.length) {
+    if (rep.length != r.rep.length) {
       return false;
     }
-    
+
     int len = rep.length;
-    for(int i = 0; i < len; i++) {
-      if(!rep[i].equals(r.rep[i])) {
+    for (int i = 0; i < len; i++) {
+      if (!rep[i].equals(r.rep[i])) {
         return false;
       }
     }
-    
+
     return true;
   }
 }
 
 
 /**
-  * VectorWriter is a seemingly trivial implemtantion of PrintWriter.
-  * The throwable instance that we are trying to represnt is asked to
-  * print itself to a VectorWriter.
-  *
-  * By our design choice, r string representation of the throwable
-  * does not contain any line separators. It follows that println()
-  * methods of VectorWriter ignore the 'ln' part.
-  * */
+ * VectorWriter is a seemingly trivial implemtantion of PrintWriter. The
+ * throwable instance that we are trying to represnt is asked to print itself to
+ * a VectorWriter.
+ *
+ * By our design choice, r string representation of the throwable does not
+ * contain any line separators. It follows that println() methods of
+ * VectorWriter ignore the 'ln' part.
+ */
 class VectorWriter extends PrintWriter {
   private Vector v;
 
--- tests/src/java/org/apache/log4j/performance/Loop.java
+++ tests/src/java/org/apache/log4j/performance/Loop.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,19 +23,17 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 import org.apache.log4j.joran.JoranConfigurator;
 
+
 /**
  * Logs in a loop a number of times and measure the elapsed time.
- * 
+ *
  * @author Ceki G&uuml;lc&uuml;
  */
 public class Loop {
-
   static int runLength;
-
-  final static Logger logger = Logger.getLogger(Loop.class);
+  static final Logger logger = Logger.getLogger(Loop.class);
 
   public static void main(String[] args) throws Exception {
-    
     Logger j = Logger.getLogger("org.apache.log4j.joran");
     j.setAdditivity(false);
     j.setLevel(Level.WARN);
@@ -44,31 +42,47 @@ public class Loop {
     a.setName("console");
     a.activateOptions();
     j.addAppender(a);
-    
-    if (args.length == 2)
-      init(args[0], args[1]);
-    else
-      usage("Wrong number of arguments.");
 
+    if (args.length == 2) {
+      init(args[0], args[1]);
+    } else {
+      usage("Wrong number of arguments.");
+    }
+
+    memPrint();
     loop(1000, logger, "Some fix message of medium length.");
+    memPrint();
+
     long res = loop(runLength, logger, "Some fix message of medium length.");
     double average = (res * 1000.0) / runLength;
-    System.out.println("Loop completed in [" + res + "] milliseconds, or ["
-        + average + "] microseconds per log.");
+    System.out.println(
+      "Loop completed in [" + res + "] milliseconds, or [" + average
+      + "] microseconds per log.");
 
+    memPrint();
   }
 
   static void usage(String msg) {
     System.err.println(msg);
-    System.err.println("Usage: java " + Loop.class.getName()
-        + " runLength configFile");
+    System.err.println(
+      "Usage: java " + Loop.class.getName() + " runLength configFile");
     System.err.println("\trunLength (integer) is the length of test loop.");
     System.err.println("\tconfigFile is an XML configuration file");
 
     System.exit(1);
   }
 
-  static void init(String runLengthStr, String configFile) throws Exception {
+  static void memPrint() {
+    Runtime runtime = Runtime.getRuntime();
+    long total = runtime.totalMemory();
+    long free = runtime.freeMemory();
+    long used = total - free;
+    System.out.println(
+      "Total: " + total + ", free: " + free + ", used: " + used);
+  }
+
+  static void init(String runLengthStr, String configFile)
+    throws Exception {
     runLength = Integer.parseInt(runLengthStr);
     JoranConfigurator jc = new JoranConfigurator();
     jc.doConfigure(configFile, LogManager.getLoggerRepository());
@@ -78,6 +92,9 @@ public class Loop {
     long before = System.currentTimeMillis();
     for (int i = 0; i < len; i++) {
       logger.debug(msg);
+      if(i == 1000) {
+        memPrint();
+      }
     }
     return (System.currentTimeMillis() - before);
   }
