2b3348873c80e84110b756eca7f1a086108d2bbe
Ceki Gulcu <ceki@apache.org>
2004-09-10 14:57:45 +0000

    Japoly induced changes
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310781 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/helpers/JNDIUtil.java
+++ src/java/org/apache/log4j/helpers/JNDIUtil.java
@@ -1,30 +1,37 @@
 /*
- * Created on Mar 28, 2004
+ * Copyright 1999,2004 The Apache Software Foundation.
  *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.apache.log4j.helpers;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+
 /**
- * @author ceki
  *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * @author Ceki Gulcu
  */
 public class JNDIUtil {
-  
-  
-  static public Context getInitialContext() throws NamingException {  
+  public static Context getInitialContext() throws NamingException {
     return new InitialContext();
   }
-  
-  static public String lookup(Context ctx, String name) {
-    if(ctx == null) {
+
+  public static String lookup(Context ctx, String name) {
+    if (ctx == null) {
       return null;
     }
     try {
