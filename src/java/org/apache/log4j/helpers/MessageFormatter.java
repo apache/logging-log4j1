a28bc74df5feb74bcceb064bc77d99a6b2f16973
Ceki Gulcu <ceki@apache.org>
2004-11-02 18:16:33 +0000

    Javdoc improvements.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310838 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/helpers/MessageFormatter.java
+++ src/java/org/apache/log4j/helpers/MessageFormatter.java
@@ -18,7 +18,9 @@ package org.apache.log4j.helpers;
 
 
 /**
- *
+ * Formats messages according to very simple rules. 
+ * See {@link #formatt(String, Object)} and 
+ * {@link #formatt(String, Object, Object)} for more details.
  *
  * @author Ceki G&uuml;lc&uuml;
  */
@@ -26,69 +28,99 @@ public class MessageFormatter {
   static final char DELIM_START = '{';
   static final char DELIM_STOP = '}';
 
-  public static String compose(String message, Object param) {
-    int j = message.indexOf(DELIM_START);
-    int len = message.length();
+  /**
+   * Performs single argument substitution for the 'messagePattern' passed as
+   * parameter.
+   * <p>
+   * For example, <code>MessageFormatter.format("Hi {}.", "there");</code> will
+   * return the string "Hi there.".
+   * <p>
+   * The {} pair is called the formatting element. It serves to designate the
+   * location where the argument needs to be inserted within the pattern.
+   * 
+   * @param messagePattern The message pattern which will be parsed and formatted
+   * @param argument The argument to be inserted instead of the formatting element
+   * @return The formatted message
+   */
+  public static String format(String messagePattern, Object argument) {
+    int j = messagePattern.indexOf(DELIM_START);
+    int len = messagePattern.length();
     char escape = 'x';
 
     // if there are no { characters or { is the last character of the messsage
-    // then we just return message
+    // then we just return messagePattern
     if (j == -1 || (j+1 == len)) {
-      return message;
+      return messagePattern;
     } else {
       if(j+1 == len) {
       }
       
-      char delimStop = message.charAt(j + 1);
+      char delimStop = messagePattern.charAt(j + 1);
       if (j > 0) {
-        escape = message.charAt(j - 1);
+        escape = messagePattern.charAt(j - 1);
       }
       if ((delimStop != DELIM_STOP) || (escape == '\\')) {
         // invalid DELIM_START/DELIM_STOP pair or espace character is
         // present
-        return message;
+        return messagePattern;
       } else {
         StringBuffer sbuf = new StringBuffer(len + 20);
-        sbuf.append(message.substring(0, j));
-        sbuf.append(param);
-        sbuf.append(message.substring(j + 2));
+        sbuf.append(messagePattern.substring(0, j));
+        sbuf.append(argument);
+        sbuf.append(messagePattern.substring(j + 2));
         return sbuf.toString();
       }
     }
   }
 
-  public static String compose(String message, Object param1, Object param2) {
+  /**
+   * /**
+   * Performs a two argument substitution for the 'messagePattern' passed as
+   * parameter.
+   * <p>
+   * For example, <code>MessageFormatter.format("Hi {}. My name is {}.", 
+   * "there", "David");</code> will return the string "Hi there. My name is David.".
+   * <p>
+   * The '{}' pair is called a formatting element. It serves to designate the
+   * location where the arguments need to be inserted within the message pattern.
+   * 
+   * @param messagePattern The message pattern which will be parsed and formatted
+   * @param arg1 The first argument to replace the first formatting element
+   * @param arg2 The second argument to replace the second formatting element
+   * @return The formatted message
+   */
+  public static String format(String messagePattern, Object arg1, Object arg2) {
     int i = 0;
-    int len = message.length();
-    int j = message.indexOf(DELIM_START);
+    int len = messagePattern.length();
+    int j = messagePattern.indexOf(DELIM_START);
 
-    StringBuffer sbuf = new StringBuffer(message.length() + 50);
+    StringBuffer sbuf = new StringBuffer(messagePattern.length() + 50);
 
     for (int L = 0; L < 2; L++) {
-      j = message.indexOf(DELIM_START, i);
+      j = messagePattern.indexOf(DELIM_START, i);
 
       if (j == -1 || (j+1 == len)) {
         // no more variables
         if (i == 0) { // this is a simple string
-          return message;
+          return messagePattern;
         } else { // add the tail string which contains no variables and return the result.
-          sbuf.append(message.substring(i, message.length()));
+          sbuf.append(messagePattern.substring(i, messagePattern.length()));
           return sbuf.toString();
         }
       } else {
-        char delimStop = message.charAt(j + 1);
+        char delimStop = messagePattern.charAt(j + 1);
         if ((delimStop != DELIM_STOP)) {
           // invalid DELIM_START/DELIM_STOP pair
-          sbuf.append(message.substring(i, message.length()));
+          sbuf.append(messagePattern.substring(i, messagePattern.length()));
           return sbuf.toString();
         }
-        sbuf.append(message.substring(i, j));
-        sbuf.append((L == 0) ? param1 : param2);
+        sbuf.append(messagePattern.substring(i, j));
+        sbuf.append((L == 0) ? arg1 : arg2);
         i = j + 2;
       }
     }
     // append the characters following the second {} pair.
-    sbuf.append(message.substring(i, message.length()));
+    sbuf.append(messagePattern.substring(i, messagePattern.length()));
     return sbuf.toString();
   }
 }
