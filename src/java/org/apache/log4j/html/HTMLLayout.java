07a081fa6e619ebd5768b8bed717da1a575a6717
Ceki Gulcu <ceki@apache.org>
2004-11-04 12:36:06 +0000

    Updaated CSS link to new location
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310845 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/html/HTMLLayout.java
+++ src/java/org/apache/log4j/html/HTMLLayout.java
@@ -167,7 +167,7 @@ public class HTMLLayout extends Layout {
     sbuf.append(Layout.LINE_SEP);
     sbuf.append("<title>" + title + "</title>");
     sbuf.append(Layout.LINE_SEP);
-    sbuf.append("<LINK REL=StyleSheet HREF=\"http://logging.apache.org/log4j/docs/css/default0.css\" TITLE=\"Basic\">");
+    sbuf.append("<LINK REL=StyleSheet HREF=\"http://logging.apache.org/log4j/docs/css/eventTable-1.0.css\" TITLE=\"Basic\">");
     sbuf.append(Layout.LINE_SEP);
     sbuf.append("</head>");
     sbuf.append(Layout.LINE_SEP);
