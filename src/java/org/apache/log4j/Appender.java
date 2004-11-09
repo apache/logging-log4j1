db68c030cbba13813f330365b221dc95ef5dc8c6
Ceki Gulcu <ceki@apache.org>
2004-11-09 11:30:01 +0000

    - The Appender.requiresLayout method is no longer needed.
      This method was only used by PropertConfigrurator and not
      DOMConfigurator nor JoranConfigurator. It was never strictly necessary
      and case the layout is *optional* for a given appender type, it causes
      unwarranted error messages.
    
      This simplification will make it slightly easier to write appender.
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310851 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/Appender.java
+++ src/java/org/apache/log4j/Appender.java
@@ -113,21 +113,9 @@ public interface Appender {
   public void setName(String name);
 
   /**
-   * Configurators call this method to determine if the appender requires a
-   * layout. If this method returns <code>true</code>, meaning that layout is
-   * required, then the configurator will configure an layout using the
-   * configuration information at its disposal.  If this method returns
-   * <code>false</code>, meaning that a layout is not required, then layout
-   * configuration will be skipped even if there is available layout
-   * configuration information at the disposal of the configurator..
-   * 
-   * <p>
-   * In the rather exceptional case, where the appender implementation admits
-   * a layout but can also work without it, then the appender should return
-   * <code>true</code>.
-   * </p>
+   * This method is no longer used nor needed.
    *
-   * @since 0.8.4
+   * @deprecated Will be removed with no replacement.
    */
   public boolean requiresLayout();
   
