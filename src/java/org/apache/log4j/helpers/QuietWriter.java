7af2412380fbe4689b1641d03d838b420d120b6b
Ceki Gulcu <ceki@apache.org>
2004-11-09 18:23:26 +0000

    Indentation changes only.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310854 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/helpers/QuietWriter.java
+++ src/java/org/apache/log4j/helpers/QuietWriter.java
@@ -1,65 +1,73 @@
 /*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
+ * Copyright 1999,2004 The Apache Software Foundation.
  *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included  with this distribution in
- * the LICENSE.txt file.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.log4j.helpers;
 
-import java.io.Writer;
+import org.apache.log4j.spi.ErrorCode;
+import org.apache.log4j.spi.ErrorHandler;
+
 import java.io.FilterWriter;
 import java.io.IOException;
-import org.apache.log4j.spi.ErrorHandler;
-import org.apache.log4j.spi.ErrorCode;
+import java.io.Writer;
 
 
 /**
-   QuietWriter does not throw exceptions when things go
-   wrong. Instead, it delegates error handling to its {@link ErrorHandler}. 
-
-   @author Ceki G&uuml;lc&uuml;
-
-   @since 0.7.3
+ * QuietWriter does not throw exceptions when things go
+ * wrong. Instead, it delegates error handling to its {@link ErrorHandler}.
+ * 
+ * @author Ceki G&uuml;lc&uuml;
+ * 
+ * @since 0.7.3
 */
 public class QuietWriter extends FilterWriter {
-
   protected ErrorHandler errorHandler;
 
-  public
-  QuietWriter(Writer writer, ErrorHandler errorHandler) {
+  public QuietWriter(Writer writer, ErrorHandler errorHandler) {
     super(writer);
     setErrorHandler(errorHandler);
   }
 
-  public
-  void write(String string) {
+  public Writer getWriter() {
+    return out;
+  }
+  
+  public void write(String string) {
     try {
       out.write(string);
-    } catch(IOException e) {
-      errorHandler.error("Failed to write ["+string+"].", e, 
-			 ErrorCode.WRITE_FAILURE);
+    } catch (IOException e) {
+      errorHandler.error(
+        "Failed to write [" + string + "].", e, ErrorCode.WRITE_FAILURE);
     }
   }
 
-  public
-  void flush() {
+  public void flush() {
     try {
       out.flush();
-    } catch(IOException e) {
-      errorHandler.error("Failed to flush writer,", e, 
-			 ErrorCode.FLUSH_FAILURE);
-    }	
+    } catch (IOException e) {
+      errorHandler.error(
+        "Failed to flush writer,", e, ErrorCode.FLUSH_FAILURE);
+    }
   }
 
-
-  public
-  void setErrorHandler(ErrorHandler eh) {
-    if(eh == null) {
+  public void setErrorHandler(ErrorHandler eh) {
+    if (eh == null) {
       // This is a programming error on the part of the enclosing appender.
-      throw new IllegalArgumentException("Attempted to set null ErrorHandler.");
-    } else { 
+      throw new IllegalArgumentException(
+        "Attempted to set null ErrorHandler.");
+    } else {
       this.errorHandler = eh;
     }
   }
