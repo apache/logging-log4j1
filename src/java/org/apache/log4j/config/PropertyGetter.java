32f2fd99af76c24b022ddf57af24da489b2d445d
Ceki Gulcu <ceki@apache.org>
2004-11-17 20:53:35 +0000

    Indentation changes only
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310876 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/config/PropertyGetter.java
+++ src/java/org/apache/log4j/config/PropertyGetter.java
@@ -1,18 +1,27 @@
 /*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
+ * Copyright 1999,2004 The Apache Software Foundation.
  *
- * This software is published under the terms of the Apache Software
- * License version 1.1, a copy of which has been included with this
- * distribution in the LICENSE.txt file.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.log4j.config;
 
-import java.beans.*;
-import java.lang.reflect.*;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.helpers.LogLog;
+
+import java.beans.*;
+
+import java.lang.reflect.*;
 
 
 /**
@@ -21,13 +30,9 @@ import org.apache.log4j.helpers.LogLog;
    @author  Anders Kristensen
  */
 public class PropertyGetter {
-  protected static final Object[] NULL_ARG = new Object[] {};
+  protected static final Object[] NULL_ARG = new Object[] {  };
   protected Object obj;
   protected PropertyDescriptor[] props;
-
-  public interface PropertyCallback {
-    void foundProperty(Object obj, String prefix, String name, Object value);
-  }
 
   /**
     Create a new PropertyGetter for the specified Object. This is done
@@ -36,16 +41,14 @@ public class PropertyGetter {
     more times.
 
     @param obj the object for which to set properties */
-  public
-  PropertyGetter(Object obj) throws IntrospectionException {
+  public PropertyGetter(Object obj) throws IntrospectionException {
     BeanInfo bi = Introspector.getBeanInfo(obj.getClass());
     props = bi.getPropertyDescriptors();
     this.obj = obj;
   }
 
-  public
-  static
-  void getProperties(Object obj, PropertyCallback callback, String prefix) {
+  public static void getProperties(
+    Object obj, PropertyCallback callback, String prefix) {
     try {
       new PropertyGetter(obj).getProperties(callback, prefix);
     } catch (IntrospectionException ex) {
@@ -53,34 +56,38 @@ public class PropertyGetter {
     }
   }
 
-  public
-  void getProperties(PropertyCallback callback, String prefix) {
+  public void getProperties(PropertyCallback callback, String prefix) {
     for (int i = 0; i < props.length; i++) {
       Method getter = props[i].getReadMethod();
-      if (getter == null) continue;
+      if (getter == null) {
+        continue;
+      }
       if (!isHandledType(getter.getReturnType())) {
-	//System.err.println("Ignoring " + props[i].getName() +" " + getter.getReturnType());
-	continue;
+        //System.err.println("Ignoring " + props[i].getName() +" " + getter.getReturnType());
+        continue;
       }
       String name = props[i].getName();
       try {
-	Object result = getter.invoke(obj, NULL_ARG);
-	//System.err.println("PROP " + name +": " + result);
-	if (result != null) {
-	  callback.foundProperty(obj, prefix, name, result);
-	}
+        Object result = getter.invoke(obj, NULL_ARG);
+
+        //System.err.println("PROP " + name +": " + result);
+        if (result != null) {
+          callback.foundProperty(obj, prefix, name, result);
+        }
       } catch (Exception ex) {
-	LogLog.warn("Failed to get value of property " + name);
+        LogLog.warn("Failed to get value of property " + name);
       }
     }
   }
 
-  protected
-  boolean isHandledType(Class type) {
-    return String.class.isAssignableFrom(type) ||
-      Integer.TYPE.isAssignableFrom(type) ||
-      Long.TYPE.isAssignableFrom(type)    ||
-      Boolean.TYPE.isAssignableFrom(type) ||
-      Level.class.isAssignableFrom(type);
+  protected boolean isHandledType(Class type) {
+    return String.class.isAssignableFrom(type)
+    || Integer.TYPE.isAssignableFrom(type) || Long.TYPE.isAssignableFrom(type)
+    || Boolean.TYPE.isAssignableFrom(type)
+    || Level.class.isAssignableFrom(type);
+  }
+
+  public interface PropertyCallback {
+    void foundProperty(Object obj, String prefix, String name, Object value);
   }
 }
