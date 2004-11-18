0cb6295bb856414bd41ec081b73b20f7545ee758
Ceki Gulcu <ceki@apache.org>
2004-11-18 11:52:30 +0000

    Clean up
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310883 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/or/jms/MessageRenderer.java
+++ src/java/org/apache/log4j/or/jms/MessageRenderer.java
@@ -1,18 +1,27 @@
 /*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
+ * Copyright 1999,2004 The Apache Software Foundation.
  *
- * This software is published under the terms of the Apache Software
- * License version 1.1, a copy of which has been included with this
- * distribution in the LICENSE.txt file.  */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.apache.log4j.or.jms;
 
-import org.apache.log4j.helpers.LogLog;
 import org.apache.log4j.or.ObjectRenderer;
 
-import javax.jms.Message;
-import javax.jms.JMSException;
 import javax.jms.DeliveryMode;
+import javax.jms.JMSException;
+import javax.jms.Message;
+
 
 /**
    Render <code>javax.jms.Message</code> objects.
@@ -20,59 +29,56 @@ import javax.jms.DeliveryMode;
    @author Ceki G&uuml;lc&uuml;
    @since 1.0 */
 public class MessageRenderer implements ObjectRenderer {
-
-  public
-  MessageRenderer() {
+  public MessageRenderer() {
   }
-
 
   /**
      Render a {@link javax.jms.Message}.
   */
-  public
-  String  doRender(Object o) {
-    if(o instanceof Message) {
+  public String doRender(Object o) {
+    if (o instanceof Message) {
       StringBuffer sbuf = new StringBuffer();
       Message m = (Message) o;
       try {
-	sbuf.append("DeliveryMode=");
-	switch(m.getJMSDeliveryMode()) {
-	case DeliveryMode.NON_PERSISTENT :
-	  sbuf.append("NON_PERSISTENT");
-	  break;
-	case DeliveryMode.PERSISTENT :
-	  sbuf.append("PERSISTENT");
-	  break;
-	default: sbuf.append("UNKNOWN");
-	}
-	sbuf.append(", CorrelationID=");
-	sbuf.append(m.getJMSCorrelationID());
+        sbuf.append("DeliveryMode=");
+        switch (m.getJMSDeliveryMode()) {
+        case DeliveryMode.NON_PERSISTENT:
+          sbuf.append("NON_PERSISTENT");
+          break;
+        case DeliveryMode.PERSISTENT:
+          sbuf.append("PERSISTENT");
+          break;
+        default:
+          sbuf.append("UNKNOWN");
+        }
+        sbuf.append(", CorrelationID=");
+        sbuf.append(m.getJMSCorrelationID());
 
-	sbuf.append(", Destination=");
-	sbuf.append(m.getJMSDestination());
+        sbuf.append(", Destination=");
+        sbuf.append(m.getJMSDestination());
 
-	sbuf.append(", Expiration=");
-	sbuf.append(m.getJMSExpiration());
+        sbuf.append(", Expiration=");
+        sbuf.append(m.getJMSExpiration());
 
-	sbuf.append(", MessageID=");
-	sbuf.append(m.getJMSMessageID());
+        sbuf.append(", MessageID=");
+        sbuf.append(m.getJMSMessageID());
 
-	sbuf.append(", Priority=");
-	sbuf.append(m.getJMSPriority());
+        sbuf.append(", Priority=");
+        sbuf.append(m.getJMSPriority());
 
-	sbuf.append(", Redelivered=");
-	sbuf.append(m.getJMSRedelivered());
+        sbuf.append(", Redelivered=");
+        sbuf.append(m.getJMSRedelivered());
 
-	sbuf.append(", ReplyTo=");
-	sbuf.append(m.getJMSReplyTo());
+        sbuf.append(", ReplyTo=");
+        sbuf.append(m.getJMSReplyTo());
 
-	sbuf.append(", Timestamp=");
-	sbuf.append(m.getJMSTimestamp());
+        sbuf.append(", Timestamp=");
+        sbuf.append(m.getJMSTimestamp());
 
-	sbuf.append(", Type=");
-	sbuf.append(m.getJMSType());
-      } catch(JMSException e) {
-	LogLog.error("Could not parse Message.", e);
+        sbuf.append(", Type=");
+        sbuf.append(m.getJMSType());
+      } catch (JMSException e) {
+        return o.toString();
       }
       return sbuf.toString();
     } else {
