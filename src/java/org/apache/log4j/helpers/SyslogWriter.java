ab0b9dd9be5a782a2e478b1dc87aeb78516b36ef
Ceki Gulcu <ceki@apache.org>
2004-11-20 14:33:53 +0000

    Indentation changes only
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310912 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/helpers/SyslogWriter.java
+++ src/java/org/apache/log4j/helpers/SyslogWriter.java
@@ -1,21 +1,30 @@
 /*
- * Copyright (C) The Apache Software Foundation. All rights reserved.
+ * Copyright 1999,2004 The Apache Software Foundation.
  *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included  with this distribution in
- * the LICENSE.txt file.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.log4j.helpers;
 
-
+import java.io.IOException;
 import java.io.Writer;
+
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.DatagramPacket;
-import java.net.UnknownHostException;
 import java.net.SocketException;
-import java.io.IOException;
+import java.net.UnknownHostException;
+
 
 /**
    SyslogWriter is a wrapper around the java.net.DatagramSocket class
@@ -24,55 +33,48 @@ import java.io.IOException;
    @since 0.7.3
 */
 public class SyslogWriter extends Writer {
-
   final int SYSLOG_PORT = 514;
   String syslogHost;
-  
   private InetAddress address;
   private DatagramSocket ds;
 
-  public
-  SyslogWriter(String syslogHost) {
+  public SyslogWriter(String syslogHost) {
     this.syslogHost = syslogHost;
 
-    try {      
+    try {
       this.address = InetAddress.getByName(syslogHost);
-    }
-    catch (UnknownHostException e) {
-      LogLog.error("Could not find " + syslogHost +
-			 ". All logging will FAIL.", e);
+    } catch (UnknownHostException e) {
+      LogLog.error(
+        "Could not find " + syslogHost + ". All logging will FAIL.", e);
     }
 
     try {
       this.ds = new DatagramSocket();
-    }
-    catch (SocketException e) {
-      e.printStackTrace(); 
-      LogLog.error("Could not instantiate DatagramSocket to " + syslogHost +
-			 ". All logging will FAIL.", e);
+    } catch (SocketException e) {
+      e.printStackTrace();
+      LogLog.error(
+        "Could not instantiate DatagramSocket to " + syslogHost
+        + ". All logging will FAIL.", e);
     }
   }
 
-
-  public
-  void write(char[] buf, int off, int len) throws IOException {
+  public void write(char[] buf, int off, int len) throws IOException {
     this.write(new String(buf, off, len));
   }
-  
-  public
-  void write(String string) throws IOException {
-    byte[] bytes = string.getBytes();
-    DatagramPacket packet = new DatagramPacket(bytes, bytes.length,
-					       address, SYSLOG_PORT);
 
-    if(this.ds != null)
+  public void write(String string) throws IOException {
+    byte[] bytes = string.getBytes();
+    DatagramPacket packet =
+      new DatagramPacket(bytes, bytes.length, address, SYSLOG_PORT);
+
+    if (this.ds != null) {
       ds.send(packet);
-    
+    }
   }
 
-  public
-  void flush() {}
+  public void flush() {
+  }
 
-  public
-  void close() {}
+  public void close() {
+  }
 }
