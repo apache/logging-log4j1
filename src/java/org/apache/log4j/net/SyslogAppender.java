254771f7bb218b8f34cff7c4f3b85b922caf164a
Ceki Gulcu <ceki@apache.org>
2004-11-29 15:36:46 +0000

    Checnhes to make SyslogAppender RFC 3164 compatible.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@310971 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/net/SyslogAppender.java
+++ src/java/org/apache/log4j/net/SyslogAppender.java
@@ -18,113 +18,58 @@ package org.apache.log4j.net;
 
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
-import org.apache.log4j.helpers.SyslogQuietWriter;
 import org.apache.log4j.helpers.SyslogWriter;
 import org.apache.log4j.spi.LoggingEvent;
+
+import java.io.IOException;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import java.text.DateFormatSymbols;
+import java.text.FieldPosition;
+import java.text.SimpleDateFormat;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 
 // Contributors: Yves Bossel <ybossel@opengets.cl>
 //               Christopher Taylor <cstaylor@pacbell.net>
 
-
 /**
- * Use SyslogAppender to send log messages to a remote syslog daemon.
+ * Implements an RFC 3164 compliant agent to send log messages to a remote
+ * syslog daemon.
+ *
  * @author Ceki G&uuml;lc&uuml;
  * @author Anders Kristensen
+ * @author Hermod Opstvedt
  */
 public class SyslogAppender extends AppenderSkeleton {
-  // The following constants are extracted from a syslog.h file
-  // copyrighted by the Regents of the University of California
-  // I hope nobody at Berkley gets offended.
-
-  /** Kernel messages */
-  public static final int LOG_KERN = 0;
-
-  /** Random user-level messages */
-  public static final int LOG_USER = 1 << 3;
-
-  /** Mail system */
-  public static final int LOG_MAIL = 2 << 3;
-
-  /** System daemons */
-  public static final int LOG_DAEMON = 3 << 3;
-
-  /** security/authorization messages */
-  public static final int LOG_AUTH = 4 << 3;
-
-  /** messages generated internally by syslogd */
-  public static final int LOG_SYSLOG = 5 << 3;
-
-  /** line printer subsystem */
-  public static final int LOG_LPR = 6 << 3;
-
-  /** network news subsystem */
-  public static final int LOG_NEWS = 7 << 3;
-
-  /** UUCP subsystem */
-  public static final int LOG_UUCP = 8 << 3;
-
-  /** clock daemon */
-  public static final int LOG_CRON = 9 << 3;
-
-  /** security/authorization  messages (private) */
-  public static final int LOG_AUTHPRIV = 10 << 3;
-
-  /** ftp daemon */
-  public static final int LOG_FTP = 11 << 3;
-
-  // other codes through 15 reserved for system use
-
-
-  /** reserved for local use */
-  public static final int LOG_LOCAL0 = 16 << 3;
-
-  /** reserved for local use */
-  public static final int LOG_LOCAL1 = 17 << 3;
-
-  /** reserved for local use */
-  public static final int LOG_LOCAL2 = 18 << 3;
-
-  /** reserved for local use */
-  public static final int LOG_LOCAL3 = 19 << 3;
-
-  /** reserved for local use */
-  public static final int LOG_LOCAL4 = 20 << 3;
-
-  /** reserved for local use */
-  public static final int LOG_LOCAL5 = 21 << 3;
-
-  /** reserved for local use */
-  public static final int LOG_LOCAL6 = 22 << 3;
-
-  /** reserved for local use*/
-  public static final int LOG_LOCAL7 = 23 << 3;
   protected static final int SYSLOG_HOST_OI = 0;
   protected static final int FACILITY_OI = 1;
   static final String TAB = "    ";
-
-  // Have LOG_USER as default
-  int syslogFacility = LOG_USER;
+  int facility;
   String facilityStr;
-  boolean facilityPrinting = false;
-
-  //SyslogTracerPrintWriter stp;
-  SyslogQuietWriter sqw;
+  String localHostname;
   String syslogHost;
 
+  //SyslogTracerPrintWriter stp;
+  SyslogWriter sw;
+  Calendar calendar = Calendar.getInstance();
+  long now = -1;
+  Date date = new Date();
+  StringBuffer timestamp = new StringBuffer();
+  protected FieldPosition pos = new FieldPosition(0);
+
+  // We must use US locale to get the correct month abreviation  
+  private SimpleDateFormat sdf =
+    new SimpleDateFormat("MMM dd hh:mm:ss", new DateFormatSymbols(Locale.US));
+
+  Layout layout;
+  
   public SyslogAppender() {
-    this.initSyslogFacilityStr();
-  }
-
-  public SyslogAppender(Layout layout, int syslogFacility) {
-    this.layout = layout;
-    this.syslogFacility = syslogFacility;
-    this.initSyslogFacilityStr();
-  }
-
-  public SyslogAppender(Layout layout, String syslogHost, int syslogFacility) {
-    this(layout, syslogFacility);
-    setSyslogHost(syslogHost);
   }
 
   /**
@@ -136,165 +81,75 @@ public class SyslogAppender extends AppenderSkeleton {
 
     // A SyslogWriter is UDP based and needs no opening. Hence, it
     // can't be closed. We just unset the variables here.
-    sqw = null;
+    sw = null;
   }
 
-  private void initSyslogFacilityStr() {
-    facilityStr = getFacilityString(this.syslogFacility);
-
-    if (facilityStr == null) {
-      System.err.println(
-        "\"" + syslogFacility
-        + "\" is an unknown syslog facility. Defaulting to \"USER\".");
-      this.syslogFacility = LOG_USER;
-      facilityStr = "user:";
-    } else {
-      facilityStr += ":";
+  /**
+   * This method gets the network name of the machine we are running on.
+   * Returns "UNKNOWN_HOST" in the unlikely case where the host name cannot be
+   * found.
+   * @return String
+   */
+  public String getLocalHostname() {
+    try {
+      InetAddress addr = InetAddress.getLocalHost();
+      return addr.getHostName();
+    } catch (UnknownHostException uhe) {
+      getLogger().error(
+        "Could not determine local host name for SyslogAppender" + "named ["
+        + name + "].", uhe);
+      return "UNKNOWN_HOST";
     }
   }
 
   /**
-   * Returns the specified syslog facility as a lower-case String, e.g. "kern", 
-   * "user", etc. 
-   * */
-  public static String getFacilityString(int syslogFacility) {
-    switch (syslogFacility) {
-    case LOG_KERN:
-      return "kern";
-    case LOG_USER:
-      return "user";
-    case LOG_MAIL:
-      return "mail";
-    case LOG_DAEMON:
-      return "daemon";
-    case LOG_AUTH:
-      return "auth";
-    case LOG_SYSLOG:
-      return "syslog";
-    case LOG_LPR:
-      return "lpr";
-    case LOG_NEWS:
-      return "news";
-    case LOG_UUCP:
-      return "uucp";
-    case LOG_CRON:
-      return "cron";
-    case LOG_AUTHPRIV:
-      return "authpriv";
-    case LOG_FTP:
-      return "ftp";
-    case LOG_LOCAL0:
-      return "local0";
-    case LOG_LOCAL1:
-      return "local1";
-    case LOG_LOCAL2:
-      return "local2";
-    case LOG_LOCAL3:
-      return "local3";
-    case LOG_LOCAL4:
-      return "local4";
-    case LOG_LOCAL5:
-      return "local5";
-    case LOG_LOCAL6:
-      return "local6";
-    case LOG_LOCAL7:
-      return "local7";
-    default:
-      return null;
-    }
-  }
-
-  /**
-   * Returns the integer value corresponding to the named syslog facility, 
+   * Returns the integer value corresponding to the named syslog facility,
    * or -1 if it couldn't be recognized.
-   * 
-   * @param facilityName one of the case-insensitive strings KERN, USER, MAIL, 
-   * DAEMON, AUTH, SYSLOG, LPR, NEWS, UUCP, CRON, AUTHPRIV, FTP, LOCAL0, LOCAL1, 
-   * LOCAL2, LOCAL3, LOCAL4, LOCAL5, LOCAL6, LOCAL7. 
-   * @since 1.1 
+   *
    * */
-  public static int getFacility(String facilityName) {
-    if (facilityName != null) {
-      facilityName = facilityName.trim();
-    }
-
-    if ("KERN".equalsIgnoreCase(facilityName)) {
-      return LOG_KERN;
-    } else if ("USER".equalsIgnoreCase(facilityName)) {
-      return LOG_USER;
-    } else if ("MAIL".equalsIgnoreCase(facilityName)) {
-      return LOG_MAIL;
-    } else if ("DAEMON".equalsIgnoreCase(facilityName)) {
-      return LOG_DAEMON;
-    } else if ("AUTH".equalsIgnoreCase(facilityName)) {
-      return LOG_AUTH;
-    } else if ("SYSLOG".equalsIgnoreCase(facilityName)) {
-      return LOG_SYSLOG;
-    } else if ("LPR".equalsIgnoreCase(facilityName)) {
-      return LOG_LPR;
-    } else if ("NEWS".equalsIgnoreCase(facilityName)) {
-      return LOG_NEWS;
-    } else if ("UUCP".equalsIgnoreCase(facilityName)) {
-      return LOG_UUCP;
-    } else if ("CRON".equalsIgnoreCase(facilityName)) {
-      return LOG_CRON;
-    } else if ("AUTHPRIV".equalsIgnoreCase(facilityName)) {
-      return LOG_AUTHPRIV;
-    } else if ("FTP".equalsIgnoreCase(facilityName)) {
-      return LOG_FTP;
-    } else if ("LOCAL0".equalsIgnoreCase(facilityName)) {
-      return LOG_LOCAL0;
-    } else if ("LOCAL1".equalsIgnoreCase(facilityName)) {
-      return LOG_LOCAL1;
-    } else if ("LOCAL2".equalsIgnoreCase(facilityName)) {
-      return LOG_LOCAL2;
-    } else if ("LOCAL3".equalsIgnoreCase(facilityName)) {
-      return LOG_LOCAL3;
-    } else if ("LOCAL4".equalsIgnoreCase(facilityName)) {
-      return LOG_LOCAL4;
-    } else if ("LOCAL5".equalsIgnoreCase(facilityName)) {
-      return LOG_LOCAL5;
-    } else if ("LOCAL6".equalsIgnoreCase(facilityName)) {
-      return LOG_LOCAL6;
-    } else if ("LOCAL7".equalsIgnoreCase(facilityName)) {
-      return LOG_LOCAL7;
+  static int facilityStringToint(String facilityStr) {
+    if ("KERN".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_KERN;
+    } else if ("USER".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_USER;
+    } else if ("MAIL".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_MAIL;
+    } else if ("DAEMON".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_DAEMON;
+    } else if ("AUTH".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_AUTH;
+    } else if ("SYSLOG".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_SYSLOG;
+    } else if ("LPR".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_LPR;
+    } else if ("NEWS".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_NEWS;
+    } else if ("UUCP".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_UUCP;
+    } else if ("CRON".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_CRON;
+    } else if ("AUTHPRIV".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_AUTHPRIV;
+    } else if ("FTP".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_FTP;
+    } else if ("LOCAL0".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_LOCAL0;
+    } else if ("LOCAL1".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_LOCAL1;
+    } else if ("LOCAL2".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_LOCAL2;
+    } else if ("LOCAL3".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_LOCAL3;
+    } else if ("LOCAL4".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_LOCAL4;
+    } else if ("LOCAL5".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_LOCAL5;
+    } else if ("LOCAL6".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_LOCAL6;
+    } else if ("LOCAL7".equalsIgnoreCase(facilityStr)) {
+      return SyslogConstants.LOG_LOCAL7;
     } else {
       return -1;
-    }
-  }
-
-  public void append(LoggingEvent event) {
-    if (!isAsSevereAsThreshold(event.getLevel())) {
-      return;
-    }
-
-    // We must not attempt to append if sqw is null.
-    if (sqw == null) {
-      errorHandler.error(
-        "No syslog host is set for SyslogAppedender named \"" + this.name
-        + "\".");
-
-      return;
-    }
-
-    String buffer =
-      (facilityPrinting ? facilityStr : "") + layout.format(event);
-
-    sqw.setLevel(event.getLevel().getSyslogEquivalent());
-    sqw.write(buffer);
-
-    String[] s = event.getThrowableStrRep();
-
-    if (s != null) {
-      int len = s.length;
-
-      if (len > 0) {
-        sqw.write(s[0]);
-
-        for (int i = 1; i < len; i++) {
-          sqw.write(TAB + s[i].substring(1));
-        }
-      }
     }
   }
 
@@ -302,30 +157,54 @@ public class SyslogAppender extends AppenderSkeleton {
    * This method returns immediately as options are activated when they are set.
    * */
   public void activateOptions() {
+    if (facilityStr == null) {
+      String errMsg =
+        "The Facility option must be set for SyslogAppender named [" + name
+        + "].";
+      getLogger().error(errMsg);
+      throw new IllegalStateException(errMsg);
+    }
+    facilityStr = facilityStr.trim();
+    getLogger().debug("Facility string set to be {}.", facilityStr);
+    facility = facilityStringToint(facilityStr);
+    getLogger().debug("Facility set to be "+ facility);
+    
+    if (facility == -1) {
+      String errMsg =
+        "Unrecognized Facility option \"" + facilityStr
+        + "\" SyslogAppender named [" + name + "].";
+      getLogger().error(errMsg);
+      throw new IllegalStateException(errMsg);
+    }
+
+    if (syslogHost == null) {
+      String errMsg =
+        "No syslog host is set for SyslogAppedender named \"" + this.name
+        + "\".";
+      getLogger().error(errMsg);
+      throw new IllegalStateException(errMsg);
+    }
+
+    if (layout == null) {
+      String errMsg =
+        "No Layout is set for SyslogAppedender named \"" + this.name
+        + "\".";
+      getLogger().error(errMsg);
+      throw new IllegalStateException(errMsg);
+    }
+    
+    localHostname = getLocalHostname();
+
+    sw = new SyslogWriter(syslogHost);
   }
 
   /**
-   * The SyslogAppender requires a layout. Hence, this method returns 
-   * <code>true</code>.
-   * 
-   * @since 0.8.4 
-   * */
-  public boolean requiresLayout() {
-    return true;
-  }
-
-  /**
-   * The <b>SyslogHost</b> option is the name of the the syslog host where log 
+   * The <b>SyslogHost</b> option is the name of the the syslog host where log
    * output should go.
-   * 
+   *
    * <b>WARNING</b> If the SyslogHost is not set, then this appender will fail.
    */
   public void setSyslogHost(String syslogHost) {
-    this.sqw =
-      new SyslogQuietWriter(
-        new SyslogWriter(syslogHost), syslogFacility, errorHandler);
-
-    //this.stp = new SyslogTracerPrintWriter(sqw);
     this.syslogHost = syslogHost;
   }
 
@@ -338,56 +217,89 @@ public class SyslogAppender extends AppenderSkeleton {
 
   /**
    * Set the syslog facility. This is the <b>Facility</b> option.
-   * 
-   * <p>The <code>facilityName</code> parameter must be one of the strings KERN,
-   * USER, MAIL, DAEMON, AUTH, SYSLOG, LPR, NEWS, UUCP, CRON, AUTHPRIV, FTP, 
-   * LOCAL0, LOCAL1, LOCAL2, LOCAL3, LOCAL4, LOCAL5, LOCAL6, LOCAL7. Case is 
-   * unimportant.
-   * 
-   * @since 0.8.1 
+   *
+   * <p>The <code>facility</code> parameter must be one of the strings KERN,
+   * USER, MAIL, DAEMON, AUTH, SYSLOG, LPR, NEWS, UUCP, CRON, AUTHPRIV, FTP,
+   * NTP, AUDIT, ALERT, CLOCK, LOCAL0, LOCAL1, LOCAL2, LOCAL3, LOCAL4, LOCAL5,
+   * LOCAL6, LOCAL7. Case is not important.
+   *
+   * See {@link SyslogConstants} and RFC 3164 for more information about the
+   * <b>Facility</b> option.
    * */
-  public void setFacility(String facilityName) {
-    if (facilityName == null) {
-      return;
-    }
-
-    syslogFacility = getFacility(facilityName);
-
-    if (syslogFacility == -1) {
-      System.err.println(
-        "[" + facilityName
-        + "] is an unknown syslog facility. Defaulting to [USER].");
-      syslogFacility = LOG_USER;
-    }
-
-    this.initSyslogFacilityStr();
-
-    // If there is already a sqw, make it use the new facility.
-    if (sqw != null) {
-      sqw.setSyslogFacility(this.syslogFacility);
-    }
+  public void setFacility(String facility) {
+    this.facilityStr = facility;
   }
 
   /**
    * Returns the value of the <b>Facility</b> option.
+   *
+   * See {@link #setFacility} for the set of allowed values.
    */
   public String getFacility() {
-    return getFacilityString(syslogFacility);
+    return facilityStr;
   }
 
-  /**
-   * If the <b>FacilityPrinting</b> option is set to true, the printed message 
-   * will include the facility name of the application. It is <em>false</em> by 
-   * default.
-   */
-  public void setFacilityPrinting(boolean on) {
-    facilityPrinting = on;
+  void fillInTimestamp() throws IOException {
+    long n = System.currentTimeMillis();
+    n -= (n & 1000);
+
+    if ((n != now) || (timestamp.length() == 0)) {
+      now = n;
+      date.setTime(n);
+      // erase any previous value of the timestamp
+      timestamp.setLength(0);
+      sdf.format(date, timestamp, pos);
+      //According to the RFC the day of the month must be right justified with
+      // no leading 0.
+      if (timestamp.charAt(4) == '0') {
+        timestamp.setCharAt(4, ' ');
+      }
+    }
+    sw.write(timestamp.toString());
   }
 
-  /**
-   * Returns the value of the <b>FacilityPrinting</b> option.
-   */
-  public boolean getFacilityPrinting() {
-    return facilityPrinting;
+  void writeInitialParts(LoggingEvent event) throws IOException {
+    int pri = facility+event.getLevel().getSyslogEquivalent();
+    System.out.println(""+pri);
+    sw.write("<");
+    sw.write(String.valueOf(pri));
+    sw.write(">");
+    fillInTimestamp();
+    sw.write(' ');
+    sw.write(localHostname);
+    sw.write(' ');
+  }
+  
+  public void append(LoggingEvent event) {
+    // We must not attempt to append if sw is null.
+    if (sw == null) {
+      return;
+    }
+
+    try {
+      writeInitialParts(event);
+      layout.format(sw, event);
+      sw.flush();
+//      String[] s = event.getThrowableStrRep();
+//
+//      if (s != null) {
+//        int len = s.length;
+//
+//        if (len > 0) {
+//          sw.write(s[0]);
+//
+//          for (int i = 1; i < len; i++) {
+//            sw.write(TAB + s[i].substring(1));
+//          }
+//        }
+//      }
+    } catch (IOException ioe) {
+    }
+  }
+  public Layout getLayout() {
+    return layout;
+  }
+  public void setLayout(Layout layout) {
+    this.layout = layout;
   }
 }
