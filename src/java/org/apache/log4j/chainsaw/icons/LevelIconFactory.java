74de90d445752a060cb9a88f16e4fdd58ee86b97
Scott Deboy <sdeboy@apache.org>
2004-12-08 09:17:42 +0000

    Using ERROR icon for fatal events Adding entries to generator
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@310999 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/chainsaw/icons/LevelIconFactory.java
+++ src/java/org/apache/log4j/chainsaw/icons/LevelIconFactory.java
@@ -32,9 +32,10 @@ public class LevelIconFactory {
   private final Map iconMap = new HashMap();
 
   private LevelIconFactory() {
+    //reuse ERROR icon for FATAL level
     String[] iconFileNames =
-      new String[] { "Warn.gif", "Inform.gif", "Error.gif" };
-    String[] iconLabels = new String[] { "WARN", "INFO", "ERROR" };
+      new String[] { "Warn.gif", "Inform.gif", "Error.gif" , "Error.gif"};
+    String[] iconLabels = new String[] { "WARN", "INFO", "ERROR", "FATAL" };
 
     for (int i = 0; i < iconLabels.length; i++) {
       URL resourceURL = UIManager.getLookAndFeel().getClass().getResource(
@@ -58,6 +59,7 @@ public class LevelIconFactory {
           newWidth, newHeight, Image.SCALE_SMOOTH);
       iconMap.put(iconLabels[i], new ImageIcon(iconImage));
     }
+    //reuse DEBUG icon for TRACE level
     iconMap.put("TRACE", ChainsawIcons.ICON_DEBUG);
     iconMap.put("DEBUG", ChainsawIcons.ICON_DEBUG);
   }
