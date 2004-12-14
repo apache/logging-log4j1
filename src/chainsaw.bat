46257bfc8a97c52db017e2709f9dfe38d983cb0e
Paul Jeffrey Smith <psmith@apache.org>
2004-12-14 21:56:36 +0000

    these 2 changes are just because we're now at alpha-4.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@311055 13f79535-47bb-0310-9956-ffa450edef68

--- src/chainsaw.bat
+++ src/chainsaw.bat
@@ -1,2 +1,2 @@
-java -classpath jakarta-oro-2.0.6.jar;log4j-1.3alpha.jar;log4j-chainsaw-1.3alpha.jar org.apache.log4j.chainsaw.LogUI
+java -classpath jakarta-oro-2.0.6.jar;log4j-1.3alpha-4.jar;log4j-chainsaw-1.3alpha-4.jar org.apache.log4j.chainsaw.LogUI
 
