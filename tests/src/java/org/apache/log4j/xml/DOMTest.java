97cd30aa214d5970705f9a94d648404a813be19c
Curtis William Arnold <carnold@apache.org>
2005-03-07 05:35:19 +0000

    Bug 33870: Restore DOMConfigurator.doConfigure(Element)
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@311309 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/java/org/apache/log4j/xml/DOMTest.java
+++ tests/src/java/org/apache/log4j/xml/DOMTest.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 1999-2005 The Apache Software Foundation.
- * 
+ * Copyright 1999,2005 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,50 +16,39 @@
 
 package org.apache.log4j.xml;
 
-import java.util.List;
-
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.apache.log4j.Level;
 import org.apache.log4j.joran.JoranConfigurator;
 import org.apache.log4j.spi.ErrorItem;
-import org.apache.log4j.util.Filter;
-import org.apache.log4j.util.JunitTestRunnerFilter;
-import org.apache.log4j.util.LineNumberFilter;
-import org.apache.log4j.util.SunReflectFilter;
-import org.apache.log4j.util.ControlFilter;
-import org.apache.log4j.util.ISO8601Filter;
-import org.apache.log4j.util.Transformer;
-import org.apache.log4j.util.Compare;
+import org.apache.log4j.util.*;
+
 import java.io.File;
 
-public class DOMTest extends TestCase {
+import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+
+public class DOMTest extends TestCase {
   static String TEMP_A1 = "output/temp.A1";
   static String TEMP_A2 = "output/temp.A2";
   static String FILTERED_A1 = "output/filtered.A1";
   static String FILTERED_A2 = "output/filtered.A2";
-
-
   static String EXCEPTION1 = "java.lang.Exception: Just testing";
   static String EXCEPTION2 = "\\s*at .*\\(.*:\\d{1,4}\\)";
   static String EXCEPTION3 = "\\s*at .*\\(Native Method\\)";
-
-  static String TEST1_1A_PAT = 
-                       "(DEBUG|INFO |WARN |ERROR|FATAL) \\w*\\.\\w* - Message \\d";
-
-  static String TEST1_1B_PAT = "(DEBUG|INFO |WARN |ERROR|FATAL) root - Message \\d";
-
-  static String TEST1_2_PAT = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3} "+
-                        "\\[main]\\ (DEBUG|INFO|WARN|ERROR|FATAL) .* - Message \\d";
-
-
-
-  Logger root; 
+  static String TEST1_1A_PAT =
+    "(DEBUG|INFO |WARN |ERROR|FATAL) \\w*\\.\\w* - Message \\d";
+  static String TEST1_1B_PAT =
+    "(DEBUG|INFO |WARN |ERROR|FATAL) root - Message \\d";
+  static String TEST1_2_PAT =
+    "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3} "
+    + "\\[main]\\ (DEBUG|INFO|WARN|ERROR|FATAL) .* - Message \\d";
+  Logger root;
   Logger logger;
 
   public DOMTest(String name) {
@@ -70,8 +59,8 @@ public class DOMTest extends TestCase {
     root = Logger.getRootLogger();
     logger = Logger.getLogger(DOMTest.class);
   }
- 
-  public void tearDown() {  
+
+  public void tearDown() {
     root.getLoggerRepository().resetConfiguration();
   }
 
@@ -82,20 +71,29 @@ public class DOMTest extends TestCase {
     dumpErrors(jc.getErrorList());
     common();
 
-    ControlFilter cf1 = new ControlFilter(new String[]{TEST1_1A_PAT, TEST1_1B_PAT, 
-					       EXCEPTION1, EXCEPTION2, EXCEPTION3});
+    ControlFilter cf1 =
+      new ControlFilter(
+        new String[] {
+          TEST1_1A_PAT, TEST1_1B_PAT, EXCEPTION1, EXCEPTION2, EXCEPTION3
+        });
 
-    ControlFilter cf2 = new ControlFilter(new String[]{TEST1_2_PAT, 
-					       EXCEPTION1, EXCEPTION2, EXCEPTION3});
+    ControlFilter cf2 =
+      new ControlFilter(
+        new String[] { TEST1_2_PAT, EXCEPTION1, EXCEPTION2, EXCEPTION3 });
 
-    Transformer.transform(TEMP_A1, FILTERED_A1, new Filter[] {cf1, 
-							new LineNumberFilter(), 
-              new SunReflectFilter(), 
-              new JunitTestRunnerFilter()});
+    Transformer.transform(
+      TEMP_A1, FILTERED_A1,
+      new Filter[] {
+        cf1, new LineNumberFilter(), new SunReflectFilter(),
+        new JunitTestRunnerFilter()
+      });
 
-    Transformer.transform(TEMP_A2, FILTERED_A2, new Filter[] {cf2,
-                                      new LineNumberFilter(), new ISO8601Filter(),
-                                      new SunReflectFilter(), new JunitTestRunnerFilter()});
+    Transformer.transform(
+      TEMP_A2, FILTERED_A2,
+      new Filter[] {
+        cf2, new LineNumberFilter(), new ISO8601Filter(),
+        new SunReflectFilter(), new JunitTestRunnerFilter()
+      });
 
     assertTrue(Compare.compare(FILTERED_A1, "witness/xml/dom.A1.1"));
     assertTrue(Compare.compare(FILTERED_A2, "witness/xml/dom.A2.1"));
@@ -103,82 +101,135 @@ public class DOMTest extends TestCase {
 
   /**
    * Identical test except that backslashes are used instead of
-   * forward slashes on all file specifications.  Test is 
+   * forward slashes on all file specifications.  Test is
    * only applicable to Windows.
-   * 
+   *
    * @throws Exception Any exception will cause test to fail
    */
   public void test2() throws Exception {
-  	if (File.separatorChar == '\\') {
- 	    JoranConfigurator jc = new JoranConfigurator();
-	    jc.doConfigure("input\\xml\\DOMTest2.xml", LogManager.getLoggerRepository());
-	    dumpErrors(jc.getErrorList());
-	    common();
-	
-	    ControlFilter cf1 = new ControlFilter(new String[]{TEST1_1A_PAT, TEST1_1B_PAT, 
-						       EXCEPTION1, EXCEPTION2, EXCEPTION3});
-	
-	    ControlFilter cf2 = new ControlFilter(new String[]{TEST1_2_PAT, 
-						       EXCEPTION1, EXCEPTION2, EXCEPTION3});
-	
-	    Transformer.transform(TEMP_A1 + ".2", FILTERED_A1 + ".2", new Filter[] {cf1, 
-								new LineNumberFilter(), 
-	              new SunReflectFilter(), 
-	              new JunitTestRunnerFilter()});
-	
-	    Transformer.transform(TEMP_A2 + ".2", FILTERED_A2 + ".2", new Filter[] {cf2,
-	                                      new LineNumberFilter(), new ISO8601Filter(),
-	                                      new SunReflectFilter(), new JunitTestRunnerFilter()});
-	
-	    assertTrue(Compare.compare(FILTERED_A1, "witness/xml/dom.A1.2"));
-	    assertTrue(Compare.compare(FILTERED_A2, "witness/xml/dom.A2.2"));
-  	}
+    if (File.separatorChar == '\\') {
+      JoranConfigurator jc = new JoranConfigurator();
+      jc.doConfigure(
+        "input\\xml\\DOMTest2.xml", LogManager.getLoggerRepository());
+      dumpErrors(jc.getErrorList());
+      common();
+
+      ControlFilter cf1 =
+        new ControlFilter(
+          new String[] {
+            TEST1_1A_PAT, TEST1_1B_PAT, EXCEPTION1, EXCEPTION2, EXCEPTION3
+          });
+
+      ControlFilter cf2 =
+        new ControlFilter(
+          new String[] { TEST1_2_PAT, EXCEPTION1, EXCEPTION2, EXCEPTION3 });
+
+      Transformer.transform(
+        TEMP_A1 + ".2", FILTERED_A1 + ".2",
+        new Filter[] {
+          cf1, new LineNumberFilter(), new SunReflectFilter(),
+          new JunitTestRunnerFilter()
+        });
+
+      Transformer.transform(
+        TEMP_A2 + ".2", FILTERED_A2 + ".2",
+        new Filter[] {
+          cf2, new LineNumberFilter(), new ISO8601Filter(),
+          new SunReflectFilter(), new JunitTestRunnerFilter()
+        });
+
+      assertTrue(Compare.compare(FILTERED_A1 + ".2", "witness/xml/dom.A1.2"));
+      assertTrue(Compare.compare(FILTERED_A2 + ".2", "witness/xml/dom.A2.2"));
+    }
   }
-  
-  
- 
+
+  /**
+   * This test checks the implementation of DOMConfigurator.doConfigure(Element)
+   * which is provided for compatibility with log4j 1.2 and used by excalibur-logging.
+   *
+   * @deprecated This test checks a deprecated method and so needs to be deprecated itself.
+   *
+   * @throws Exception on failure to find parser, etc.
+   */
+  public void test3() throws Exception {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setNamespaceAware(true);
+
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    org.w3c.dom.Document doc =
+      builder.parse(new File("input/xml/DOMTest3.xml"));
+
+    DOMConfigurator domConfig = new DOMConfigurator();
+    domConfig.doConfigure(
+      doc.getDocumentElement(), LogManager.getLoggerRepository());
+
+    dumpErrors(domConfig.getErrorList());
+    common();
+
+    ControlFilter cf1 =
+      new ControlFilter(
+        new String[] {
+          TEST1_1A_PAT, TEST1_1B_PAT, EXCEPTION1, EXCEPTION2, EXCEPTION3
+        });
+
+    ControlFilter cf2 =
+      new ControlFilter(
+        new String[] { TEST1_2_PAT, EXCEPTION1, EXCEPTION2, EXCEPTION3 });
+
+    Transformer.transform(
+      TEMP_A1 + ".3", FILTERED_A1 + ".3",
+      new Filter[] {
+        cf1, new LineNumberFilter(), new SunReflectFilter(),
+        new JunitTestRunnerFilter()
+      });
+
+    Transformer.transform(
+      TEMP_A2 + ".3", FILTERED_A2 + ".3",
+      new Filter[] {
+        cf2, new LineNumberFilter(), new ISO8601Filter(),
+        new SunReflectFilter(), new JunitTestRunnerFilter()
+      });
+
+    assertTrue(Compare.compare(FILTERED_A1 + ".3", "witness/xml/dom.A1.3"));
+    assertTrue(Compare.compare(FILTERED_A2 + ".3", "witness/xml/dom.A2.3"));
+  }
+
   void common() {
     int i = -1;
- 
+
     logger.debug("Message " + ++i);
-    root.debug("Message " + i);        
+    root.debug("Message " + i);
 
-    logger.info ("Message " + ++i);
-    root.info("Message " + i);        
+    logger.info("Message " + ++i);
+    root.info("Message " + i);
 
-    logger.warn ("Message " + ++i);
-    root.warn("Message " + i);        
+    logger.warn("Message " + ++i);
+    root.warn("Message " + i);
 
     logger.error("Message " + ++i);
     root.error("Message " + i);
-    
+
     logger.log(Level.FATAL, "Message " + ++i);
-    root.log(Level.FATAL, "Message " + i);    
-    
+    root.log(Level.FATAL, "Message " + i);
+
     Exception e = new Exception("Just testing");
     logger.debug("Message " + ++i, e);
     root.debug("Message " + i, e);
-    
+
     logger.error("Message " + ++i, e);
-    root.error("Message " + i, e);    
+    root.error("Message " + i, e);
   }
 
   void dumpErrors(List errorList) {
-    for(int i = 0; i < errorList.size(); i++) {
+    for (int i = 0; i < errorList.size(); i++) {
       ErrorItem ei = (ErrorItem) errorList.get(i);
       System.out.println(ei);
+
       Throwable t = ei.getException();
-      if(t != null) {
+
+      if (t != null) {
         t.printStackTrace(System.out);
       }
     }
   }
-
-  public static Test suite() {
-    TestSuite suite = new TestSuite();
-    suite.addTest(new DOMTest("test1"));
-    //suite.addTest(new DOMTest("test2"));
-    return suite;
-  }
-
 }
