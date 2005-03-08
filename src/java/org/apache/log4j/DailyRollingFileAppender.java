fac8d28b69ab2a9ce0f788f1a8634f5159aed6ad
Curtis William Arnold <carnold@apache.org>
2005-03-08 20:21:18 +0000

    Bug 33914: Emulation of o.a.l.RollingFileAppender and o.a.l.DRFA
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@311312 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/DailyRollingFileAppender.java
@@ -0,0 +1,319 @@
+/*
+ * Copyright 1999,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.log4j;
+
+import org.apache.log4j.rolling.TimeBasedRollingPolicy;
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggerRepository;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.OptionHandler;
+
+import java.io.IOException;
+
+
+/**
+  * org.apache.log4j.DailyRollingFileAppender emulates earlier implementations
+  * by delegating to general purpose org.apache.log4j.rollling.RollingFileAppender
+  * introduced in log4j 1.3.  This class is provided for compatibility with
+  *  existing code and should not be used except when compatibility with version
+  * of log4j prior to 1.3 is a concern.
+  *
+  *  @author Curt Arnold
+  * @deprecated Replaced by {@link org.apache.log4j.rolling.RollingFileAppender}
+*/
+public final class DailyRollingFileAppender implements Appender, OptionHandler {
+  /**
+     The date pattern used to initiate rollover.
+  */
+  private String datePattern = "'.'yyyy-MM-dd";
+
+  /**
+   *  Nested new rolling file appender.
+   */
+  private final org.apache.log4j.rolling.RollingFileAppender rfa =
+    new org.apache.log4j.rolling.RollingFileAppender();
+
+  /**
+     The default constructor simply calls its {@link
+     FileAppender#FileAppender parents constructor}.  */
+  public DailyRollingFileAppender() {
+  }
+
+  /**
+    Instantiate a DailyRollingFileAppender and open the file designated by
+    <code>filename</code>. The opened filename will become the ouput
+    destination for this appender.
+
+  */
+  public DailyRollingFileAppender(
+    final Layout layout, final String filename, final String datePattern)
+    throws IOException {
+    rfa.setLayout(layout);
+    rfa.setFile(filename);
+    this.datePattern = datePattern;
+    activateOptions();
+  }
+
+  /**
+     The <b>DatePattern</b> takes a string in the same format as
+     expected by {@link java.text.SimpleDateFormat}. This options determines the
+     rollover schedule.
+   */
+  public void setDatePattern(String pattern) {
+    datePattern = pattern;
+  }
+
+  /** Returns the value of the <b>DatePattern</b> option. */
+  public String getDatePattern() {
+    return datePattern;
+  }
+
+  /**
+   * Prepares DailyRollingFileAppender for use.
+   */
+  public void activateOptions() {
+    TimeBasedRollingPolicy policy = new TimeBasedRollingPolicy();
+    StringBuffer pattern = new StringBuffer(rfa.getFile());
+    boolean inLiteral = false;
+    boolean inPattern = false;
+
+    for (int i = 0; i < datePattern.length(); i++) {
+      if (datePattern.charAt(i) == '\'') {
+        inLiteral = !inLiteral;
+
+        if (inLiteral && inPattern) {
+          pattern.append("}");
+          inPattern = false;
+        }
+      } else {
+        if (!inLiteral && !inPattern) {
+          pattern.append("%d{");
+          inPattern = true;
+        }
+
+        pattern.append(datePattern.charAt(i));
+      }
+    }
+
+    if (inPattern) {
+      pattern.append("}");
+    }
+
+    policy.setFileNamePattern(pattern.toString());
+    policy.activateOptions();
+    rfa.setTriggeringPolicy(policy);
+    rfa.setRollingPolicy(policy);
+
+    rfa.activateOptions();
+  }
+
+  /**
+   * An appender must be activated before it can be used.
+   */
+  public void activate() {
+    activateOptions();
+  }
+
+  /**
+   * Add a filter to the end of the filter list.
+   *
+   * @since 0.9.0
+   */
+  public void addFilter(final Filter newFilter) {
+    rfa.addFilter(newFilter);
+  }
+
+  /**
+   * Returns the head Filter. The Filters are organized in a linked list and
+   * so all Filters on this Appender are available through the result.
+   *
+   * @return the head Filter or null, if no Filters are present
+   *
+   * @since 1.1
+   */
+  public Filter getFilter() {
+    return rfa.getFilter();
+  }
+
+  /**
+   * Clear the list of filters by removing all the filters in it.
+   *
+   * @since 0.9.0
+   */
+  public void clearFilters() {
+    rfa.clearFilters();
+  }
+
+  /**
+   * Release any resources allocated within the appender such as file handles,
+   * network connections, etc.
+   *
+   * <p>
+   * It is a programming error to append to a closed appender.
+   * </p>
+   *
+   * @since 0.8.4
+   */
+  public void close() {
+    rfa.close();
+  }
+
+  /**
+   * Is this appender closed?
+   *
+   * @since 1.3
+   */
+  public boolean isClosed() {
+    return rfa.isClosed();
+  }
+
+  /**
+   * Is this appender in working order?
+   *
+   * @since 1.3
+   */
+  public boolean isActive() {
+    return rfa.isActive();
+  }
+
+  /**
+   * Log in <code>Appender</code> specific way. When appropriate, Loggers will
+   * call the <code>doAppend</code> method of appender implementations in
+   * order to log.
+   */
+  public void doAppend(final LoggingEvent event) {
+    rfa.doAppend(event);
+  }
+
+  /**
+   * Get the name of this appender. The name uniquely identifies the appender.
+   */
+  public String getName() {
+    return rfa.getName();
+  }
+
+  /**
+   * Set the {@link Layout} for this appender.
+   *
+   * @since 0.8.1
+   */
+  public void setLayout(final Layout layout) {
+    rfa.setLayout(layout);
+  }
+
+  /**
+   * Returns this appenders layout.
+   *
+   * @since 1.1
+   */
+  public Layout getLayout() {
+    return rfa.getLayout();
+  }
+
+  /**
+   * Set the name of this appender. The name is used by other components to
+   * identify this appender.
+   *
+   * @since 0.8.1
+   */
+  public void setName(final String name) {
+    rfa.setName(name);
+  }
+
+  public void setLoggerRepository(final LoggerRepository repository)
+    throws IllegalStateException {
+    rfa.setLoggerRepository(repository);
+  }
+
+  /**
+     The <b>File</b> property takes a string value which should be the
+     name of the file to append to.
+
+     <p><font color="#DD0044"><b>Note that the special values
+     "System.out" or "System.err" are no longer honored.</b></font>
+
+     <p>Note: Actual opening of the file is made when {@link
+     #activateOptions} is called, not when the options are set.  */
+  public void setFile(final String file) {
+    rfa.setFile(file);
+  }
+
+  /**
+      Returns the value of the <b>Append</b> option.
+   */
+  public boolean getAppend() {
+    return rfa.getAppend();
+  }
+
+  /** Returns the value of the <b>File</b> option. */
+  public String getFile() {
+    return rfa.getFile();
+  }
+
+  /**
+     Get the value of the <b>BufferedIO</b> option.
+
+     <p>BufferedIO will significatnly increase performance on heavily
+     loaded systems.
+
+  */
+  public boolean getBufferedIO() {
+    return rfa.getBufferedIO();
+  }
+
+  /**
+     Get the size of the IO buffer.
+  */
+  public int getBufferSize() {
+    return rfa.getBufferSize();
+  }
+
+  /**
+     The <b>Append</b> option takes a boolean value. It is set to
+     <code>true</code> by default. If true, then <code>File</code>
+     will be opened in append mode by {@link #setFile setFile} (see
+     above). Otherwise, {@link #setFile setFile} will open
+     <code>File</code> in truncate mode.
+
+     <p>Note: Actual opening of the file is made when {@link
+     #activateOptions} is called, not when the options are set.
+   */
+  public void setAppend(final boolean flag) {
+    rfa.setAppend(flag);
+  }
+
+  /**
+     The <b>BufferedIO</b> option takes a boolean value. It is set to
+     <code>false</code> by default. If true, then <code>File</code>
+     will be opened and the resulting {@link java.io.Writer} wrapped
+     around a {@link java.io.BufferedWriter}.
+
+     BufferedIO will significatnly increase performance on heavily
+     loaded systems.
+
+  */
+  public void setBufferedIO(final boolean bufferedIO) {
+    rfa.setBufferedIO(bufferedIO);
+  }
+
+  /**
+     Set the size of the IO buffer.
+  */
+  public void setBufferSize(final int bufferSize) {
+    rfa.setBufferSize(bufferSize);
+  }
+}
---
+++ tests/src/java/org/apache/log4j/rolling/ObsoleteRollingFileAppenderTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 1999,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.log4j.rolling;
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.*;
+
+import java.io.File;
+
+
+/**
+ * Tests the emulation of org.apache.log4j.RollingFileAppender
+ *
+ * @author Curt Arnold
+ * @deprecated Class under test is deprecated.
+ *
+ */
+public class ObsoleteRollingFileAppenderTest extends TestCase {
+  Logger logger = Logger.getLogger(ObsoleteRollingFileAppenderTest.class);
+  Logger root = Logger.getRootLogger();
+
+  public ObsoleteRollingFileAppenderTest(final String name) {
+    super(name);
+  }
+
+  public void setUp() {
+  }
+
+  public void tearDown() {
+    LogManager.shutdown();
+  }
+
+  /**
+   * Test basic rolling functionality.
+   */
+  public void test1() throws Exception {
+    PropertyConfigurator.configure("input/rolling/obsoleteRFA1.properties");
+
+    // Write exactly 10 bytes with each log
+    for (int i = 0; i < 25; i++) {
+      Thread.sleep(100);
+
+      if (i < 10) {
+        logger.debug("Hello---" + i);
+      } else if (i < 100) {
+        logger.debug("Hello--" + i);
+      }
+    }
+
+    assertTrue(new File("output/obsoleteRFA-test1.log").exists());
+    assertTrue(new File("output/obsoleteRFA-test1.log.1").exists());
+  }
+
+  /**
+   * Test basic rolling functionality.
+   */
+  public void test2() throws Exception {
+    PatternLayout layout = new PatternLayout("%m\n");
+    org.apache.log4j.RollingFileAppender rfa =
+      new org.apache.log4j.RollingFileAppender();
+    rfa.setName("ROLLING");
+    rfa.setLayout(layout);
+    rfa.setAppend(false);
+    rfa.setMaximumFileSize(100);
+    rfa.setFile("output/obsoleteRFA-test2.log");
+    rfa.activateOptions();
+    root.addAppender(rfa);
+
+    // Write exactly 10 bytes with each log
+    for (int i = 0; i < 25; i++) {
+      Thread.sleep(100);
+
+      if (i < 10) {
+        logger.debug("Hello---" + i);
+      } else if (i < 100) {
+        logger.debug("Hello--" + i);
+      }
+    }
+
+    assertTrue(new File("output/obsoleteRFA-test2.log").exists());
+    assertTrue(new File("output/obsoleteRFA-test2.log.1").exists());
+  }
+}
