7999a885f25e315057b4785c324f3abb09e32d6e
No Author <dev-null@apache.org>
2005-04-28 20:34:05 +0000

    This commit was manufactured by cvs2svn to create branch 'v1_2-branch'.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/branches/v1_2-branch@311327 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/filtered-java/org/slf4j/impl/JDK14Logger.java
@@ -0,0 +1,243 @@
+/* 
+ * Copyright (c) 2004-2005 SLF4J.ORG
+ * Copyright (c) 2004-2005 QOS.ch
+ * 
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to  deal in  the Software without  restriction, including
+ * without limitation  the rights to  use, copy, modify,  merge, publish,
+ * distribute, and/or sell copies of  the Software, and to permit persons
+ * to whom  the Software is furnished  to do so, provided  that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the  Software and  that both  the above  copyright notice(s)  and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+ * EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR  A PARTICULAR PURPOSE AND NONINFRINGEMENT
+ * OF  THIRD PARTY  RIGHTS. IN  NO EVENT  SHALL THE  COPYRIGHT  HOLDER OR
+ * HOLDERS  INCLUDED IN  THIS  NOTICE BE  LIABLE  FOR ANY  CLAIM, OR  ANY
+ * SPECIAL INDIRECT  OR CONSEQUENTIAL DAMAGES, OR  ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS  OF USE, DATA OR PROFITS, WHETHER  IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE  OR OTHER TORTIOUS  ACTION, ARISING OUT OF  OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * Except as  contained in  this notice, the  name of a  copyright holder
+ * shall not be used in advertising or otherwise to promote the sale, use
+ * or other dealings in this Software without prior written authorization
+ * of the copyright holder.
+ *
+ */
+
+package org.slf4j.impl;
+import org.slf4j.ULogger;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * A wrapper over @{link java.utill.Logger} which conforms to the 
+ * {@link ULogger} interface.
+ * 
+ * @author Ceki G&uuml;lc&uuml;
+ */
+public class JDK14Logger implements ULogger {
+  final Logger logger;
+
+  // WARN: JDK14Logger constructor should have only package access so that
+  // only JDK14LoggerFA be able to create one.
+  JDK14Logger(Logger logger) {
+    this.logger = logger;
+  }
+
+  /**
+   * Is the logger instance enabled for the DEBUG level?
+   * @return
+   */
+  public boolean isDebugEnabled() {
+    return logger.isLoggable(Level.FINE);
+  }
+
+  //
+
+  /**
+   * Log a message object with the DEBUG level.
+   * @param msg - the message object to be logged
+   */
+  public void debug(Object msg) {
+    logger.fine(String.valueOf(msg));
+  }
+
+  /**
+   * Log a parameterized message object at the DEBUG level.
+   *
+   * <p>This form is useful in avoiding the superflous object creation
+   * problem when invoking this method while it is disabled.
+   * </p>
+   * @param parameterizedMsg - the parameterized message object
+   * @param param1 - the parameter
+   */
+  public void debug(Object parameterizedMsg, Object param1) {
+    if (logger.isLoggable(Level.FINE)) {
+      if (parameterizedMsg instanceof String) {
+        String msgStr = (String) parameterizedMsg;
+        msgStr = MessageFormatter.format(msgStr, param1);
+        logger.fine(msgStr);
+      } else {
+        // To be failsafe, we handle the case where 'messagePattern' is not
+        // a String. Unless the user makes a mistake, this should not happen.
+        logger.fine(parameterizedMsg.toString());
+      }
+    }
+  }
+
+  /**
+   * Log a parameterized message object at the DEBUG level.
+   *
+   * <p>This form is useful in avoiding the superflous object creation
+   * problem when invoking this method while it is disabled.
+   * </p>
+   * @param parameterizedMsg - the parameterized message object
+   * @param param1 - the first parameter
+   * @param param2 - the second parameter
+   */
+  public void debug(String parameterizedMsg, Object param1, Object param2) {
+    if (logger.isLoggable(Level.FINE)) {
+      if (parameterizedMsg instanceof String) {
+        String msgStr = (String) parameterizedMsg;
+        msgStr = MessageFormatter.format(msgStr, param1, param2);
+        logger.fine(msgStr);
+      } else {
+        // To be failsafe, we handle the case where 'messagePattern' is not
+        // a String. Unless the user makes a mistake, this should not happen.
+        logger.fine(parameterizedMsg.toString());
+      }
+    }
+  }
+
+  public void debug(Object msg, Throwable t) {
+    logger.log(Level.FINE, msg.toString(), t);
+  }
+
+  public boolean isInfoEnabled() {
+    return logger.isLoggable(Level.INFO);
+  }
+
+  public void info(Object msg) {
+    logger.info(msg.toString());
+  }
+
+  public void info(Object parameterizedMsg, Object param1) {
+    if (logger.isLoggable(Level.INFO)) {
+      if (parameterizedMsg instanceof String) {
+        String msgStr = (String) parameterizedMsg;
+        msgStr = MessageFormatter.format(msgStr, param1);
+        logger.info(msgStr);
+      } else {
+        // To be failsafe, we handle the case where 'messagePattern' is not
+        // a String. Unless the user makes a mistake, this should not happen.
+        logger.info(parameterizedMsg.toString());
+      }
+    }
+  }
+
+  public void info(String parameterizedMsg, Object param1, Object param2) {
+    if (logger.isLoggable(Level.INFO)) {
+      if (parameterizedMsg instanceof String) {
+        String msgStr = (String) parameterizedMsg;
+        msgStr = MessageFormatter.format(msgStr, param1, param2);
+        logger.info(msgStr);
+      } else {
+        // To be failsafe, we handle the case where 'messagePattern' is not
+        // a String. Unless the user makes a mistake, this should not happen.
+        logger.info(parameterizedMsg.toString());
+      }
+    }
+  }
+
+  public void info(Object msg, Throwable t) {
+    logger.log(Level.INFO, msg.toString(), t);
+  }
+
+  public boolean isWarnEnabled() {
+    return logger.isLoggable(Level.WARNING);
+  }
+
+  public void warn(Object msg) {
+    logger.warning(msg.toString());
+  }
+
+  public void warn(Object parameterizedMsg, Object param1) {
+    if (logger.isLoggable(Level.WARNING)) {
+      if (parameterizedMsg instanceof String) {
+        String msgStr = (String) parameterizedMsg;
+        msgStr = MessageFormatter.format(msgStr, param1);
+        logger.warning(msgStr);
+      } else {
+        // To be failsafe, we handle the case where 'messagePattern' is not
+        // a String. Unless the user makes a mistake, this should not happen.
+        logger.warning(parameterizedMsg.toString());
+      }
+    }
+  }
+
+  public void warn(String parameterizedMsg, Object param1, Object param2) {
+    if (logger.isLoggable(Level.WARNING)) {
+      if (parameterizedMsg instanceof String) {
+        String msgStr = (String) parameterizedMsg;
+        msgStr = MessageFormatter.format(msgStr, param1, param2);
+        logger.warning(msgStr);
+      } else {
+        // To be failsafe, we handle the case where 'messagePattern' is not
+        // a String. Unless the user makes a mistake, this should not happen.
+        logger.warning(parameterizedMsg.toString());
+      }
+    }
+  }
+
+  public void warn(Object msg, Throwable t) {
+    logger.log(Level.WARNING, msg.toString(), t);
+  }
+
+  public boolean isErrorEnabled() {
+    return logger.isLoggable(Level.SEVERE);
+  }
+
+  public void error(Object msg) {
+    logger.severe(msg.toString());
+  }
+
+  public void error(Object parameterizedMsg, Object param1) {
+    if (logger.isLoggable(Level.WARNING)) {
+      if (parameterizedMsg instanceof String) {
+        String msgStr = (String) parameterizedMsg;
+        msgStr = MessageFormatter.format(msgStr, param1);
+        logger.severe(msgStr);
+      } else {
+        // To be failsafe, we handle the case where 'messagePattern' is not
+        // a String. Unless the user makes a mistake, this should not happen.
+        logger.severe(parameterizedMsg.toString());
+      }
+    }
+  }
+  
+  public void error(String parameterizedMsg, Object param1, Object param2) {
+    if (logger.isLoggable(Level.WARNING)) {
+      if (parameterizedMsg instanceof String) {
+        String msgStr = (String) parameterizedMsg;
+        msgStr = MessageFormatter.format(msgStr, param1, param2);
+        logger.severe(msgStr);
+      } else {
+        // To be failsafe, we handle the case where 'messagePattern' is not
+        // a String. Unless the user makes a mistake, this should not happen.
+        logger.severe(parameterizedMsg.toString());
+      }
+    }
+  }
+  public void error(Object msg, Throwable t) {
+    logger.log(Level.SEVERE, msg.toString(), t);
+  }
+}
---
+++ src/java/org/slf4j/impl/MessageFormatter.java
@@ -0,0 +1,143 @@
+/* 
+ * Copyright (c) 2004-2005 SLF4J.ORG
+ * Copyright (c) 2004-2005 QOS.ch
+ *
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to  deal in  the Software without  restriction, including
+ * without limitation  the rights to  use, copy, modify,  merge, publish,
+ * distribute, and/or sell copies of  the Software, and to permit persons
+ * to whom  the Software is furnished  to do so, provided  that the above
+ * copyright notice(s) and this permission notice appear in all copies of
+ * the  Software and  that both  the above  copyright notice(s)  and this
+ * permission notice appear in supporting documentation.
+ * 
+ * THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+ * EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR  A PARTICULAR PURPOSE AND NONINFRINGEMENT
+ * OF  THIRD PARTY  RIGHTS. IN  NO EVENT  SHALL THE  COPYRIGHT  HOLDER OR
+ * HOLDERS  INCLUDED IN  THIS  NOTICE BE  LIABLE  FOR ANY  CLAIM, OR  ANY
+ * SPECIAL INDIRECT  OR CONSEQUENTIAL DAMAGES, OR  ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS  OF USE, DATA OR PROFITS, WHETHER  IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE  OR OTHER TORTIOUS  ACTION, ARISING OUT OF  OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * Except as  contained in  this notice, the  name of a  copyright holder
+ * shall not be used in advertising or otherwise to promote the sale, use
+ * or other dealings in this Software without prior written authorization
+ * of the copyright holder.
+ *
+ */
+
+package org.slf4j.impl;
+
+
+/**
+ * Formats messages according to very simple rules. 
+ * See {@link #format(String, Object)} and 
+ * {@link #format(String, Object, Object)} for more details.
+ *
+ * @author Ceki G&uuml;lc&uuml;
+ */
+public class MessageFormatter {
+  static final char DELIM_START = '{';
+  static final char DELIM_STOP = '}';
+
+  /**
+   * Performs single argument substitution for the 'messagePattern' passed as
+   * parameter.
+   * <p>
+   * For example, <code>MessageFormatter.format("Hi {}.", "there");</code> will
+   * return the string "Hi there.".
+   * <p>
+   * The {} pair is called the formatting element. It serves to designate the
+   * location where the argument needs to be inserted within the pattern.
+   * 
+   * @param messagePattern The message pattern which will be parsed and formatted
+   * @param argument The argument to be inserted instead of the formatting element
+   * @return The formatted message
+   */
+  public static String format(String messagePattern, Object argument) {
+    int j = messagePattern.indexOf(DELIM_START);
+    int len = messagePattern.length();
+    char escape = 'x';
+
+    // if there are no { characters or { is the last character of the messsage
+    // then we just return messagePattern
+    if (j == -1 || (j+1 == len)) {
+      return messagePattern;
+    } else {
+      if(j+1 == len) {
+      }
+      
+      char delimStop = messagePattern.charAt(j + 1);
+      if (j > 0) {
+        escape = messagePattern.charAt(j - 1);
+      }
+      if ((delimStop != DELIM_STOP) || (escape == '\\')) {
+        // invalid DELIM_START/DELIM_STOP pair or espace character is
+        // present
+        return messagePattern;
+      } else {
+        StringBuffer sbuf = new StringBuffer(len + 20);
+        sbuf.append(messagePattern.substring(0, j));
+        sbuf.append(argument);
+        sbuf.append(messagePattern.substring(j + 2));
+        return sbuf.toString();
+      }
+    }
+  }
+
+  /**
+   * /**
+   * Performs a two argument substitution for the 'messagePattern' passed as
+   * parameter.
+   * <p>
+   * For example, <code>MessageFormatter.format("Hi {}. My name is {}.", 
+   * "there", "David");</code> will return the string "Hi there. My name is David.".
+   * <p>
+   * The '{}' pair is called a formatting element. It serves to designate the
+   * location where the arguments need to be inserted within the message pattern.
+   * 
+   * @param messagePattern The message pattern which will be parsed and formatted
+   * @param arg1 The first argument to replace the first formatting element
+   * @param arg2 The second argument to replace the second formatting element
+   * @return The formatted message
+   */
+  public static String format(String messagePattern, Object arg1, Object arg2) {
+    int i = 0;
+    int len = messagePattern.length();
+    int j = messagePattern.indexOf(DELIM_START);
+
+    StringBuffer sbuf = new StringBuffer(messagePattern.length() + 50);
+
+    for (int L = 0; L < 2; L++) {
+      j = messagePattern.indexOf(DELIM_START, i);
+
+      if (j == -1 || (j+1 == len)) {
+        // no more variables
+        if (i == 0) { // this is a simple string
+          return messagePattern;
+        } else { // add the tail string which contains no variables and return the result.
+          sbuf.append(messagePattern.substring(i, messagePattern.length()));
+          return sbuf.toString();
+        }
+      } else {
+        char delimStop = messagePattern.charAt(j + 1);
+        if ((delimStop != DELIM_STOP)) {
+          // invalid DELIM_START/DELIM_STOP pair
+          sbuf.append(messagePattern.substring(i, messagePattern.length()));
+          return sbuf.toString();
+        }
+        sbuf.append(messagePattern.substring(i, j));
+        sbuf.append((L == 0) ? arg1 : arg2);
+        i = j + 2;
+      }
+    }
+    // append the characters following the second {} pair.
+    sbuf.append(messagePattern.substring(i, messagePattern.length()));
+    return sbuf.toString();
+  }
+}
