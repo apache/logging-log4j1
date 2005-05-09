82915e2515074384de8db345f3f67fc30f125743
Curtis William Arnold <carnold@apache.org>
2005-05-09 22:31:36 +0000

    Bug 34827: LoggingEvent serialization unit tests
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@311343 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/src/java/org/apache/log4j/CoreTestSuite.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 1999,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.log4j;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.apache.log4j.spi.LoggingEventTest;
+
+
+/**
+ * Suite of log4j class level unit tests.
+ *
+ * @author Curt Arnold
+ * @since 1.3
+ */
+public class CoreTestSuite {
+    /**
+     * Constructs test suite.
+     * @return test suite
+     */
+    public static Test suite() {
+        TestSuite s = new TestSuite();
+        s.addTestSuite(LoggingEventTest.class);
+        return s;
+    }
+}
