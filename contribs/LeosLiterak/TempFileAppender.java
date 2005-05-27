3de5c08a99b95fcb53036b3857b49665532b5b08
Mark Dwayne Womack <mwomack@apache.org>
2005-05-27 03:27:57 +0000

    Fix for #35032.  Added license header to .java files that did not already have a license.
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/branches/v1_2-branch@311380 13f79535-47bb-0310-9956-ffa450edef68

--- contribs/LeosLiterak/TempFileAppender.java
+++ contribs/LeosLiterak/TempFileAppender.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
  package org.apache.log4j;
  
 import java.io.File;
@@ -172,7 +187,11 @@ public class TempFileAppender extends AppenderSkeleton {
  * @since $Date$
  *
  * $Log$
+ * Revision 1.1.2.1  2005/05/27 03:27:54  mwomack
+ * Fix for #35032.  Added license header to .java files that did not already have a license.
+ *
  * Revision 1.1  2001/04/20 17:38:31  ceki
+ *
  * Added LeosLiterak's TempFileAppender.java
  *
 */
