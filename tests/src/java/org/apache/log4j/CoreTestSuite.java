bfce823b79df7b122566f63c6680ce929ee32059
Curtis William Arnold <carnold@apache.org>
2005-07-18 16:19:00 +0000

    Bug 26433: Level serialization
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@311424 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/java/org/apache/log4j/CoreTestSuite.java
+++ tests/src/java/org/apache/log4j/CoreTestSuite.java
@@ -38,6 +38,7 @@ public class CoreTestSuite {
         s.addTestSuite(org.apache.log4j.pattern.PatternParserTest.class);
         s.addTestSuite(org.apache.log4j.rolling.helper.FileNamePatternTestCase.class);
         s.addTestSuite(org.apache.log4j.pattern.FormattingInfoTest.class);
+        s.addTestSuite(org.apache.log4j.LevelTest.class);
         return s;
     }
 }
