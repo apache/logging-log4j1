2b6dc1113c8d76046535e3d188723fe12ec1a7f2
Curtis William Arnold <carnold@apache.org>
2005-07-22 18:57:02 +0000

    Bug 35052: Problem with String.intern() in CategoryKey
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/branches/v1_2-branch@311436 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/CategoryKey.java
+++ src/java/org/apache/log4j/CategoryKey.java
@@ -17,7 +17,8 @@
 package org.apache.log4j;
 
 /**
-   CategoryKey is heavily used internally to accelerate hash table searches.
+   CategoryKey is a wrapper for String that apparently accellerated
+   hash table lookup in early JVM's.
    @author Ceki G&uuml;lc&uuml; 
 */
 class CategoryKey {
@@ -26,7 +27,7 @@ class CategoryKey {
   int hashCache;
 
   CategoryKey(String name) {
-    this.name = name.intern();
+    this.name = name;
     hashCache = name.hashCode();
   }
 
@@ -43,7 +44,7 @@ class CategoryKey {
       return true;
     
     if(rArg != null && CategoryKey.class == rArg.getClass()) 
-      return  name == ((CategoryKey)rArg ).name;
+      return  name.equals(((CategoryKey)rArg ).name);
     else 
       return false;
   }
