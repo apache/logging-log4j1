1457a37d8b05b4bec30e8d597157f007dd03f731
Yoav Shapira <yoavs@apache.org>
2005-07-28 19:12:46 +0000

    Fixed Bugzilla 30853 (http://issues.apache.org/bugzilla/show_bug.cgi?id=30853).
    Note that this fix introduces usage of the log4j.debug system property again.
    While we want to stay away from this system property, I'm not sure a better
    solution can be had since this initial debug information happens prior
    to Logger setup.  If an alternative solution is found without using this
    system property, I'd be all for replacing my fix with the better one.
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@311461 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/LogManager.java
+++ src/java/org/apache/log4j/LogManager.java
@@ -40,187 +40,218 @@ import java.util.Enumeration;
  *
  * @author Ceki G&uuml;lc&uuml; */
 public class LogManager {
-  private static Object guard = null;
-  private static RepositorySelector repositorySelector;
-  //private static Scheduler schedulerInstance = null;
-
-  /**
-   * The default LoggerRepository instance created by LogManager. This instance
-   * is provided for the convenience of the {@link RepositorySelector} instance.
-   * The selector, if it choses, may ignore this default repository.
-   */
-  public final static LoggerRepository defaultLoggerRepository;
-  
-
-  
-  // The following static initializer gets invoked immediately after a call to 
-  // Logger.getLogger() is made. Here is a description of the static initializer.
-  // 
-  // create defaultLoggerRepository
-  // configure(defaultLoggerRepository) depending on system properties
-  // during the configuration of defaultLoggerRepository a temporary repository 
-  // selector is used.
-  //
-  // 
-  static {
-    //System.out.println("**Start of LogManager static initializer");
-    defaultLoggerRepository = new Hierarchy(new RootLogger(Level.DEBUG));
-    defaultLoggerRepository.setName(Constants.DEFAULT_REPOSITORY_NAME);
-    
-    // temporary repository
-    repositorySelector = new DefaultRepositorySelector(defaultLoggerRepository);
-    
-    //  Attempt to perform automatic configuration of the default repository
-    String configuratorClassName =
-      OptionConverter.getSystemProperty(Constants.CONFIGURATOR_CLASS_KEY, null);
-    String configurationOptionStr = 
-      OptionConverter.getSystemProperty(Constants.DEFAULT_CONFIGURATION_KEY, null);
-
-    if (configurationOptionStr == null) {
-      if (Loader.getResource(Constants.DEFAULT_XML_CONFIGURATION_FILE) != null) {
-        configurationOptionStr = Constants.DEFAULT_XML_CONFIGURATION_FILE;
-      } else if (
-        Loader.getResource(Constants.DEFAULT_CONFIGURATION_FILE) != null) {
-        configurationOptionStr = Constants.DEFAULT_CONFIGURATION_FILE;
-      }
-    }
-
-    System.out.println("*** configurationOptionStr=" + configurationOptionStr);
-
-    IntializationUtil.initialConfiguration(
-        defaultLoggerRepository, configurationOptionStr, configuratorClassName);
-    
-    String repositorySelectorStr = 
-      OptionConverter.getSystemProperty("log4j.repositorySelector", null);
-
-    if (repositorySelectorStr == null) {
-      // NOTHING TO DO, the default repository has been configured already
-    } else if (repositorySelectorStr.equalsIgnoreCase("JNDI")) {
-      System.out.println("*** Will use ContextJNDISelector **");
-      repositorySelector = new ContextJNDISelector();
-      guard = new Object();
-    } else {
-      Object r =
-        OptionConverter.instantiateByClassName(
-          repositorySelectorStr, RepositorySelector.class, null);
-
-      if (r instanceof RepositorySelector) {
-        System.out.println(
-          "*** Using [" + repositorySelectorStr
-          + "] instance as repository selector.");
-        repositorySelector = (RepositorySelector) r;
-        guard = new Object();
-      } else {
-        System.out.println(
-          "*** Could not insantiate [" + repositorySelectorStr
-          + "] as repository selector.");
-        System.out.println("*** Using default repository selector");
-        repositorySelector = new DefaultRepositorySelector(defaultLoggerRepository);
-      }
-    }
-
-    System.out.println("** End of LogManager static initializer");
-  }
-
-  /**
-     Sets <code>RepositorySelector</code> but only if the correct
-     <em>guard</em> is passed as parameter.
-
-     <p>Initally the guard is null, unless the JVM is started with the
-     log4j.repositorySelector system property
-     (-Dlog4j.repositorySelector=[JNDI | <fully qualified class name>]).
-     If the guard is
-     <code>null</code>, then invoking this method sets the logger
-     repository and the guard. Following invocations will throw a {@link
-     IllegalArgumentException}, unless the previously set
-     <code>guard</code> is passed as the second parameter.
-
-     <p>This allows a high-level component to set the {@link
-     RepositorySelector} used by the <code>LogManager</code>.
-
-     <p>For example, when tomcat starts it will be able to install its
-     own repository selector. However, if and when Tomcat is embedded
-     within JBoss, then JBoss will install its own repository selector
-     and Tomcat will use the repository selector set by its container,
-     JBoss.  
+    /**
+     * Concurrency guard.
      */
-  public static void setRepositorySelector(
-    RepositorySelector selector, Object guard) throws IllegalArgumentException {
-    if ((LogManager.guard != null) && (LogManager.guard != guard)) {
-      throw new IllegalArgumentException(
-        "Attempted to reset the LoggerFactory without possessing the guard.");
-    }
-    if (selector == null) {
-      throw new IllegalArgumentException(
-        "RepositorySelector must be non-null.");
-    }
+    private static Object guard = null;
 
-    LogManager.guard = guard;
-    LogManager.repositorySelector = selector;
-  }
+    /**
+     * The repository selector.
+     */
+    private static RepositorySelector repositorySelector;
 
+    /**
+     * The debug flag, false by default.
+     * True will cause debug printing to System.out.
+     * Modify via system property log4j.debug.
+     */
+    private static boolean debug = false;
 
-  /**
-   * Return the repository selector currently in use.
-   * 
-   * @since 1.3
-   * @return {@link RepositorySelector} currently in use.
-   */
-  public static RepositorySelector getRepositorySelector() {
-    return  LogManager.repositorySelector;
-  } 
+    /**
+     * The default LoggerRepository instance created by LogManager. This instance
+     * is provided for the convenience of the {@link RepositorySelector} instance.
+     * The selector, if it choses, may ignore this default repository.
+     */
+    public static final LoggerRepository defaultLoggerRepository;
   
-  public static LoggerRepository getLoggerRepository() {
-    return repositorySelector.getLoggerRepository();
-  }
 
-  /**
-     Retrieve the appropriate root logger.
-   */
-  public static Logger getRootLogger() {
-    // Delegate the actual manufacturing of the logger to the logger repository.
-    return repositorySelector.getLoggerRepository().getRootLogger();
-  }
+    /**
+     * The following static initializer gets invoked immediately after a call to 
+     * Logger.getLogger() is made. Here is a description of the static initializer:
+     *
+     * - Create defaultLoggerRepository,
+     * - Configure(defaultLoggerRepository) depending on system properties,
+     *   during the configuration of defaultLoggerRepository a temporary repository 
+     *   selector is used.
+     */
+    static {
+        // Check debug
+        String debugProp = System.getProperty("log4j.debug");
+        if(Boolean.valueOf(debugProp).booleanValue()) {
+            debug = true;
+        }
 
-  /**
-     Retrieve the appropriate {@link Logger} instance.
-  */
-  public static Logger getLogger(String name) {
-    //LogLog.info("LogManager.getLogger("+name+") called");
-    // Delegate the actual manufacturing of the logger to the logger repository.
-    return repositorySelector.getLoggerRepository().getLogger(name);
-  }
+        if(debug) {
+            System.out.println("**Start of LogManager static initializer");
+        }
 
-  /**
-      Retrieve the appropriate {@link Logger} instance.
-   */
-  public static Logger getLogger(Class clazz) {
-    // Delegate the actual manufacturing of the logger to the logger repository.
-    return repositorySelector.getLoggerRepository().getLogger(clazz.getName());
-  }
+        defaultLoggerRepository = new Hierarchy(new RootLogger(Level.DEBUG));
+        defaultLoggerRepository.setName(Constants.DEFAULT_REPOSITORY_NAME);
+        
+        // temporary repository
+        repositorySelector = new DefaultRepositorySelector(defaultLoggerRepository);
+        
+        //  Attempt to perform automatic configuration of the default repository
+        String configuratorClassName =
+            OptionConverter.getSystemProperty(Constants.CONFIGURATOR_CLASS_KEY, null);
+        String configurationOptionStr = 
+            OptionConverter.getSystemProperty(Constants.DEFAULT_CONFIGURATION_KEY, null);
+        
+        if (configurationOptionStr == null) {
+            if (Loader.getResource(Constants.DEFAULT_XML_CONFIGURATION_FILE) != null) {
+                configurationOptionStr = Constants.DEFAULT_XML_CONFIGURATION_FILE;
+            } else if (
+                       Loader.getResource(Constants.DEFAULT_CONFIGURATION_FILE) != null) {
+                configurationOptionStr = Constants.DEFAULT_CONFIGURATION_FILE;
+            }
+        }
+        
+        if(debug) {
+            System.out.println("*** configurationOptionStr=" + configurationOptionStr);
+        }
+      
+        IntializationUtil.initialConfiguration(
+                                               defaultLoggerRepository, configurationOptionStr, configuratorClassName);
+        
+        String repositorySelectorStr = 
+            OptionConverter.getSystemProperty("log4j.repositorySelector", null);
+        
+        if (repositorySelectorStr == null) {
+            // NOTHING TO DO, the default repository has been configured already
+        } else if (repositorySelectorStr.equalsIgnoreCase("JNDI")) {
+            if(debug) {
+                System.out.println("*** Will use ContextJNDISelector **");
+            }
 
-  /**
-     Retrieve the appropriate {@link Logger} instance.
-  */
-  public static Logger getLogger(String name, LoggerFactory factory) {
-    // Delegate the actual manufacturing of the logger to the logger repository.
-    return repositorySelector.getLoggerRepository().getLogger(name, factory);
-  }
+            repositorySelector = new ContextJNDISelector();
+            guard = new Object();
+        } else {
+            Object r =
+                OptionConverter.instantiateByClassName(
+                                                       repositorySelectorStr, RepositorySelector.class, null);
+            
+            if (r instanceof RepositorySelector) {
+                if(debug) {
+                    System.out.println(
+                                       "*** Using [" + repositorySelectorStr
+                                       + "] instance as repository selector.");
+                }
+                repositorySelector = (RepositorySelector) r;
+                guard = new Object();
+            } else {
+                if(debug) {
+                    System.out.println(
+                                       "*** Could not insantiate [" + repositorySelectorStr
+                                       + "] as repository selector.");
+                    System.out.println("*** Using default repository selector");
+                }
 
-  public static Logger exists(String name) {
-    return repositorySelector.getLoggerRepository().exists(name);
-  }
+                repositorySelector = new DefaultRepositorySelector(defaultLoggerRepository);
+            }
+        }
 
-  public static Enumeration getCurrentLoggers() {
-    return repositorySelector.getLoggerRepository().getCurrentLoggers();
-  }
+        if(debug) {
+            System.out.println("** End of LogManager static initializer");
+        }
+    }
 
-  public static void shutdown() {
-    repositorySelector.getLoggerRepository().shutdown();
-  }
-
-  public static void resetConfiguration() {
-    repositorySelector.getLoggerRepository().resetConfiguration();
-  }
-
+    /**
+       Sets <code>RepositorySelector</code> but only if the correct
+       <em>guard</em> is passed as parameter.
+       
+       <p>Initally the guard is null, unless the JVM is started with the
+       log4j.repositorySelector system property
+       (-Dlog4j.repositorySelector=[JNDI | <fully qualified class name>]).
+       If the guard is
+       <code>null</code>, then invoking this method sets the logger
+       repository and the guard. Following invocations will throw a {@link
+       IllegalArgumentException}, unless the previously set
+       <code>guard</code> is passed as the second parameter.
+       
+       <p>This allows a high-level component to set the {@link
+       RepositorySelector} used by the <code>LogManager</code>.
+       
+       <p>For example, when tomcat starts it will be able to install its
+       own repository selector. However, if and when Tomcat is embedded
+       within JBoss, then JBoss will install its own repository selector
+       and Tomcat will use the repository selector set by its container,
+       JBoss.  
+    */
+    public static void setRepositorySelector(
+                                             RepositorySelector selector, Object guard) throws IllegalArgumentException {
+        if ((LogManager.guard != null) && (LogManager.guard != guard)) {
+            throw new IllegalArgumentException(
+                                               "Attempted to reset the LoggerFactory without possessing the guard.");
+        }
+        if (selector == null) {
+            throw new IllegalArgumentException(
+                                               "RepositorySelector must be non-null.");
+        }
+        
+        LogManager.guard = guard;
+        LogManager.repositorySelector = selector;
+    }
+    
+    
+    /**
+     * Return the repository selector currently in use.
+     * 
+     * @since 1.3
+     * @return {@link RepositorySelector} currently in use.
+     */
+    public static RepositorySelector getRepositorySelector() {
+        return  LogManager.repositorySelector;
+    } 
+    
+    public static LoggerRepository getLoggerRepository() {
+        return repositorySelector.getLoggerRepository();
+    }
+    
+    /**
+       Retrieve the appropriate root logger.
+    */
+    public static Logger getRootLogger() {
+        // Delegate the actual manufacturing of the logger to the logger repository.
+        return repositorySelector.getLoggerRepository().getRootLogger();
+    }
+    
+    /**
+       Retrieve the appropriate {@link Logger} instance.
+    */
+    public static Logger getLogger(String name) {
+        // Delegate the actual manufacturing of the logger to the logger repository.
+        return repositorySelector.getLoggerRepository().getLogger(name);
+    }
+    
+    /**
+       Retrieve the appropriate {@link Logger} instance.
+    */
+    public static Logger getLogger(Class clazz) {
+        // Delegate the actual manufacturing of the logger to the logger repository.
+        return repositorySelector.getLoggerRepository().getLogger(clazz.getName());
+    }
+    
+    /**
+       Retrieve the appropriate {@link Logger} instance.
+    */
+    public static Logger getLogger(String name, LoggerFactory factory) {
+        // Delegate the actual manufacturing of the logger to the logger repository.
+        return repositorySelector.getLoggerRepository().getLogger(name, factory);
+    }
+    
+    public static Logger exists(String name) {
+        return repositorySelector.getLoggerRepository().exists(name);
+    }
+    
+    public static Enumeration getCurrentLoggers() {
+        return repositorySelector.getLoggerRepository().getCurrentLoggers();
+    }
+    
+    public static void shutdown() {
+        repositorySelector.getLoggerRepository().shutdown();
+    }
+    
+    public static void resetConfiguration() {
+        repositorySelector.getLoggerRepository().resetConfiguration();
+    }
 }
