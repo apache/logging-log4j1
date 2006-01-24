df329256ddcbffc2e56bb8abc0a745e27c479a15
Curtis William Arnold <carnold@apache.org>
2006-01-24 19:10:32 +0000

    Update ASL 1.1 notices to ASL 2.0
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@371983 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/java/org/apache/log4j/HierarchyThresholdTestCase.java
+++ tests/src/java/org/apache/log4j/HierarchyThresholdTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999,2004 The Apache Software Foundation.
+ * Copyright 1999,2006 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,6 @@
  * limitations under the License.
  */
 
-/* Copyright (C) The Apache Software Foundation. All rights reserved.
- *
- * This software is published under the terms of the Apache Software License
- * version 1.1, a copy of which has been included  with this distribution in
- * the LICENSE.txt file.
- */
 package org.apache.log4j;
 
 import junit.framework.Test;
