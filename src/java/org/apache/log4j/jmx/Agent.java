2c8eff5d343135cbb201f93d1f4db8d59e68df71
Curtis William Arnold <carnold@apache.org>
2006-01-25 21:31:56 +0000

    Bug 35452: Restoring o.a.l.jmx classes from 1.2 branch
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@372319 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/log4j/jmx/Agent.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 1999-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.log4j.jmx;
+
+import javax.management.ObjectName;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import com.sun.jdmk.comm.HtmlAdaptorServer;
+
+import org.apache.log4j.Logger;
+
+
+public class Agent {
+
+  static Logger log = Logger.getLogger(Agent.class);
+
+  public Agent() {
+  }
+
+
+  public
+  void start() {
+
+    MBeanServer server = MBeanServerFactory.createMBeanServer();
+    HtmlAdaptorServer html = new HtmlAdaptorServer();
+
+    try {
+      log.info("Registering HtmlAdaptorServer instance.");
+      server.registerMBean(html, new ObjectName("Adaptor:name=html,port=8082"));
+      log.info("Registering HierarchyDynamicMBean instance.");
+      HierarchyDynamicMBean hdm = new HierarchyDynamicMBean();
+      server.registerMBean(hdm, new ObjectName("log4j:hiearchy=default"));
+
+    } catch(Exception e) {
+      log.error("Problem while regitering MBeans instances.", e);
+      return;
+    }
+    html.start();
+  }
+}
