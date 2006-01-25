ae88a4a5a94530500baee5037363ef18b205244b
Mark Dwayne Womack <mwomack@apache.org>
2006-01-25 05:04:27 +0000

    Reverting recent changes to FileWatchdogTestCase.java
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@372104 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/java/org/apache/log4j/watchdog/FileWatchdogTestCase.java
+++ tests/src/java/org/apache/log4j/watchdog/FileWatchdogTestCase.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999,2006 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +34,7 @@ import org.apache.log4j.Level;
 public class FileWatchdogTestCase extends TestCase {
 
     private Logger logger = Logger.getLogger(FileWatchdogTestCase.class);
-    
+
     static String SOURCE_CONFIG = "input/watchdog/watchdog.FileWatchdog";
     static String FILE = "output/watchdog.FileWatchdog";
     static String WITNESS = "witness/watchdog/watchdog.FileWatchdog";
@@ -55,40 +55,40 @@ public class FileWatchdogTestCase extends TestCase {
         size = in.read(buffer);
         if (size > 0) out.write(buffer,0,size);
       } while (size > 0);
-      
+
       try {
         in.close();
       } catch (Exception e) {
         // don't care
       }
-      
+
       try {
         out.close();
       } catch (Exception e) {
         // don't care
       }
     }
-    
+
     private String getSourceXMLConfigFile(String caseName, int index) {
 
         return SOURCE_CONFIG + "." + caseName + "_" + index + ".xml";
     }
 
     private String getXMLConfigFile(String caseName) {
-      
+
       return FILE + "." + caseName + ".xml";
     }
-    
+
     private String getSourceConfigFile(String caseName, int index) {
 
         return SOURCE_CONFIG + "." + caseName + "_" + index + ".properties";
     }
 
     private String getConfigFile(String caseName) {
-      
+
       return FILE + "." + caseName + ".properties";
     }
-    
+
     private String getOutputFile(String caseName) {
 
       return FILE + "." + caseName + ".txt";
@@ -98,7 +98,7 @@ public class FileWatchdogTestCase extends TestCase {
 
       return WITNESS + "." + caseName + ".txt";
     }
-    
+
     // basic test of plugin in standalone mode
     public void test1() throws Exception {
       File outFile = new File(getOutputFile("test1"));
@@ -106,28 +106,28 @@ public class FileWatchdogTestCase extends TestCase {
           assertTrue(outFile.delete());
       }
 
-      
+
       // set up the needed file references
       File sourceFile1 = new File(getSourceXMLConfigFile("test1", 1));
       File sourceFile2 = new File(getSourceXMLConfigFile("test1", 2));
       assertTrue(sourceFile1.exists());
       assertTrue(sourceFile2.exists());
-      
+
       File configFile = new File(getXMLConfigFile("test1"));
-      
+
       // move the first config file into place
       copyFile(sourceFile1, configFile);
       assertTrue(configFile.exists());
-      
+
       // configure environment to first config file
       JoranConfigurator configurator = new JoranConfigurator();
       configurator.doConfigure(configFile.getAbsolutePath(),
           LogManager.getLoggerRepository());
-      
+
       // now watch the file for changes
       FileWatchdog watchdog = new FileWatchdog();
       watchdog.setFile(configFile.getAbsolutePath());
-      watchdog.setInterval(500);
+      watchdog.setInterval(1000);
       watchdog.setConfigurator(JoranConfigurator.class.getName());
       ((LoggerRepositoryEx) LogManager.getLoggerRepository()).getPluginRegistry().addPlugin(watchdog);
       watchdog.activateOptions();
@@ -139,13 +139,13 @@ public class FileWatchdogTestCase extends TestCase {
       logger.error("error message");
       logger.fatal("fatal message");
 
+      Thread.sleep(2000);
       // copy over a new version of the config file
       copyFile(sourceFile2, configFile);
-      configFile.setLastModified(System.currentTimeMillis());
-      
-      // wait until it appears the watchdog reacts to the file change
-      for (int i = 0; i < 20; i++) {
-          Thread.sleep(1000);
+
+      // wait a few seconds for the watchdog to react
+      for (int i = 0; i < 40; i++) {
+          Thread.sleep(500);
           if (logger.getLevel() == Level.INFO) {
               // output some test messages
               logger.debug("debug message");
@@ -161,7 +161,7 @@ public class FileWatchdogTestCase extends TestCase {
       }
       fail("Expected change in level did not occur within 20 seconds.");
     }
-    
+
     // basic test of plugin in standalone mode with PropertyConfigurator
     public void test2() throws Exception {
       File outFile = new File(getOutputFile("test2"));
@@ -174,22 +174,22 @@ public class FileWatchdogTestCase extends TestCase {
       File sourceFile2 = new File(getSourceConfigFile("test2", 2));
       assertTrue(sourceFile1.exists());
       assertTrue(sourceFile2.exists());
-      
+
       File configFile = new File(getConfigFile("test2"));
-      
+
       // move the first config file into place
       copyFile(sourceFile1, configFile);
       assertTrue(configFile.exists());
-      
+
       // configure environment to first config file
       PropertyConfigurator configurator = new PropertyConfigurator();
       configurator.doConfigure(configFile.getAbsolutePath(),
           LogManager.getLoggerRepository());
-      
+
       // now watch the file for changes
       FileWatchdog watchdog = new FileWatchdog();
       watchdog.setFile(configFile.getAbsolutePath());
-      watchdog.setInterval(500);
+      watchdog.setInterval(1000);
       watchdog.setConfigurator(PropertyConfigurator.class.getName());
       ((LoggerRepositoryEx) LogManager.getLoggerRepository()).getPluginRegistry().addPlugin(watchdog);
       watchdog.activateOptions();
@@ -201,13 +201,13 @@ public class FileWatchdogTestCase extends TestCase {
       logger.error("error message");
       logger.fatal("fatal message");
 
+      Thread.sleep(2000);
       // copy over a new version of the config file
       copyFile(sourceFile2, configFile);
-      configFile.setLastModified(System.currentTimeMillis());
 
       // wait a few seconds for the watchdog to react
-      for (int i = 0; i < 20; i++) {
-          Thread.sleep(1000);
+      for (int i = 0; i < 40; i++) {
+          Thread.sleep(500);
           if (logger.getLevel() == Level.INFO) {
             // output some test messages
             logger.debug("debug message");
@@ -215,7 +215,7 @@ public class FileWatchdogTestCase extends TestCase {
             logger.warn("warn message");
             logger.error("error message");
             logger.fatal("fatal message");
-      
+
             assertTrue(Compare.compare(getOutputFile("test2"),
                 getWitnessFile("test2")));
             return;
