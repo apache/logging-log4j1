17eb23df525e8b287af97220be25c82a00318c07
Curtis William Arnold <carnold@apache.org>
2006-08-01 23:13:25 +0000

    Bug 35123: Additivity not exported by PropertyPrinter
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@427789 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/config/PropertyPrinter.java
+++ src/java/org/apache/log4j/config/PropertyPrinter.java
@@ -119,6 +119,9 @@ public class PropertyPrinter implements PropertyGetter.PropertyCallback {
     if (appenderString != "") {
       out.println(catKey + "=" + appenderString);
     }
+    if (!cat.getAdditivity() && cat != Logger.getRootLogger()) {
+    	out.println("log4j.additivity." + cat.getName() + "=false");    
+    }    
   }
   
   protected
