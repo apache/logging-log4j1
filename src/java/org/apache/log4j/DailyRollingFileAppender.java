321e531829302bad40dcbee87da93b6118266e92
Curtis William Arnold <carnold@apache.org>
2006-09-01 05:13:58 +0000

    Bug 38559: Monthly logs not generated at midnight with DRFA
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/branches/v1_2-branch@439184 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/DailyRollingFileAppender.java
+++ src/java/org/apache/log4j/DailyRollingFileAppender.java
@@ -421,6 +421,7 @@ class RollingCalendar extends GregorianCalendar {
     case DailyRollingFileAppender.TOP_OF_WEEK:
 	this.set(Calendar.DAY_OF_WEEK, getFirstDayOfWeek());
 	this.set(Calendar.HOUR_OF_DAY, 0);
+	this.set(Calendar.MINUTE, 0);
 	this.set(Calendar.SECOND, 0);
 	this.set(Calendar.MILLISECOND, 0);
 	this.add(Calendar.WEEK_OF_YEAR, 1);
@@ -428,6 +429,7 @@ class RollingCalendar extends GregorianCalendar {
     case DailyRollingFileAppender.TOP_OF_MONTH:
 	this.set(Calendar.DATE, 1);
 	this.set(Calendar.HOUR_OF_DAY, 0);
+	this.set(Calendar.MINUTE, 0);
 	this.set(Calendar.SECOND, 0);
 	this.set(Calendar.MILLISECOND, 0);
 	this.add(Calendar.MONTH, 1);
