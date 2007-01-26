5446796dd3b3e250dbf2c87a74f79ce2c771ed84
Elias Nathan Ross <eross@apache.org>
2007-01-26 22:22:41 +0000

    Bug 17531 - Add "log4j.reset" to property configurator as well as XML/Joran one
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@500384 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/PropertyConfigurator.java
+++ src/java/org/apache/log4j/PropertyConfigurator.java
@@ -96,6 +96,11 @@ public class PropertyConfigurator extends ConfiguratorBase
   static final String RENDERER_PREFIX = "log4j.renderer.";
   static final String THRESHOLD_PREFIX = "log4j.threshold";
 
+  /**
+   * Makes log4j reset the configuration by setting it to true.
+   */ 
+  static final String RESET_KEY = "log4j.reset";
+  
   /** Key for specifying the {@link org.apache.log4j.spi.LoggerFactory
       LoggerFactory}.  Currently set to "<code>log4j.loggerFactory</code>".  */
   public static final String LOGGER_FACTORY_KEY = "log4j.loggerFactory";
@@ -407,16 +412,21 @@ public class PropertyConfigurator extends ConfiguratorBase
      See {@link #doConfigure(String, LoggerRepository)} for the expected format.
   */
   public void doConfigure(Properties properties, LoggerRepository repository) {
-    String value = properties.getProperty(DEBUG_KEY);
+    String debug = properties.getProperty(DEBUG_KEY);
+    String reset = properties.getProperty(RESET_KEY);
 
     try {
       // we start by attaching a temporary list appender
       attachListAppender(repository);
 
-      boolean attachedConsoleApepnder = false;
-      if ((value != null) && OptionConverter.toBoolean(value, true)) {
+      if (OptionConverter.toBoolean(reset, false)) {
+        repository.resetConfiguration();
+      }
+      
+      boolean attachedConsoleAppender = false;
+      if ((debug != null) && OptionConverter.toBoolean(debug, true)) {
         attachTemporaryConsoleAppender(repository);
-        attachedConsoleApepnder = true;
+        attachedConsoleAppender = true;
       }
       
       // As soon as we start configuration process, the pristine flag is set to 
@@ -442,7 +452,7 @@ public class PropertyConfigurator extends ConfiguratorBase
 
       getLogger(repository).debug("Finished configuring.");
 
-      if (attachedConsoleApepnder) {
+      if (attachedConsoleAppender) {
         detachTemporaryConsoleAppender(repository, errorList);
       }
 
--- tests/src/java/org/apache/log4j/xml/ResetTest.java
+++ tests/src/java/org/apache/log4j/xml/ResetTest.java
@@ -1,33 +1,68 @@
 package org.apache.log4j.xml;
 
+import java.util.Properties;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.joran.JoranConfigurator;
 
 import junit.framework.TestCase;
 
 public class ResetTest extends TestCase {
   
-  public void testNoReset() {
-    test("input/xml/resetTest.xml", true);
-    test("input/xml/defaultInit.xml", false);
+  Logger logx; 
+  
+  protected void setUp() {
+    LogManager.getLoggerRepository().resetConfiguration();
+    initx();
+  }
+  
+  public void testResetXml() {
+    test("tests/input/xml/defaultInit.xml", false);
+    test("tests/input/xml/resetTest.xml", true);
+  }
+  
+  private void initx() {
+    logx = Logger.getLogger("x");
+    logx.setLevel(Level.ERROR);
+    assertEquals(Level.ERROR, logx.getLevel());
+  }
+  
+  public void testResetProps() {
+    Properties p = new Properties();
+    p.put("log4j.rootLogger","DEBUG,C");
+    p.put("log4j.appender.C","org.apache.log4j.ConsoleAppender");
+    p.put("log4j.appender.C","layout=org.apache.log4j.SimpleLayout");
+    testProperties(p, false);
+    testProperties(p, true);
   }
 
   private void test(String file, boolean reset)
   {
-    LogManager.getLoggerRepository().resetConfiguration();
-    Logger l = Logger.getLogger("x");
-    l.setLevel(Level.ERROR);
-    assertEquals(Level.ERROR, l.getLevel());
-
     JoranConfigurator jc = new JoranConfigurator();
     jc.doConfigure(file, LogManager.getLoggerRepository());
     if (reset)
-      assertEquals(null, l.getLevel());
+      assertEquals("reset=" + reset, null, logx.getLevel());
     else
-      assertEquals(Level.ERROR, l.getLevel());
+      assertEquals(Level.ERROR, logx.getLevel());
   }
   
+  private void testProperties(Properties p, boolean reset)
+  {
+    if (reset) {
+      p.put("log4j.reset", "true");
+    } else {
+      p.remove("log4j.reset");
+    }
+    System.out.println(" --- test " + p);
+    PropertyConfigurator.configure(p);
+    if (reset)
+      assertEquals("reset=" + reset, null, logx.getLevel());
+    else
+      assertEquals(Level.ERROR, logx.getLevel());
+  }
 
+  
 }
