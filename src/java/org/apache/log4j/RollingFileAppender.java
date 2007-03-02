ff24448a0f3d6d72bc2c8a629afe8f243e479005
Curtis William Arnold <carnold@apache.org>
2007-03-02 06:58:38 +0000

    Bug 41735: RollingFileAppender may delete files during rollover
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/branches/v1_2-branch@513638 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/RollingFileAppender.java
+++ src/java/org/apache/log4j/RollingFileAppender.java
@@ -47,6 +47,8 @@ public class RollingFileAppender extends FileAppender {
    */
   protected int  maxBackupIndex  = 1;
 
+  private long nextRollover = 0;
+
   /**
      The default constructor simply calls its {@link
      FileAppender#FileAppender parents constructor}.  */
@@ -120,27 +122,33 @@ public class RollingFileAppender extends FileAppender {
     File file;
 
     if (qw != null) {
-    	LogLog.debug("rolling over count=" + ((CountingQuietWriter) qw).getCount());
+        long size = ((CountingQuietWriter) qw).getCount();
+        LogLog.debug("rolling over count=" + size);
+        //   if operation fails, do not roll again until
+        //      maxFileSize more bytes are written
+        nextRollover = size + maxFileSize;
     }
     LogLog.debug("maxBackupIndex="+maxBackupIndex);
 
+    boolean renameSucceeded = true;
     // If maxBackups <= 0, then there is no file renaming to be done.
     if(maxBackupIndex > 0) {
       // Delete the oldest file, to keep Windows happy.
       file = new File(fileName + '.' + maxBackupIndex);
       if (file.exists())
-       file.delete();
+       renameSucceeded = file.delete();
 
       // Map {(maxBackupIndex - 1), ..., 2, 1} to {maxBackupIndex, ..., 3, 2}
-      for (int i = maxBackupIndex - 1; i >= 1; i--) {
+      for (int i = maxBackupIndex - 1; i >= 1 && renameSucceeded; i--) {
 	file = new File(fileName + "." + i);
 	if (file.exists()) {
 	  target = new File(fileName + '.' + (i + 1));
 	  LogLog.debug("Renaming file " + file + " to " + target);
-	  file.renameTo(target);
+	  renameSucceeded = file.renameTo(target);
 	}
       }
 
+    if(renameSucceeded) {
       // Rename fileName to fileName.1
       target = new File(fileName + "." + 1);
 
@@ -148,16 +156,34 @@ public class RollingFileAppender extends FileAppender {
 
       file = new File(fileName);
       LogLog.debug("Renaming file " + file + " to " + target);
-      file.renameTo(target);
+      renameSucceeded = file.renameTo(target);
+      //
+      //   if file rename failed, reopen file with append = true
+      //
+      if (!renameSucceeded) {
+          try {
+            this.setFile(fileName, true, bufferedIO, bufferSize);
+          }
+          catch(IOException e) {
+            LogLog.error("setFile("+fileName+", true) call failed.", e);
+          }
+      }
+    }
     }
 
+    //
+    //   if all renames were successful, then
+    //
+    if (renameSucceeded) {
     try {
       // This will also close the file. This is OK since multiple
       // close operations are safe.
       this.setFile(fileName, false, bufferedIO, bufferSize);
+      nextRollover = 0;
     }
     catch(IOException e) {
       LogLog.error("setFile("+fileName+", false) call failed.", e);
+    }
     }
   }
 
@@ -235,8 +261,11 @@ public class RollingFileAppender extends FileAppender {
   protected
   void subAppend(LoggingEvent event) {
     super.subAppend(event);
-    if((fileName != null) &&
-                     ((CountingQuietWriter) qw).getCount() >= maxFileSize)
-      this.rollOver();
+    if(fileName != null && qw != null) {
+        long size = ((CountingQuietWriter) qw).getCount();
+        if (size >= maxFileSize && size >= nextRollover) {
+            rollOver();
+        }
+    }
    }
 }
