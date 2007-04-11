0f8e0c206c67f291a3a14af6a8998c7c7f6d50b5
Curtis William Arnold <carnold@apache.org>
2007-04-11 19:29:39 +0000

    Bug 42092: Checkstyle fixes in org.apache.log4j.plugins
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@527629 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/log4j/plugins/PluginRegistry.java
+++ src/java/org/apache/log4j/plugins/PluginRegistry.java
@@ -29,253 +29,276 @@ import org.apache.log4j.spi.LoggerRepositoryEventListener;
 
 
 /**
-  This is a registry for Plugin instances. It provides methods to
-  start and stop plugin objects individually and to stop all
-  plugins for a repository.
-
-  @author Mark Womack
-  @author Paul Smith
-  @since 1.3
-*/
+ * This is a registry for Plugin instances. It provides methods to
+ * start and stop plugin objects individually and to stop all
+ * plugins for a repository.
+ *
+ * @author Mark Womack
+ * @author Paul Smith
+ * @since 1.3
+ */
 public final class PluginRegistry {
-  /**
-   * The pluginMap is keyed by plugin name and contains plugins as values.
-   * key=plugin.getName, value=plugin
-   */
-  private final Map pluginMap;
-  private final LoggerRepositoryEx loggerRepository;
+    /**
+     * The pluginMap is keyed by plugin name and contains plugins as values.
+     * key=plugin.getName, value=plugin
+     */
+    private final Map pluginMap;
+    /**
+     * Logger repository.
+     */
+    private final LoggerRepositoryEx loggerRepository;
 
-  /**
-   * the listener used to listen for repository events.
-   */
-  private final RepositoryListener listener = new RepositoryListener();
-  private final List listenerList = Collections.synchronizedList(new ArrayList());
+    /**
+     * the listener used to listen for repository events.
+     */
+    private final RepositoryListener listener = new RepositoryListener();
+    /**
+     * List of listeners.
+     */
+    private final List listenerList =
+            Collections.synchronizedList(new ArrayList());
 
-  public PluginRegistry(LoggerRepositoryEx loggerRepository) {
-    pluginMap = new HashMap();
-    this.loggerRepository = loggerRepository;
-    this.loggerRepository.addLoggerRepositoryEventListener(listener);
-  }
-
-  public LoggerRepositoryEx getLoggerRepository() {
-    return loggerRepository;
-  }
-
-
-  /**
-   * Returns true if the specified name is already taken by
-   * an existing Plugin registered within the scope of the specified
-   * LoggerRepository.
-   * @param name The name to check the repository for
-   * @return true if the name is already in use, otherwise false
-   */
-  public boolean pluginNameExists(String name) {
-    synchronized (pluginMap) {
-      return pluginMap.containsKey(name);
+    /**
+     * Creates a new instance.
+     * @param repository logger repository.
+     */
+    public PluginRegistry(final LoggerRepositoryEx repository) {
+        super();
+        pluginMap = new HashMap();
+        this.loggerRepository = repository;
+        this.loggerRepository.addLoggerRepositoryEventListener(listener);
     }
-  }
 
-
-  /**
-   * Adds a plugin to the plugin registry. If a plugin with the same name exists
-   * already, it is shutdown and removed.
-   *
-   * @param plugin the plugin to add.
-   * */
-  public void addPlugin(Plugin plugin) {
-    // put plugin into the repository's reciever map
-    synchronized (pluginMap) {
-      String name = plugin.getName();
-
-      // make sure the plugin has reference to repository
-      plugin.setLoggerRepository(getLoggerRepository());
-
-      Plugin existingPlugin = (Plugin)pluginMap.get(name);
-      if (existingPlugin != null) {
-        existingPlugin.shutdown();
-      }
-
-      // put the new plugin into the map
-      pluginMap.put(name, plugin);
-      firePluginStarted(plugin);
+    /**
+     * Get logger repository.
+     * @return logger repository.
+     */
+    public LoggerRepositoryEx getLoggerRepository() {
+        return loggerRepository;
     }
-  }
 
 
-  /**
-   * Calls the pluginStarted method on every registered PluginListener.
-   *
-   * @param plugin The plugin that has been started.
-   */
-  private void firePluginStarted(Plugin plugin) {
-  	PluginEvent e = null;
-  	synchronized(listenerList){
-  		for (Iterator iter = listenerList.iterator(); iter.hasNext();) {
-  			PluginListener listener = (PluginListener) iter.next();
-  			if (e == null) {
-  				e = new PluginEvent(plugin);
-  			}
-  			listener.pluginStarted(e);
-  		}
-  	}
-  }
-
-
-  /**
-   * Calls the pluginStopped method for every registered PluginListner.
-   *
-   * @param plugin The plugin that has been stopped.
-   */
-  private void firePluginStopped(Plugin plugin) {
-  	PluginEvent e = null;
-  	synchronized(listenerList){
-  		for (Iterator iter = listenerList.iterator(); iter.hasNext();) {
-  			PluginListener listener = (PluginListener) iter.next();
-  			if (e == null) {
-  				e = new PluginEvent(plugin);
-  			}
-  			listener.pluginStopped(e);
-  		}
-  	}
-  }
-
-
-  /**
-   * Returns all the plugins for a given repository.
-   *
-   * @return List list of plugins from the repository.
-   */
-  public List getPlugins() {
-    synchronized (pluginMap) {
-      List pluginList = new ArrayList(pluginMap.size());
-      Iterator iter = pluginMap.values().iterator();
-
-      while (iter.hasNext()) {
-        pluginList.add(iter.next());
-      }
-      return pluginList;
-    }
-  }
-
-
-  /**
-    Returns all the plugins for a given repository that are instances
-    of a certain class.
-
-    @param pluginClass the class the plugin must implement to be selected.
-    @return List list of plugins from the repository. */
-  public List getPlugins(Class pluginClass) {
-    synchronized (pluginMap) {
-      List pluginList = new ArrayList(pluginMap.size());
-      Iterator iter = pluginMap.values().iterator();
-
-      while (iter.hasNext()) {
-        Object plugin = iter.next();
-
-        if (pluginClass.isInstance(plugin)) {
-          pluginList.add(plugin);
+    /**
+     * Returns true if the specified name is already taken by
+     * an existing Plugin registered within the scope of the specified
+     * LoggerRepository.
+     *
+     * @param name The name to check the repository for
+     * @return true if the name is already in use, otherwise false
+     */
+    public boolean pluginNameExists(final String name) {
+        synchronized (pluginMap) {
+            return pluginMap.containsKey(name);
         }
-      }
-      return pluginList;
-    }
-  }
-
-
-  /**
-    Stops a plugin by plugin name and repository.
-
-    @param pluginName the name of the plugin to stop.
-    @return Plugin the plugin, if stopped, or null if the
-      the plugin was not found in the registry. */
-  public Plugin stopPlugin(String pluginName) {
-    synchronized (pluginMap) {
-      Plugin plugin = (Plugin)pluginMap.get(pluginName);
-
-      if (plugin == null) {
-        return null;
-      }
-
-      // shutdown the plugin
-      plugin.shutdown();
-
-      // remove it from the plugin map
-      pluginMap.remove(pluginName);
-      firePluginStopped(plugin);
-
-      // return it for future use
-      return plugin;
-    }
-  }
-
-  /**
-    Stops all plugins in the given logger repository.
-
-
-   */
-  public void stopAllPlugins() {
-    synchronized (pluginMap) {
-      // remove the listener for this repository
-      loggerRepository.removeLoggerRepositoryEventListener(listener);
-
-      Iterator iter = pluginMap.values().iterator();
-
-      while (iter.hasNext()) {
-        Plugin plugin = (Plugin)iter.next();
-        plugin.shutdown();
-        firePluginStopped(plugin);
-      }
-    }
-  }
-
-
-  /**
-   * Adds a PluginListener to this registry to be notified
-   * of PluginEvents
-   *
-   * @param l PluginListener to add to this registry
-   */
-  public final void addPluginListener(PluginListener l) {
-    listenerList.add(l);
-  }
-
-
-  /**
-   * Removes a particular PluginListener from this registry
-   * such that it will no longer be notified of PluginEvents
-   *
-   * @param l PluginListener to remove
-   */
-  public final void removePluginListener(PluginListener l) {
-    listenerList.remove(l);
-  }
-
-  /**
-    Internal class used to handle listener events from repositories. */
-  private class RepositoryListener implements LoggerRepositoryEventListener {
-    /**
-      Stops all plugins associated with the repository being reset.
-
-      @param repository the repository that was reset. */
-    public void configurationResetEvent(LoggerRepository repository) {
-      PluginRegistry.this.stopAllPlugins();
     }
 
 
     /**
-      Called when the repository configuration is changed.
+     * Adds a plugin to the plugin registry.
+     * If a plugin with the same name exists
+     * already, it is shutdown and removed.
+     *
+     * @param plugin the plugin to add.
+     */
+    public void addPlugin(final Plugin plugin) {
+        // put plugin into the repository's reciever map
+        synchronized (pluginMap) {
+            String name = plugin.getName();
 
-      @param repository the repository that was changed. */
-    public void configurationChangedEvent(LoggerRepository repository) {
-      // do nothing with this event
+            // make sure the plugin has reference to repository
+            plugin.setLoggerRepository(getLoggerRepository());
+
+            Plugin existingPlugin = (Plugin) pluginMap.get(name);
+            if (existingPlugin != null) {
+                existingPlugin.shutdown();
+            }
+
+            // put the new plugin into the map
+            pluginMap.put(name, plugin);
+            firePluginStarted(plugin);
+        }
     }
 
 
     /**
-      Stops all plugins associated with the repository being shutdown.
-
-      @param repository the repository being shutdown. */
-    public void shutdownEvent(LoggerRepository repository) {
-      PluginRegistry.this.stopAllPlugins();
+     * Calls the pluginStarted method on every registered PluginListener.
+     *
+     * @param plugin The plugin that has been started.
+     */
+    private void firePluginStarted(final Plugin plugin) {
+        PluginEvent e = null;
+        synchronized (listenerList) {
+            for (Iterator iter = listenerList.iterator(); iter.hasNext();) {
+                PluginListener l = (PluginListener) iter.next();
+                if (e == null) {
+                    e = new PluginEvent(plugin);
+                }
+                l.pluginStarted(e);
+            }
+        }
     }
-  }
+
+
+    /**
+     * Calls the pluginStopped method for every registered PluginListner.
+     *
+     * @param plugin The plugin that has been stopped.
+     */
+    private void firePluginStopped(final Plugin plugin) {
+        PluginEvent e = null;
+        synchronized (listenerList) {
+            for (Iterator iter = listenerList.iterator(); iter.hasNext();) {
+                PluginListener l = (PluginListener) iter.next();
+                if (e == null) {
+                    e = new PluginEvent(plugin);
+                }
+                l.pluginStopped(e);
+            }
+        }
+    }
+
+
+    /**
+     * Returns all the plugins for a given repository.
+     *
+     * @return List list of plugins from the repository.
+     */
+    public List getPlugins() {
+        synchronized (pluginMap) {
+            List pluginList = new ArrayList(pluginMap.size());
+            Iterator iter = pluginMap.values().iterator();
+
+            while (iter.hasNext()) {
+                pluginList.add(iter.next());
+            }
+            return pluginList;
+        }
+    }
+
+
+    /**
+     * Returns all the plugins for a given repository that are instances
+     * of a certain class.
+     *
+     * @param pluginClass the class the plugin must implement to be selected.
+     * @return List list of plugins from the repository.
+     */
+    public List getPlugins(final Class pluginClass) {
+        synchronized (pluginMap) {
+            List pluginList = new ArrayList(pluginMap.size());
+            Iterator iter = pluginMap.values().iterator();
+
+            while (iter.hasNext()) {
+                Object plugin = iter.next();
+
+                if (pluginClass.isInstance(plugin)) {
+                    pluginList.add(plugin);
+                }
+            }
+            return pluginList;
+        }
+    }
+
+
+    /**
+     * Stops a plugin by plugin name and repository.
+     *
+     * @param pluginName the name of the plugin to stop.
+     * @return Plugin the plugin, if stopped, or null if the
+     *         the plugin was not found in the registry.
+     */
+    public Plugin stopPlugin(final String pluginName) {
+        synchronized (pluginMap) {
+            Plugin plugin = (Plugin) pluginMap.get(pluginName);
+
+            if (plugin == null) {
+                return null;
+            }
+
+            // shutdown the plugin
+            plugin.shutdown();
+
+            // remove it from the plugin map
+            pluginMap.remove(pluginName);
+            firePluginStopped(plugin);
+
+            // return it for future use
+            return plugin;
+        }
+    }
+
+    /**
+     * Stops all plugins in the given logger repository.
+     */
+    public void stopAllPlugins() {
+        synchronized (pluginMap) {
+            // remove the listener for this repository
+            loggerRepository.removeLoggerRepositoryEventListener(listener);
+
+            Iterator iter = pluginMap.values().iterator();
+
+            while (iter.hasNext()) {
+                Plugin plugin = (Plugin) iter.next();
+                plugin.shutdown();
+                firePluginStopped(plugin);
+            }
+        }
+    }
+
+
+    /**
+     * Adds a PluginListener to this registry to be notified
+     * of PluginEvents.
+     *
+     * @param l PluginListener to add to this registry
+     */
+    public void addPluginListener(final PluginListener l) {
+        listenerList.add(l);
+    }
+
+
+    /**
+     * Removes a particular PluginListener from this registry
+     * such that it will no longer be notified of PluginEvents.
+     *
+     * @param l PluginListener to remove
+     */
+    public void removePluginListener(final PluginListener l) {
+        listenerList.remove(l);
+    }
+
+    /**
+     * Internal class used to handle listener events from repositories.
+     */
+    private class RepositoryListener implements LoggerRepositoryEventListener {
+        /**
+         * Stops all plugins associated with the repository being reset.
+         *
+         * @param repository the repository that was reset.
+         */
+        public void configurationResetEvent(final LoggerRepository repository) {
+            PluginRegistry.this.stopAllPlugins();
+        }
+
+
+        /**
+         * Called when the repository configuration is changed.
+         *
+         * @param repository the repository that was changed.
+         */
+        public void configurationChangedEvent(
+                final LoggerRepository repository) {
+            // do nothing with this event
+        }
+
+
+        /**
+         * Stops all plugins associated with the repository being shutdown.
+         *
+         * @param repository the repository being shutdown.
+         */
+        public void shutdownEvent(final LoggerRepository repository) {
+            PluginRegistry.this.stopAllPlugins();
+        }
+    }
 }
--- src/java/org/apache/log4j/plugins/PluginSkeleton.java
+++ src/java/org/apache/log4j/plugins/PluginSkeleton.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,160 +26,198 @@ import java.beans.PropertyChangeSupport;
 
 
 /**
-  A convienent abstract class for plugin subclasses that implements
-  the basic methods of the Plugin interface. Subclasses are required
-  to implement the isActive(), activateOptions(), and shutdown()
-  methods.
-
-  <p>Developers are not required to subclass PluginSkeleton to
-  develop their own plugins (they are only required to implement the
-  Plugin interface), but it provides a convenient base class to start
-  from.
-
-  Contributors: Nicko Cadell
-
-  @author Mark Womack <mwomack@apache.org>
-  @author Paul Smith <psmith@apache.org>
-  @since 1.3
-*/
+ * A convienent abstract class for plugin subclasses that implements
+ * the basic methods of the Plugin interface. Subclasses are required
+ * to implement the isActive(), activateOptions(), and shutdown()
+ * methods.
+ * <p/>
+ * <p>Developers are not required to subclass PluginSkeleton to
+ * develop their own plugins (they are only required to implement the
+ * Plugin interface), but it provides a convenient base class to start
+ * from.
+ * <p/>
+ * Contributors: Nicko Cadell
+ *
+ * @author Mark Womack (mwomack@apache.org)
+ * @author Paul Smith (psmith@apache.org)
+ * @since 1.3
+ */
 public abstract class PluginSkeleton extends ComponentBase implements Plugin {
-  /** Name of this plugin. */
-  protected String name = "";
+    /**
+     * Name of this plugin.
+     */
+    protected String name = "";
 
-  protected boolean active;
+    /**
+     * Active state of plugin.
+     */
+    protected boolean active;
 
-  /**
-   * This is a delegate that does all the PropertyChangeListener
-   * support.
-   */
-  private PropertyChangeSupport propertySupport =
-    new PropertyChangeSupport(this);
+    /**
+     * This is a delegate that does all the PropertyChangeListener
+     * support.
+     */
+    private PropertyChangeSupport propertySupport =
+            new PropertyChangeSupport(this);
 
-  /**
-    Gets the name of the plugin.
+    /**
+     * Construct new instance.
+     */
+    protected PluginSkeleton() {
+        super();
+    }
 
-    @return String the name of the plugin. */
-  public String getName() {
-    return name;
-  }
+    /**
+     * Gets the name of the plugin.
+     *
+     * @return String the name of the plugin.
+     */
+    public String getName() {
+        return name;
+    }
 
-  /**
-    Sets the name of the plugin and notifies PropertyChangeListeners of the change
+    /**
+     * Sets the name of the plugin and notifies
+     * PropertyChangeListeners of the change.
+     *
+     * @param newName the name of the plugin to set.
+     */
+    public void setName(final String newName) {
+        String oldName = this.name;
+        this.name = newName;
+        propertySupport.firePropertyChange("name", oldName, this.name);
+    }
 
-    @param name the name of the plugin to set. */
-  public void setName(String name) {
-    String oldName = this.name;
-    this.name = name;
-    propertySupport.firePropertyChange("name", oldName, this.name);
-  }
+    /**
+     * Gets the logger repository for this plugin.
+     *
+     * @return LoggerRepository the logger repository this plugin will affect.
+     */
+    public LoggerRepository getLoggerRepository() {
+        return repository;
+    }
 
-  /**
-    Gets the logger repository for this plugin.
+    /**
+     * Sets the logger repository used by this plugin and notifies a
+     * relevant PropertyChangeListeners registered. This
+     * repository will be used by the plugin functionality.
+     *
+     * @param repository the logger repository that this plugin should affect.
+     */
+    public void setLoggerRepository(final LoggerRepository repository) {
+        Object oldValue = this.repository;
+        this.repository = repository;
+        firePropertyChange("loggerRepository", oldValue, this.repository);
+    }
 
-    @return LoggerRepository the logger repository this plugin will affect. */
-  public LoggerRepository getLoggerRepository() {
-    return repository;
-  }
+    /**
+     * Returns whether this plugin is Active or not.
+     *
+     * @return true/false
+     */
+    public synchronized boolean isActive() {
+        return active;
+    }
 
-  /**
-    Sets the logger repository used by this plugin and notifies an relevant PropertyChangeListeners registered. This
-    repository will be used by the plugin functionality.
+    /**
+     * Returns true if the plugin has the same name and logger repository as the
+     * testPlugin passed in.
+     *
+     * @param testPlugin The plugin to test equivalency against.
+     * @return Returns true if testPlugin is considered to be equivalent.
+     */
+    public boolean isEquivalent(final Plugin testPlugin) {
+        return (repository == testPlugin.getLoggerRepository())
+                && ((this.name == null && testPlugin.getName() == null)
+                || (this.name != null
+                           && name.equals(testPlugin.getName())))
+                && this.getClass().equals(testPlugin.getClass());
+    }
 
-    @param repository the logger repository that this plugin should affect. */
-  public void setLoggerRepository(LoggerRepository repository) {
-    Object oldValue = this.repository;
-    this.repository = repository;
-    firePropertyChange("loggerRepository", oldValue, this.repository);
-  }
+    /**
+     * Add property change listener.
+     * @param listener listener.
+     */
+    public final void addPropertyChangeListener(
+            final PropertyChangeListener listener) {
+        propertySupport.addPropertyChangeListener(listener);
+    }
 
-  /**
-   * Returns whether this plugin is Active or not
-   * @return true/false
-   */
-  public synchronized boolean isActive() {
-    return active;
-  }
+    /**
+     * Add property change listener for one property only.
+     * @param propertyName property name.
+     * @param listener listener.
+     */
+    public final void addPropertyChangeListener(
+            final String propertyName,
+            final PropertyChangeListener listener) {
+        propertySupport.addPropertyChangeListener(propertyName, listener);
+    }
 
-  /**
-   * Returns true if the plugin has the same name and logger repository as the
-   * testPlugin passed in.
-   * 
-   * @param testPlugin The plugin to test equivalency against.
-   * @return Returns true if testPlugin is considered to be equivalent.
-   */
-  public boolean isEquivalent(Plugin testPlugin) {
-    return (repository == testPlugin.getLoggerRepository()) &&
-      ((this.name == null && testPlugin.getName() == null) ||
-       (this.name != null && name.equals(testPlugin.getName()))) && this.getClass().equals(testPlugin.getClass());
-  }
+    /**
+     * Remove property change listener.
+     * @param listener listener.
+     */
+    public final void removePropertyChangeListener(
+            final PropertyChangeListener listener) {
+        propertySupport.removePropertyChangeListener(listener);
+    }
 
-  /**
-   * @param listener
-   */
-  public final void addPropertyChangeListener(PropertyChangeListener listener) {
-    propertySupport.addPropertyChangeListener(listener);
-  }
+    /**
+     * Remove property change listener on a specific property.
+     * @param propertyName property name.
+     * @param listener listener.
+     */
+    public final void removePropertyChangeListener(
+            final String propertyName,
+            final PropertyChangeListener listener) {
+        propertySupport.removePropertyChangeListener(propertyName, listener);
+    }
 
-  /**
-   * @param propertyName
-   * @param listener
-   */
-  public final void addPropertyChangeListener(
-    String propertyName, PropertyChangeListener listener) {
-    propertySupport.addPropertyChangeListener(propertyName, listener);
-  }
+    /**
+     * Fire a property change event to appropriate listeners.
+     * @param evt change event.
+     */
+    protected final void firePropertyChange(
+            final PropertyChangeEvent evt) {
+        propertySupport.firePropertyChange(evt);
+    }
 
-  /**
-   * @param listener
-   */
-  public final void removePropertyChangeListener(
-    PropertyChangeListener listener) {
-    propertySupport.removePropertyChangeListener(listener);
-  }
+    /**
+     * Fire property change event to appropriate listeners.
+     * @param propertyName property name.
+     * @param oldValue old value.
+     * @param newValue new value.
+     */
+    protected final void firePropertyChange(
+            final String propertyName,
+            final boolean oldValue,
+            final boolean newValue) {
+        propertySupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
 
-  /**
-   * @param propertyName
-   * @param listener
-   */
-  public final void removePropertyChangeListener(
-    String propertyName, PropertyChangeListener listener) {
-    propertySupport.removePropertyChangeListener(propertyName, listener);
-  }
+    /**
+     * Fire property change event to appropriate listeners.
+     * @param propertyName property name.
+     * @param oldValue old value.
+     * @param newValue new value.
+     */
+    protected final void firePropertyChange(
+            final String propertyName,
+            final int oldValue, final int newValue) {
+        propertySupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
 
-  /**
-   * @param evt
-   */
-  protected final void firePropertyChange(PropertyChangeEvent evt) {
-    propertySupport.firePropertyChange(evt);
-  }
-
-  /**
-   * @param propertyName
-   * @param oldValue
-   * @param newValue
-   */
-  protected final void firePropertyChange(
-    String propertyName, boolean oldValue, boolean newValue) {
-    propertySupport.firePropertyChange(propertyName, oldValue, newValue);
-  }
-
-  /**
-   * @param propertyName
-   * @param oldValue
-   * @param newValue
-   */
-  protected final void firePropertyChange(
-    String propertyName, int oldValue, int newValue) {
-    propertySupport.firePropertyChange(propertyName, oldValue, newValue);
-  }
-
-  /**
-   * @param propertyName
-   * @param oldValue
-   * @param newValue
-   */
-  protected final void firePropertyChange(
-    String propertyName, Object oldValue, Object newValue) {
-    propertySupport.firePropertyChange(propertyName, oldValue, newValue);
-  }
+    /**
+     * Fire property change event to appropriate listeners.
+     * @param propertyName property name.
+     * @param oldValue old value.
+     * @param newValue new value.
+     */
+    protected final void firePropertyChange(
+            final String propertyName,
+            final Object oldValue,
+            final Object newValue) {
+        propertySupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
 }
