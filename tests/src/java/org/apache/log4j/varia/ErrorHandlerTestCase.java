e36d192bdf9bdbc7919a905e016dcd5a6f59aebf
Curtis William Arnold <carnold@apache.org>
2008-07-18 02:20:19 +0000

    Bug 45426: ErrorHandlerTestCase is not run and does not pass
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4j/trunk@677804 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/java/org/apache/log4j/varia/ErrorHandlerTestCase.java
+++ tests/src/java/org/apache/log4j/varia/ErrorHandlerTestCase.java
@@ -18,39 +18,30 @@
 package org.apache.log4j.varia;
 
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import junit.framework.Test;
-
-import org.apache.log4j.Logger;
+import org.apache.log4j.Appender;
 import org.apache.log4j.Level;
-import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.ErrorHandler;
 import org.apache.log4j.util.Filter;
-import org.apache.log4j.util.LineNumberFilter;
-import org.apache.log4j.util.ControlFilter;
-import org.apache.log4j.util.ISO8601Filter;
 import org.apache.log4j.util.Transformer;
 import org.apache.log4j.util.Compare;
+import org.apache.log4j.util.JunitTestRunnerFilter;
+import org.apache.log4j.util.LineNumberFilter;
+import org.apache.log4j.util.ControlFilter;
+import org.apache.log4j.xml.DOMConfigurator;
 
 public class ErrorHandlerTestCase extends TestCase {
 
-  static String TEMP_A1 = "output/temp.A1";
-  static String TEMP_A2 = "output/temp.A2";
-  static String FILTERED_A1 = "output/filtered.A1";
-  static String FILTERED_A2 = "output/filtered.A2";
+  static String TEMP = "output/temp";
+  static String FILTERED = "output/filtered";
 
 
   static String EXCEPTION1 = "java.lang.Exception: Just testing";
   static String EXCEPTION2 = "\\s*at .*\\(.*:\\d{1,4}\\)";
   static String EXCEPTION3 = "\\s*at .*\\(Native Method\\)";
 
-  static String TEST1_1A_PAT = 
-                       "(DEBUG|INFO |WARN |ERROR|FATAL) \\w*\\.\\w* - Message \\d";
-
-  static String TEST1_1B_PAT = "(DEBUG|INFO |WARN |ERROR|FATAL) root - Message \\d";
-
-  static String TEST1_2_PAT = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3} "+
-                        "\\[main]\\ (DEBUG|INFO|WARN|ERROR|FATAL) .* - Message \\d";
-
+  static String TEST1_PAT =
+                       "FALLBACK - (root|test) - Message \\d";
 
 
   Logger root; 
@@ -71,22 +62,21 @@ public class ErrorHandlerTestCase extends TestCase {
 
   public void test1() throws Exception {
     DOMConfigurator.configure("input/xml/fallback1.xml");
+    Appender primary = root.getAppender("PRIMARY");
+    ErrorHandler eh = primary.getErrorHandler();
+    assertNotNull(eh);
+
     common();
 
-    ControlFilter cf1 = new ControlFilter(new String[]{TEST1_1A_PAT, TEST1_1B_PAT, 
+    ControlFilter cf = new ControlFilter(new String[]{TEST1_PAT,
 					       EXCEPTION1, EXCEPTION2, EXCEPTION3});
 
-    ControlFilter cf2 = new ControlFilter(new String[]{TEST1_2_PAT, 
-					       EXCEPTION1, EXCEPTION2, EXCEPTION3});
+    Transformer.transform(TEMP, FILTERED, new Filter[] {cf,
+							new LineNumberFilter(),
+                            new JunitTestRunnerFilter()});
 
-    Transformer.transform(TEMP_A1, FILTERED_A1, new Filter[] {cf1, 
-							new LineNumberFilter()});
 
-    Transformer.transform(TEMP_A2, FILTERED_A2, new Filter[] {cf2,
-                                      new LineNumberFilter(), new ISO8601Filter()});
-
-    assertTrue(Compare.compare(FILTERED_A1, "witness/dom.A1.1"));
-    assertTrue(Compare.compare(FILTERED_A2, "witness/dom.A2.1"));
+    assertTrue(Compare.compare(FILTERED, "witness/fallback1"));
   }
 
   void common() {
@@ -114,12 +104,6 @@ public class ErrorHandlerTestCase extends TestCase {
     logger.error("Message " + ++i, e);
     root.error("Message " + i, e);    
 
-  }
-
-  public static Test suite() {
-    TestSuite suite = new TestSuite();
-    suite.addTest(new ErrorHandlerTestCase("test1"));
-    return suite;
   }
 
 }
