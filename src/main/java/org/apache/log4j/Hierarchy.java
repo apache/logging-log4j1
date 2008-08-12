1fba280bfdfc2c82acac3f44d2023ea296aa85b5
Curtis William Arnold <carnold@apache.org>
2008-08-12 17:03:29 +0000

    Bug 25747: more explanations when hitting WARN No appenders could be found for logger
    git-svn-id: https://svn.eu.apache.org/repos/asf/logging/log4j/trunk@685228 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/log4j/Hierarchy.java
+++ src/main/java/org/apache/log4j/Hierarchy.java
@@ -134,6 +134,7 @@ public class Hierarchy implements LoggerRepository, RendererSupport {
       LogLog.warn("No appenders could be found for logger (" +
 		   cat.getName() + ").");
       LogLog.warn("Please initialize the log4j system properly.");
+      LogLog.warn("See http://logging.apache.org/log4j/1.2/faq.html#noconfig for more info.");
       this.emittedNoAppenderWarning = true;
     }
   }
