8479b06285c1742ff54bac155bc74ce59390e10d
Leo Simons <leosimons@apache.org>
2021-12-18 13:39:48 +0100

    Disable more unsafe networking code in log4j.varia.
    Disables
    * ExternallyRolledFileAppender network code
    
    Behavior reduces to be a normal non-rolling FileAppender.

--- tests/src/java/org/apache/log4j/varia/ERFATestCase.java
+++ tests/src/java/org/apache/log4j/varia/ERFATestCase.java
@@ -59,30 +59,7 @@ public class ERFATestCase extends TestCase {
       assertEquals(0, appender.getPort());
   }
 
-    /**
-     * Send a message to the ERFA.
-     * @param port port number.
-     * @param msg message, may not be null.
-     * @param expectedResponse expected response, may not be null.
-     * @throws IOException thrown on IO error.
-     */
-  void sendMessage(int port, final String msg, final String expectedResponse) throws IOException {
-      Socket socket = new Socket((String) null, port);
-      DataInputStream reader = new DataInputStream(socket.getInputStream());
-      DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
-      writer.writeUTF(msg);
-      String response = reader.readUTF();
-      assertEquals(expectedResponse, response);
-      reader.close();
-      writer.close();
-      socket.close();
-  }
-
-    /**
-     * Test externally triggered rollover.
-     * @throws IOException thrown on IO error.
-     */
-  public void testRollover() throws IOException {
+  public void testRolloverDoesNotWork() {
       ExternallyRolledFileAppender erfa =
               new ExternallyRolledFileAppender();
 
@@ -96,46 +73,19 @@ public class ERFATestCase extends TestCase {
       erfa.setMaxBackupIndex(2);
       erfa.setPort(port);
       erfa.setFile("output/ERFA-test2.log");
-      try {
-        erfa.activateOptions();
-      } catch(SecurityException ex) {
-          return;
-      }
-      try {
-         Thread.sleep(100);
-      } catch(InterruptedException ex) {
-      }
+      erfa.activateOptions();
       root.addAppender(erfa);
-
 
       // Write exactly 10 bytes with each log
       for (int i = 0; i < 55; i++) {
         if (i < 10) {
           logger.debug("Hello---" + i);
-        } else if (i < 100) {
+        } else {
           logger.debug("Hello--" + i);
         }
-        if ((i % 10) == 9) {
-            try {
-                sendMessage(port, "RollOver", "OK");
-            } catch(SecurityException ex) {
-                return;
-            }
-        }
       }
-
-      try {
-        sendMessage(port,
-              "That's all folks.",
-              "Expecting [RollOver] string.");
-      } catch(SecurityException ex) {
-          return;
-      }
-
 
       assertTrue(new File("output/ERFA-test2.log").exists());
-      assertTrue(new File("output/ERFA-test2.log.1").exists());
-      assertTrue(new File("output/ERFA-test2.log.2").exists());
-      assertFalse(new File("output/ERFA-test2.log.3").exists());
+      assertFalse(new File("output/ERFA-test2.log.1").exists());
   }
 }
