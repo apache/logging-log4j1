02f7edd7cfe9c6020662d7fcdcabf6c7dc06e6de
Leo Simons <leosimons@apache.org>
2021-12-19 13:01:30 +0100

    fix: remove insecure code from varia package
    Disables networking in:
    * ExternallyRolledFileAppender
    
    Behavior reduces to be a normal non-rolling FileAppender.

--- src/main/java/org/apache/log4j/varia/ExternallyRolledFileAppender.java
+++ src/main/java/org/apache/log4j/varia/ExternallyRolledFileAppender.java
@@ -17,75 +17,43 @@
 
 package org.apache.log4j.varia;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import org.apache.log4j.RollingFileAppender;
 import org.apache.log4j.helpers.LogLog;
 
 /**
    This appender listens on a socket on the port specified by the
-   <b>Port</b> property for a "RollOver" message. When such a message
-   is received, the underlying log file is rolled over and an
-   acknowledgment message is sent back to the process initiating the
-   roll over.
+   <b>Port</b> property for a "RollOver" message in Log4j up to 1.2.17.
 
-   <p>This method of triggering roll over has the advantage of being
-   operating system independent, fast and reliable.
-
-   <p>A simple application {@link Roller} is provided to initiate the
-   roll over.
-
-   <p>Note that the initiator is not authenticated. Anyone can trigger
-   a rollover. In production environments, it is recommended that you
-   add some form of protection to prevent undesired rollovers.
-
+   Changed in 1.2.18+ to complain about its use and act as a regular
+   FileAppender otherwise. This may mean your files stop rolling over
+   if you keep using this class! See
+   <a href="https://logging.apache.org/log4j/1.2/">the log4j 1.2 homepage</a>
+   for more information on why this class is disabled since 1.2.18.
 
    @author Ceki G&uuml;lc&uuml;
-   @since version 0.9.0 */
+   @since version 0.9.0
+   @noinspection unused
+*/
 public class ExternallyRolledFileAppender extends RollingFileAppender {
-
-  /**
-     The string constant sent to initiate a roll over.   Current value of
-     this string constant is <b>RollOver</b>.
-  */
+  static final String EXTERNAL_ROLLING_UNSUPPORTED =
+      "ERROR-LOG4J-NETWORKING-UNSUPPORTED: External Rolled File Appender unsupported!" +
+      " This is a breaking change in Log4J 1 >=1.2.18. Change your config to stop using it!";
   static final public String ROLL_OVER = "RollOver";
-
-  /**
-     The string constant sent to acknowledge a roll over.   Current value of
-      this string constant is <b>OK</b>.
-  */
   static final public String OK = "OK";
 
-  int port = 0;
-  HUP hup;
-
-  /**
-     The default constructor does nothing but calls its super-class
-     constructor.  */
   public
   ExternallyRolledFileAppender() {
+    super();
+    LogLog.error(EXTERNAL_ROLLING_UNSUPPORTED);
   }
 
-  /**
-     The <b>Port</b> [roperty is used for setting the port for
-     listening to external roll over messages.
-  */
   public
   void setPort(int port) {
-    this.port = port;
   }
 
-  /**
-     Returns value of the <b>Port</b> option.
-   */
   public
   int getPort() {
-    return port;
+    return 0;
   }
 
   /**
@@ -94,96 +62,5 @@ public class ExternallyRolledFileAppender extends RollingFileAppender {
   public
   void activateOptions() {
     super.activateOptions();
-    if(port != 0) {
-      if(hup != null) {
-	hup.interrupt();
-      }
-      hup = new HUP(this, port);
-      hup.setDaemon(true);
-      hup.start();
-    }
   }
 }
-
-
-class HUP extends Thread {
-
-  int port;
-  ExternallyRolledFileAppender er;
-
-  HUP(ExternallyRolledFileAppender er, int port) {
-    this.er = er;
-    this.port = port;
-  }
-
-  public
-  void run() {
-    while(!isInterrupted()) {
-      try {
-	ServerSocket serverSocket = new ServerSocket(port);
-	while(true) {
-	  Socket socket = serverSocket.accept();
-	  LogLog.debug("Connected to client at " + socket.getInetAddress());
-	  new Thread(new HUPNode(socket, er), "ExternallyRolledFileAppender-HUP").start();
-	}
-      } catch(InterruptedIOException e) {
-        Thread.currentThread().interrupt();
-	    e.printStackTrace();
-      } catch(IOException e) {
-	    e.printStackTrace();
-      } catch(RuntimeException e) {
-	    e.printStackTrace();
-      }
-    }
-  }
-}
-
-class HUPNode implements Runnable {
-
-  Socket socket;
-  DataInputStream dis;
-  DataOutputStream dos;
-  ExternallyRolledFileAppender er;
-
-  public
-  HUPNode(Socket socket, ExternallyRolledFileAppender er) {
-    this.socket = socket;
-    this.er = er;
-    try {
-      dis = new DataInputStream(socket.getInputStream());
-      dos = new DataOutputStream(socket.getOutputStream());
-    } catch(InterruptedIOException e) {
-      Thread.currentThread().interrupt();
-      e.printStackTrace();
-    } catch(IOException e) {
-      e.printStackTrace();
-    } catch(RuntimeException e) {
-      e.printStackTrace();
-    }
-  }
-
-  public void run() {
-    try {
-      String line = dis.readUTF();
-      LogLog.debug("Got external roll over signal.");
-      if(ExternallyRolledFileAppender.ROLL_OVER.equals(line)) {
-	synchronized(er) {
-	  er.rollOver();
-	}
-	dos.writeUTF(ExternallyRolledFileAppender.OK);
-      }
-      else {
-	dos.writeUTF("Expecting [RollOver] string.");
-      }
-      dos.close();
-    } catch(InterruptedIOException e) {
-      Thread.currentThread().interrupt();
-      LogLog.error("Unexpected exception. Exiting HUPNode.", e);
-    } catch(IOException e) {
-      LogLog.error("Unexpected exception. Exiting HUPNode.", e);
-    } catch(RuntimeException e) {
-      LogLog.error("Unexpected exception. Exiting HUPNode.", e);
-    }
-  }
-}
-
