2f5a7546019c5c7c80798034359a190cd9d37587
Leo Simons <leosimons@apache.org>
2021-12-28 16:47:13 +0300

    test: run NTEventLogAppenderTest on Windows only

--- tests/src/java/org/apache/log4j/nt/NTEventLogAppenderTest.java
+++ tests/src/java/org/apache/log4j/nt/NTEventLogAppenderTest.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +16,12 @@
  */
 
 package org.apache.log4j.nt;
+
 import junit.framework.TestCase;
-import org.apache.log4j.Logger;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Level;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 
 /**
@@ -42,6 +43,9 @@ public class NTEventLogAppenderTest extends TestCase {
    *   Simple test of NTEventLogAppender.
    */
   public void testSimple() {
+    if (!System.getProperty("os.name").startsWith("Windows")) {
+      return;
+    }
     BasicConfigurator.configure(new NTEventLogAppender());
     Logger logger = Logger.getLogger("org.apache.log4j.nt.NTEventLogAppenderTest");
     int i  = 0;
